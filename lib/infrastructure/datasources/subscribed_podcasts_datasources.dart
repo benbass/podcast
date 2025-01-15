22fc173112d7bbce8e800e266eafe1c92eaed219
bruno <1201ba@gmail.com>
2025-01-15 17:32:29 +0100

    Added parameter unreadEpisodes to SubscribedPodcastEntity

--- lib/infrastructure/datasources/subscribed_podcasts_datasources.dart
+++ lib/infrastructure/datasources/subscribed_podcasts_datasources.dart
@@ -4,11 +4,11 @@ import 'package:http/http.dart' as http;
 
 import '../../domain/entities/subscribed_podcast_entity.dart';
 
-abstract class SubscribedPodcastsDataSources{
+abstract class SubscribedPodcastsDataSources {
   Future<List<SubscribedPodcastEntity>> getSubscribedPodcastsFromDb();
 }
 
-class SubscribedPodcastsDataSourcesImpl extends SubscribedPodcastsDataSources{
+class SubscribedPodcastsDataSourcesImpl extends SubscribedPodcastsDataSources {
   @override
   Future<List<SubscribedPodcastEntity>> getSubscribedPodcastsFromDb() async {
     // We are creating an object for now and until we implement the DB.
@@ -17,7 +17,11 @@ class SubscribedPodcastsDataSourcesImpl extends SubscribedPodcastsDataSources{
           'https://www.rbb-online.de/content/dam/rbb/inf/Podcasts2022/rbb24_Inforadio_Podcast_Unterwegs_16_9.jpg.jpg/rendition=ard.jpg/size=1400x1400.jpg'),
     );
     Uint8List? artwork = response.bodyBytes;
-    return [SubscribedPodcastEntity(artwork: artwork, url: "url")];
+    return [
+      SubscribedPodcastEntity(artwork: artwork, url: "url", unreadEpisodes: 14),
+      SubscribedPodcastEntity(artwork: artwork, url: "url2", unreadEpisodes: 3),
+      SubscribedPodcastEntity(
+          artwork: artwork, url: "url3", unreadEpisodes: 364),
+    ];
   }
-
 }
