759fc17bf74b7f5e6b7e4cb3a657c389b90c90e2
bruno <1201ba@gmail.com>
2025-01-22 17:08:21 +0100

    New const for base url

--- lib/core/globals.dart
+++ lib/core/globals.dart
@@ -2,5 +2,6 @@ import 'package:objectbox/objectbox.dart';
 import 'package:podcast/domain/entities/episode_entity.dart';
 import 'package:podcast/domain/entities/podcast_entity.dart';
 
+const String baseUrl = 'https://api.podcastindex.org/api/1.0';
 late Box<PodcastEntity> podcastBox;
 late Box<EpisodeEntity> episodeBox;
