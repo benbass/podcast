8fcd664b255926870207f1d00531469757dd3e4a
bruno <1201ba@gmail.com>
2025-01-28 18:39:12 +0100

    - code uncommented

--- lib/core/objectbox.dart
+++ lib/core/objectbox.dart
@@ -1,7 +1,6 @@
 import 'package:path/path.dart' as p;
 import 'package:path_provider/path_provider.dart';
-//import '../objectbox.g.dart'; // created by `flutter pub run build_runner build`
-/*
+import '../objectbox.g.dart'; // created by `flutter pub run build_runner build`
 
 class ObjectBox {
   /// The Store of this app.
@@ -14,9 +13,9 @@ class ObjectBox {
   /// Create an instance of ObjectBox to use throughout the app.
   static Future<ObjectBox> create() async {
     final docsDir = await getApplicationDocumentsDirectory();
-    // Future<Store> openStore() {...} is defined in the generated objectbox.g.dart
+    // method Future<Store> openStore() {...} is defined in the generated objectbox.g.dart
     final store = await openStore(directory: p.join(docsDir.path, "obx-podcasts"));
     return ObjectBox._create(store);
   }
 
-}*/
+}
