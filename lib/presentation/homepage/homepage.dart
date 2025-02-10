ffd898698b3666854f3a113bab2969f620421e93
bruno <1201ba@gmail.com>
2025-02-10 17:27:21 +0100

    Changed page structure

--- lib/presentation/homepage/homepage.dart
+++ lib/presentation/homepage/homepage.dart
@@ -1,117 +1,39 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:podcast/presentation/homepage/widgets/search_textfield.dart';
-import 'package:podcast/presentation/homepage/widgets/subscribed_podcast_card.dart';
 
 import '../../application/podcasts_bloc/podcasts_bloc.dart';
+import '../custom_widgets/page_transition.dart';
+import '../subscribed_podcasts/subscribed_podcasts_page.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
-
-  static const double _spacing = 20.0;
 
   @override
   Widget build(BuildContext context) {
     BlocProvider.of<PodcastsBloc>(context).add(FetchSubscribedPodcastsEvent());
 
-    return Scaffold(
-      appBar: AppBar(
-        title: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            const Text("Podcasts"),
-            IconButton(
-                onPressed: () {}, icon: const Icon(Icons.search_rounded)),
-          ],
+    void navigateToSubscribedPodcastsPage() {
+      Navigator.push(
+        context,
+        SlideBottomRoute(
+          page: const SubscribedPodcastsPage(),
         ),
-      ),
-      body: SafeArea(
-        child: BlocBuilder<PodcastsBloc, PodcastsState>(
-          builder: (context, state) {
-            return Padding(
-              padding: const EdgeInsets.symmetric(horizontal: _spacing),
-              child: CustomScrollView(
-                physics: const BouncingScrollPhysics(),
-                slivers: [
-                  const SliverToBoxAdapter(
-                    child: Padding(
-                      padding: EdgeInsets.only(bottom: _spacing),
-                      child: SearchTextField(),
-                    ),
-                  ),
-                  _buildPodcastGrid(state),
-                ],
-              ),
-            );
-          },
-        ),
-      ),
-    );
-  }
+      );
+    }
 
-  Widget _buildPodcastGrid(PodcastsState state) {
-    // Define the empty state widget
-    const emptyStateWidget = SliverToBoxAdapter(
-      child: Column(
-        children: [
-          Icon(
-            Icons.arrow_upward_rounded,
-            size: 50,
-          ),
-          Text("Quite empty here!"),
-          Text("Search and follow podcasts."),
-        ],
-      ),
-    );
-
-    // Helper function to build the grid
-    SliverGrid buildGrid(List<dynamic> podcasts) {
-      return SliverGrid.builder(
-        itemCount: podcasts.length,
-        gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
-          maxCrossAxisExtent: 200,
-          crossAxisSpacing: _spacing,
-          mainAxisSpacing: _spacing,
-        ),
-        itemBuilder: (BuildContext context, int index) {
-          return SubscribedPodcastCard(
-            podcast: podcasts[index],
-          );
+    return BlocListener<PodcastsBloc, PodcastsState>(
+        listener: (context, state) {
+          if (state is SubscribedPodcastsFetchSuccessState) {
+            Future.delayed(const Duration(seconds: 1),
+                () => navigateToSubscribedPodcastsPage());
+          }
         },
-      );
-    }
-
-    // Build the grid based on the state
-    if (state is FetchingSubscribedPodcastsState) {
-      return const SliverToBoxAdapter(
-        child: Center(child: CircularProgressIndicator()),
-      );
-    } else if (state is SubscribedPodcastsFetchSuccessState) {
-      return state.podcasts.isEmpty
-          ? emptyStateWidget
-          : buildGrid(state.podcasts);
-    } else if (state is PodcastsFetchSuccessState) {
-      // We handle this state here in case the user navigates back from the results page after a podcast search
-      // we still need to show the subscribed podcasts despite state is no more SubscribedPodcastsFetchSuccessState
-      return state.subscribedPodcasts.isEmpty
-          ? emptyStateWidget
-          : buildGrid(state.subscribedPodcasts);
-    } else if (state is PodcastFillWithEpisodesSuccessState) {
-      // We handle this state here in case the user navigates back from the episodes list page after a podcast search
-      // we still need to show the subscribed podcasts despite state is no more SubscribedPodcastsFetchSuccessState
-      return state.subscribedPodcasts.isEmpty
-          ? emptyStateWidget
-          : buildGrid(state.subscribedPodcasts);
-    } else if (state is PodcastChangeSubscriptionState) {
-      // We handle this state here in case the user navigates back from the episodes list page after he subscribes to a podcast
-      // we still need to show the subscribed podcasts despite state is no more SubscribedPodcastsFetchSuccessState
-      return state.subscribedPodcasts.isEmpty
-          ? emptyStateWidget
-          : buildGrid(state.subscribedPodcasts);
-    } else {
-      return const SliverToBoxAdapter(
-        child: Center(child: SizedBox()),
-      );
-    }
+        child: Center(
+          ///TODO: Replace with app logo
+          child: Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: Image.asset("assets/placeholder.png"),
+          ),
+        ));
   }
 }
