aa3725be555d1d6d761766903ee612a45e45adc0
bruno <1201ba@gmail.com>
2025-02-27 17:51:52 +0100

    ImageProvider is now a Future where image url is checked via http whether it's valid: it prevents 404 errors

--- lib/helpers/core/image_provider.dart
+++ lib/helpers/core/image_provider.dart
@@ -1,17 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 class MyImageProvider {
   final String url;
 
   MyImageProvider({required this.url});
 
-  ImageProvider get imageProvider {
-    try {
-      return url.isNotEmpty
-          ? NetworkImage(url)
-          : const AssetImage("assets/placeholder.png");
-    } catch (e) {
+  Future<ImageProvider> get imageProvider async {
+    if (url.isEmpty) {
       return const AssetImage("assets/placeholder.png");
     }
+
+    final response = await http.head(Uri.parse(url));
+
+    if (response.statusCode == 404) {
+      return const AssetImage("assets/placeholder.png");
+    }
+
+    if (response.statusCode != 200) {
+        // Handle other non-200 status codes (e.g., 500) if needed
+        return const AssetImage("assets/placeholder.png");
+      }
+
+    return NetworkImage(url);
+
   }
+
 }
