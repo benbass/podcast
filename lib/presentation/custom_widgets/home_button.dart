d3211a6ac2721266edbd62952bfe6ea339449820
bruno <1201ba@gmail.com>
2025-02-28 15:46:07 +0100

    Add Home Button widget

---
+++ lib/presentation/custom_widgets/home_button.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:podcast/presentation/custom_widgets/page_transition.dart';
+
+import '../subscribed_podcasts/subscribed_podcasts_page.dart';
+
+class MyHomeButton extends StatelessWidget {
+  const MyHomeButton({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return IconButton(
+      onPressed: () {
+        Navigator.push(
+          context,
+          SlideRouteWithCurve(
+            page: const SubscribedPodcastsPage(),
+          ),
+        );
+      },
+      icon: const Icon(Icons.home_rounded),
+    );
+  }
+}
