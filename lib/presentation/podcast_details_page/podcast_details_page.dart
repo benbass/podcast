50c2aacce4a42cb995df243be62b9ca225d52cad
bruno <1201ba@gmail.com>
2025-03-03 13:28:39 +0100

    Add loading indicator in Podcast Details Page

--- lib/presentation/podcast_details_page/podcast_details_page.dart
+++ lib/presentation/podcast_details_page/podcast_details_page.dart
@@ -7,7 +7,6 @@ import '../../application/podcast_bloc/podcast_bloc.dart';
 import '../custom_widgets/flexible_space.dart';
 
 class PodcastDetailsPage extends StatelessWidget {
-
   const PodcastDetailsPage({super.key});
 
   @override
@@ -15,83 +14,93 @@ class PodcastDetailsPage extends StatelessWidget {
     return Scaffold(
       body: BlocBuilder<PodcastBloc, PodcastState>(
         builder: (context, state) {
-          return SafeArea(
-            child: CustomScrollView(
-              slivers: [
-                FlexibleSpace(
-                  podcast: state.podcast,
-                  episode: null,
-                  title: state.podcast!.title,
-                ),
-                SliverPadding(
-                    padding: const EdgeInsets.fromLTRB(10.0, 0.0, 20.0, 10.0),
-                    sliver: SliverToBoxAdapter(
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.start,
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          ...state.podcast!.categories.map((value) =>
-                              Categories(
-                                value: value,
-                              )),
-                        ],
+          return state.loading
+              ? const Center(
+                  child: CircularProgressIndicator(),
+                )
+              : SafeArea(
+                  child: CustomScrollView(
+                    slivers: [
+                      FlexibleSpace(
+                        podcast: state.podcast,
+                        episode: null,
+                        title: state.podcast!.title,
                       ),
-                    )),
-                SliverPadding(
-                  padding: const EdgeInsets.fromLTRB(40.0, 10.0, 40.0, 10.0),
-                  sliver: SliverToBoxAdapter(
-                    child: RowIconButtonsPodcasts(podcast: state.podcast!),
-                  ),
-                ),
-                SliverPadding(
-                  padding: const EdgeInsets.fromLTRB(20.0, 0.0, 20.0, 20.0),
-                  sliver: SliverToBoxAdapter(
-                    child: Text(
-                      state.podcast!.description,
-                      style: const TextStyle(
-                        fontSize: 16,
+                      SliverPadding(
+                          padding:
+                              const EdgeInsets.fromLTRB(10.0, 0.0, 20.0, 10.0),
+                          sliver: SliverToBoxAdapter(
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.start,
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                ...state.podcast!.categories
+                                    .map((value) => Categories(
+                                          value: value,
+                                        )),
+                              ],
+                            ),
+                          )),
+                      SliverPadding(
+                        padding:
+                            const EdgeInsets.fromLTRB(40.0, 10.0, 40.0, 10.0),
+                        sliver: SliverToBoxAdapter(
+                          child:
+                              RowIconButtonsPodcasts(podcast: state.podcast!),
+                        ),
                       ),
-                    ),
-                  ),
-                ),
-                SliverPadding(
-                  padding: const EdgeInsets.fromLTRB(20.0, 0.0, 20.0, 100.0),
-                  sliver: SliverToBoxAdapter(
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(
-                          state.podcast!.author,
-                          style: const TextStyle(
-                            fontSize: 16,
+                      SliverPadding(
+                        padding:
+                            const EdgeInsets.fromLTRB(20.0, 0.0, 20.0, 20.0),
+                        sliver: SliverToBoxAdapter(
+                          child: Text(
+                            state.podcast!.description,
+                            style: const TextStyle(
+                              fontSize: 16,
+                            ),
                           ),
                         ),
-                        Padding(
-                          padding: const EdgeInsets.symmetric(vertical: 10.0),
-                          child: Row(
+                      ),
+                      SliverPadding(
+                        padding:
+                            const EdgeInsets.fromLTRB(20.0, 0.0, 20.0, 100.0),
+                        sliver: SliverToBoxAdapter(
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
                             children: [
-                              const Icon(
-                                Icons.language,
-                              ),
-                              const SizedBox(
-                                width: 10,
-                              ),
                               Text(
-                                state.podcast!.language.toUpperCase(),
+                                state.podcast!.author,
                                 style: const TextStyle(
                                   fontSize: 16,
+                                ),
+                              ),
+                              Padding(
+                                padding:
+                                    const EdgeInsets.symmetric(vertical: 10.0),
+                                child: Row(
+                                  children: [
+                                    const Icon(
+                                      Icons.language,
+                                    ),
+                                    const SizedBox(
+                                      width: 10,
+                                    ),
+                                    Text(
+                                      state.podcast!.language.toUpperCase(),
+                                      style: const TextStyle(
+                                        fontSize: 16,
+                                      ),
+                                    ),
+                                  ],
                                 ),
                               ),
                             ],
                           ),
                         ),
-                      ],
-                    ),
+                      ),
+                    ],
                   ),
-                ),
-              ],
-            ),
-          );
+                );
         },
       ),
     );
