cc5d20434deab2e34403f9efa51ee4fc463e5dd2
bruno <1201ba@gmail.com>
2025-03-03 13:19:53 +0100

    Update Subscribe Button logic, navigate to Subscribed page and wrap Text widget in BlocBuilder

--- lib/presentation/custom_widgets/elevated_button_subscribe.dart
+++ lib/presentation/custom_widgets/elevated_button_subscribe.dart
@@ -3,14 +3,17 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:podcast/domain/entities/podcast_entity.dart';
 import 'package:podcast/presentation/custom_widgets/page_transition.dart';
 import '../../application/podcast_bloc/podcast_bloc.dart';
-import '../homepage/homepage.dart';
+import '../subscribed_podcasts/subscribed_podcasts_page.dart';
 
 class ElevatedButtonSubscribe extends StatelessWidget {
   final PodcastEntity podcast;
   final bool navigate;
 
-  const ElevatedButtonSubscribe(
-      {super.key, required this.podcast, required this.navigate});
+  const ElevatedButtonSubscribe({
+    super.key,
+    required this.podcast,
+    required this.navigate,
+  });
 
   @override
   Widget build(BuildContext context) {
@@ -18,19 +21,23 @@ class ElevatedButtonSubscribe extends StatelessWidget {
       onPressed: () {
         !podcast.subscribed
             ? BlocProvider.of<PodcastBloc>(context)
-                .add(SubscribeToPodcastEvent(podcast: podcast))
+                .add(SubscribeToPodcastEvent())
             : BlocProvider.of<PodcastBloc>(context)
-                .add(UnSubscribeFromPodcastEvent(podcast: podcast));
+                .add(UnSubscribeFromPodcastEvent());
         if (navigate) {
           Navigator.push(
             context,
             SlideRouteWithCurve(
-              page: const HomePage(),
+              page: const SubscribedPodcastsPage(),
             ),
           );
         }
       },
-      child: Text(podcast.subscribed ? "Unsubscribe" : "Subscribe"),
+      child: BlocBuilder<PodcastBloc, PodcastState>(
+        builder: (context, state) {
+          return Text(state.podcast!.subscribed ? "Unsubscribe" : "Subscribe");
+        },
+      ),
     );
   }
 }
