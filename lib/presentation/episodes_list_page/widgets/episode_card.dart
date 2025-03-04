2733a1efc8bc2e00f5f992a069445211bae2b1ce
bruno <1201ba@gmail.com>
2025-03-04 16:54:32 +0100

    Update the visibility of the favorite icon in the episode card.

--- lib/presentation/episodes_list_page/widgets/episode_card.dart
+++ lib/presentation/episodes_list_page/widgets/episode_card.dart
@@ -27,109 +27,113 @@ class EpisodeCard extends StatelessWidget {
               ? snapshot.data!
               : const AssetImage('assets/placeholder.png');
           return Card(
-          key: ValueKey(item.eId),
-          color: themeData.colorScheme.primaryContainer,
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(10.0),
-          ),
-          elevation: 5.0,
-          shadowColor: Colors.black,
-          margin: const EdgeInsets.all(8.0),
-          clipBehavior: Clip.antiAlias,
-          child: SizedBox(
-            height: 90.0,
-            child: InkWell(
-              splashColor: Colors.black87,
-              onTap: () async {
-                Navigator.push(
-                  context,
-                  SizeRoute(
-                    page: EpisodeSelectedPage(
-                      episode: item,
-                      podcast: podcast,
-                    ),
-                  ),
-                );
-              },
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.only(right: 8.0),
-                    child: Stack(
-                      children: [
-                        Container(
-                          width: 90,
-                          decoration: BoxDecoration(
-                            image: DecorationImage(
-                              image: imageProvider,
-                              fit: BoxFit.fitWidth,
-                            ),
-                          ),
-                        ),
-                        if(item.position > 0)
-                        Positioned(
-                          top: 0,
-                          left: 0,
-                          child: SizedBox(
-                            height: 90,
-                            width: 90,
-                            child: LinearProgressIndicator(
-                              value: (item.position.toDouble()/item.duration!.toDouble()).clamp(0.0, 1.0),
-                              color: themeData.colorScheme.primary.withValues(alpha: 0.6),
-                              backgroundColor: Colors.transparent,
-                            ),
-                          ),
-                        )
-                      ],
-                    ),
-                  ),
-                  Expanded(
-                    child: Padding(
-                      padding: const EdgeInsets.fromLTRB(
-                        6.0,
-                        10.0,
-                        8.0,
-                        10.0,
+            key: ValueKey(item.eId),
+            color: themeData.colorScheme.primaryContainer,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(10.0),
+            ),
+            elevation: 5.0,
+            shadowColor: Colors.black,
+            margin: const EdgeInsets.all(8.0),
+            clipBehavior: Clip.antiAlias,
+            child: SizedBox(
+              height: 90.0,
+              child: InkWell(
+                splashColor: Colors.black87,
+                onTap: () async {
+                  Navigator.push(
+                    context,
+                    SizeRoute(
+                      page: EpisodeSelectedPage(
+                        episode: item,
+                        podcast: podcast,
                       ),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    ),
+                  );
+                },
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.only(right: 8.0),
+                      child: Stack(
                         children: [
-                          Text(
-                            item.title,
-                            overflow: TextOverflow.ellipsis,
-                            softWrap: true,
-                            maxLines: 2,
-                            style: Theme.of(context).textTheme.displayMedium,
-                          ),
-                          Text(
-                            formatTimestamp(
-                              item.datePublished,
+                          Container(
+                            width: 90,
+                            decoration: BoxDecoration(
+                              image: DecorationImage(
+                                image: imageProvider,
+                                fit: BoxFit.fitWidth,
+                              ),
                             ),
-                            style: Theme.of(context).textTheme.displayMedium,
                           ),
+                          if (item.position > 0)
+                            Positioned(
+                              top: 0,
+                              left: 0,
+                              child: SizedBox(
+                                height: 90,
+                                width: 90,
+                                child: LinearProgressIndicator(
+                                  value: (item.position.toDouble() /
+                                          item.duration!.toDouble())
+                                      .clamp(0.0, 1.0),
+                                  color: themeData.colorScheme.primary
+                                      .withValues(alpha: 0.6),
+                                  backgroundColor: Colors.transparent,
+                                ),
+                              ),
+                            )
                         ],
                       ),
                     ),
-                  ),
-                   Padding(
-                     padding: const EdgeInsets.only(right: 8.0),
-                     child: Column(
-                       mainAxisAlignment: MainAxisAlignment.spaceAround,
-                      children: [
-                        Icon(item.read ? Icons.check_rounded : null),
-                        Icon(item.favorite ? Icons.favorite_rounded : Icons.favorite_border_rounded),
-                      ],
-                                   ),
-                   )
-                ],
+                    Expanded(
+                      child: Padding(
+                        padding: const EdgeInsets.fromLTRB(
+                          6.0,
+                          10.0,
+                          8.0,
+                          10.0,
+                        ),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Text(
+                              item.title,
+                              overflow: TextOverflow.ellipsis,
+                              softWrap: true,
+                              maxLines: 2,
+                              style: Theme.of(context).textTheme.displayMedium,
+                            ),
+                            Text(
+                              formatTimestamp(
+                                item.datePublished,
+                              ),
+                              style: Theme.of(context).textTheme.displayMedium,
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(right: 8.0),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.spaceAround,
+                        children: [
+                          Icon(item.read ? Icons.check_rounded : null),
+                          Icon(item.favorite && podcast.subscribed
+                              ? Icons.favorite_rounded
+                              : null),
+                        ],
+                      ),
+                    )
+                  ],
+                ),
               ),
             ),
-          ),
-        );
-      }
-    );
+          );
+        });
   }
 }
