0f788cc6f8db3dc450d5356b0785465fdeb8a4b6
bruno <1201ba@gmail.com>
2025-03-04 11:55:58 +0100

    Refactor search results view and add navigation to details page.

--- lib/presentation/podcasts_search_page/podcasts_search_page.dart
+++ lib/presentation/podcasts_search_page/podcasts_search_page.dart
@@ -4,6 +4,8 @@ import 'package:podcast/presentation/custom_widgets/home_button.dart';
 import 'package:podcast/presentation/podcasts_search_page/widgets/podcast_card.dart';
 
 import '../../application/podcast_bloc/podcast_bloc.dart';
+import '../custom_widgets/page_transition.dart';
+import '../podcast_details_page/podcast_details_page.dart';
 import 'widgets/search_textfield.dart';
 
 class PodcastsSearchPage extends StatelessWidget {
@@ -27,69 +29,78 @@ class PodcastsSearchPage extends StatelessWidget {
 
   AppBar _buildAppBar(BuildContext context) {
     return AppBar(
-      title: const Padding(
-        padding: EdgeInsets.symmetric(vertical: 20.0),
-        child: SearchTextField(),
-      ),
-      leading: const MyHomeButton()
-    );
+        title: const Padding(
+          padding: EdgeInsets.symmetric(vertical: 20.0),
+          child: SearchTextField(),
+        ),
+        leading: const MyHomeButton());
   }
 
   Widget _buildBody(BuildContext context, PodcastState state) {
-    return CustomScrollView(
-      physics: const BouncingScrollPhysics(),
-      slivers: [
-        if (state.loading)
-          _buildLoadingIndicator(context)
-        else
-          _buildPodcastList(context, state)
-      ],
+    return Center(
+      child: ConstrainedBox(
+        constraints: const BoxConstraints.expand(),
+        child: state.loading
+            ? _buildLoadingIndicator(context)
+            : _buildPodcastList(context, state),
+      ),
     );
   }
 
   Widget _buildLoadingIndicator(BuildContext context) {
-    return SliverToBoxAdapter(
-      child: SizedBox(
-        height: MediaQuery.of(context).size.height,
-        child: const Center(
-          child: CircularProgressIndicator(),
-        ),
-      ),
+    return const Center(
+      child: CircularProgressIndicator(),
     );
   }
 
   Widget _buildPodcastList(BuildContext context, PodcastState state) {
     if (state.keyword == null) {
-      return _buildEmptyList(context, "Enter keyword(s) to\nsearch for podcasts");
+      return _buildEmptyList(
+          context, "Enter keyword(s) to\nsearch for podcasts");
     }
     if (state.podcastsQueryResult.isEmpty && state.keyword != null) {
       return _buildEmptyList(context, "No podcasts found\nfor your keyword(s)");
     }
-    return SliverPadding(
-      padding: const EdgeInsets.only(bottom: 80.0),
-      sliver: SliverList.builder(
-        itemCount: state.podcastsQueryResult.length,
-        itemBuilder: (context, index) {
+    return CarouselView(
+      itemExtent: MediaQuery.of(context).size.width ,
+      scrollDirection: Axis.vertical,
+      itemSnapping: true,
+      padding: const EdgeInsets.all(10.0),
+      backgroundColor: Colors.transparent,//Theme.of(context).colorScheme.secondary,
+      elevation: 3,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(10.0),
+        side: const BorderSide(
+          color: Colors.transparent,
+          width: 0.0,
+        ),
+      ),
+      onTap: (index) {
+        BlocProvider.of<PodcastBloc>(context)
+            .add(PodcastTappedEvent(podcast: state.podcastsQueryResult[index]));
+        Navigator.push(
+          context,
+          SizeRoute(
+            page: const PodcastDetailsPage(),
+          ),
+        );
+      },
+      children: List.generate(
+        state.podcastsQueryResult.length,
+        (index) {
           final podcast = state.podcastsQueryResult[index];
-          return BlocBuilder<PodcastBloc, PodcastState>(
-            builder: (context, state) {
-              return PodcastCard(podcast: podcast);
-            },
-          );
+          return PodcastCard(podcast: podcast);
         },
       ),
     );
   }
 
   Widget _buildEmptyList(BuildContext context, String text) {
-    return SliverFillRemaining(
-      hasScrollBody: false,
-      child: Center(
-        child: Text(
-          text,
-          style: const TextStyle(fontSize: 20, height: 1.7),
-          textAlign: TextAlign.center,
-        ),
+    return Center(
+      child: Text(
+        text,
+        style: const TextStyle(fontSize: 20, height: 1.7),
+        textAlign: TextAlign.center,
       ),
     );
   }
