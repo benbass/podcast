029d2cb0c99f0385fd58c1ab0a68da5eb35286b0
bruno <1201ba@gmail.com>
2025-03-07 17:49:01 +0100

    Listen to audio processing state and clear episode data on completion

--- lib/presentation/homepage/homepage.dart
+++ lib/presentation/homepage/homepage.dart
@@ -1,5 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:just_audio/just_audio.dart';
 
+import '../../application/episode_playback/episode_playback_cubit.dart';
+import '../../helpers/player/audiohandler.dart';
+import '../../injection.dart';
+import '../audioplayer_overlays/audioplayer_overlays.dart';
 import '../custom_widgets/page_transition.dart';
 import '../subscribed_podcasts/subscribed_podcasts_page.dart';
 
@@ -19,12 +25,24 @@ class HomePage extends StatelessWidget {
     Future.delayed(
         const Duration(seconds: 1), () => navigateToSubscribedPodcastsPage());
 
-    return Center(
-      ///TODO: Replace with app logo
-      child: Padding(
-        padding: const EdgeInsets.all(20.0),
-        child: Image.asset("assets/placeholder.png"),
-      ),
-    );
+    // We wrap the app in a just_audio stream builder so we can listen to player states whatever page is called
+    return StreamBuilder<ProcessingState>(
+        stream: getItI<MyAudioHandler>().myPlayerProcessingStateStream,
+        builder: (context, snapshot) {
+          final processingState = snapshot.data;
+          if (processingState == ProcessingState.completed) {
+            getItI<MyAudioHandler>().stop();
+            removeOverlay();
+            BlocProvider.of<EpisodePlaybackCubit>(context)
+                .setPlaybackEpisode(null);
+          }
+          return Center(
+            ///TODO: Replace with app logo
+            child: Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: Image.asset("assets/placeholder.png"),
+            ),
+          );
+        });
   }
 }
