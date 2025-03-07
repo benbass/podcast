70ec09ef8d0e0c46bdf34641f767b1215b0f4246
bruno <1201ba@gmail.com>
2025-03-07 17:53:23 +0100

    Refactor play button to use EpisodePlaybackCubit and update its logic.

--- lib/presentation/custom_widgets/play_button.dart
+++ lib/presentation/custom_widgets/play_button.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:just_audio/just_audio.dart';
 
-import '../../application/episode_playback_url/episode_playback_url_cubit.dart';
+import '../../application/episode_playback/episode_playback_cubit.dart';
 import '../../domain/entities/episode_entity.dart';
 import '../../domain/entities/podcast_entity.dart';
 import '../../helpers/player/audiohandler.dart';
@@ -23,65 +23,70 @@ class PlayButton extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return BlocBuilder<EpisodePlaybackUrlCubit, String>(
+    return BlocBuilder<EpisodePlaybackCubit, EpisodeEntity?>(
       builder: (context, state) {
-        if (state != episode.enclosureUrl) {
-          return StreamBuilder<PlayerState>(
-              stream: getItI<MyAudioHandler>()
-                  .player
-                  .playerStateStream,
-              builder: (context, stream) {
-                final isPlaying = stream.data?.playing;
-
-                return IconButton(
-                  onPressed: () async {
-                    if (isPlaying == true) {
-                      // Only if player is playing we have an overlay (from current playback) that we can remove
-                      removeOverlay();
-                    }
-
-                    // source error?
-                    try {
-                      await getItI<MyAudioHandler>()
-                          .player
-                          .setUrl(episode.enclosureUrl);
-                      getItI<MyAudioHandler>().play();
-
-                      // We need to wait for the previous overlayEntry, if any, to be removed before we can insert the new one
-                      Future.delayed(
-                          const Duration(seconds: 2));
-                      if (context.mounted) {
-                        BlocProvider.of<
-                            EpisodePlaybackUrlCubit>(
-                            context)
-                            .setPlaybackEpisodeUrl(
-                            episode.enclosureUrl);
-                        showOverlayPlayerMin(context,
-                            episode, podcast, podcastTitle);
-                      }
-                    } on PlayerException {
-                      if (context.mounted) {
-                        showOverlayError(context,
-                            "Error: No valid file exists under the requested url.");
-                      }
-                    }
-                  },
-                  padding: EdgeInsets.zero,
-                  constraints: const BoxConstraints(),
-                  icon: const Icon(
-                    Icons.play_arrow_rounded,
-                    size: 80,
-                  ),
-                );
-              });
+        if (state != episode) {
+          return PlayButtonActive(episode: episode);
         } else {
-          return const Icon(
-            Icons.play_arrow_rounded,
-            color: Colors.white24,
-            size: 80,
-          );
+          return const PlayButtonInactive();
         }
       },
+    );
+  }
+}
+
+class PlayButtonInactive extends StatelessWidget {
+  const PlayButtonInactive({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return const Icon(
+      Icons.play_arrow_rounded,
+      color: Colors.white24,
+      size: 80,
+    );
+  }
+}
+
+class PlayButtonActive extends StatelessWidget {
+  const PlayButtonActive({
+    super.key,
+    required this.episode,
+  });
+
+  final EpisodeEntity episode;
+
+  @override
+  Widget build(BuildContext context) {
+    return IconButton(
+      onPressed: () async {
+        // source error?
+        try {
+          await getItI<MyAudioHandler>()
+              .player
+              .setUrl(episode.enclosureUrl);
+          getItI<MyAudioHandler>().play();
+
+          if (context.mounted) {
+            BlocProvider.of<EpisodePlaybackCubit>(context)
+                .setPlaybackEpisode(episode);
+            //showOverlayPlayerMin(context, episode, podcast, podcastTitle);
+          }
+        } on PlayerException {
+          if (context.mounted) {
+            showOverlayError(context,
+                "Error: No valid file exists under the requested url.");
+          }
+        }
+      },
+      padding: EdgeInsets.zero,
+      constraints: const BoxConstraints(),
+      icon: const Icon(
+        Icons.play_arrow_rounded,
+        size: 80,
+      ),
     );
   }
 }
