196e19ed730326a60197c30a9672d62aefab36dd
bruno <1201ba@gmail.com>
2025-03-13 15:03:15 +0100

    Add a fake episode for testing.

--- lib/core/globals.dart
+++ lib/core/globals.dart
@@ -5,3 +5,33 @@ import 'package:podcast/domain/entities/podcast_entity.dart';
 const String baseUrl = 'https://api.podcastindex.org/api/1.0';
 late Box<PodcastEntity> podcastBox;
 late Box<EpisodeEntity> episodeBox;
+
+/// For testing: create a fake episode
+DateTime now = DateTime.now();
+int secondsSinceEpochUtc = now.millisecondsSinceEpoch ~/ 1000;
+EpisodeEntity fakeEpisode = EpisodeEntity(
+  eId: 9009090100,
+  title: "FAKE EPISODE",
+  description: "description",
+  guid: "00098",
+  datePublished: secondsSinceEpochUtc,
+  datePublishedPretty: "null",
+  enclosureUrl: "enclosureUrl",
+  enclosureLength: 1994398,
+  duration: 213242432,
+  explicit: 0,
+  episodeNr: 0,
+  episodeType: null,
+  season: null,
+  image: "",
+  feedUrl: "feedUrl",
+  link: "link",
+  feedImage: "",
+  feedId: 202500000000, // podcast.pId,
+  podcastGuid: "abcdefghFake", // podcast.podcastGuid,
+  favorite: true,
+  read: false,
+  completed: false,
+  position: 09789798,
+  filePath: null,
+);
