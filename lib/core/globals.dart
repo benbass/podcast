2dd2669c4516d29cf3795a98aeb55e56b2d8d384
bruno <1201ba@gmail.com>
2025-03-15 13:49:33 +0100

    Initialize ObjectBox as global

--- lib/core/globals.dart
+++ lib/core/globals.dart
@@ -2,7 +2,10 @@ import 'package:objectbox/objectbox.dart';
 import 'package:podcast/domain/entities/episode_entity.dart';
 import 'package:podcast/domain/entities/podcast_entity.dart';
 
+import 'objectbox.dart';
+
 const String baseUrl = 'https://api.podcastindex.org/api/1.0';
+late ObjectBox objectBox;
 late Box<PodcastEntity> podcastBox;
 late Box<EpisodeEntity> episodeBox;
 
