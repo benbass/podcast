3b9f4400074b2eef58984314c74deed2f526f2d8
bruno <1201ba@gmail.com>
2025-03-16 15:29:11 +0100

    Refactor episode loading with stream builder
    The episode list is now loaded from a stream, which handles loading and error states.
    The stream fetches data from the database or the remote server depending on if the podcast is subscribed.

--- lib/presentation/episodes_list_page/episodes_list_page.dart
+++ lib/presentation/episodes_list_page/episodes_list_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:podcast/core/globals.dart';
 import 'package:podcast/domain/entities/episode_entity.dart';
 
 import 'package:podcast/presentation/custom_widgets/elevated_button_subscribe.dart';
@@ -17,88 +18,121 @@ class EpisodesListPage extends StatelessWidget {
     return Scaffold(
       body: BlocBuilder<PodcastBloc, PodcastState>(
         builder: (context, state) {
-          // We make sure to sort episodes by datePublished in descending order
-          // because database returns objects by id in ascending order.
-          final List<EpisodeEntity> episodes = state.podcast != null ? (state.podcast!.episodes..sort((a, b) => b.datePublished.compareTo(a.datePublished)) ): [];
           return SafeArea(
             child: state.loading
                 ? const Center(child: CircularProgressIndicator())
-                : episodes.isNotEmpty
-                    ? CustomScrollView(
-                        physics: const BouncingScrollPhysics(),
-                        slivers: [
-                          SliverAppBar(
-                            collapsedHeight: 60,
-                            expandedHeight: 170,
-                            pinned: true,
-                            flexibleSpace: FlexibleSpaceBar(
-                              background: Column(
-                                mainAxisAlignment:
-                                    MainAxisAlignment.spaceBetween,
-                                spacing: 12,
+                : CustomScrollView(
+                    physics: const BouncingScrollPhysics(),
+                    slivers: [
+                      SliverAppBar(
+                        collapsedHeight: 60,
+                        expandedHeight: 170,
+                        pinned: true,
+                        flexibleSpace: FlexibleSpaceBar(
+                          background: Column(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            spacing: 12,
+                            children: [
+                              Padding(
+                                padding: const EdgeInsets.symmetric(
+                                    horizontal: 80.0),
+                                child: Text(
+                                  state.podcast!.title,
+                                  style:
+                                      Theme.of(context).textTheme.displayLarge,
+                                  maxLines: 1,
+                                  overflow: TextOverflow.ellipsis,
+                                ),
+                              ),
+                              Row(
+                                mainAxisAlignment: MainAxisAlignment.center,
                                 children: [
-                                  Padding(
-                                    padding: const EdgeInsets.symmetric(
-                                        horizontal: 80.0),
-                                    child: Text(
-                                      state.podcast!.title,
-                                      style: Theme.of(context)
-                                          .textTheme
-                                          .displayLarge,
-                                      maxLines: 1,
-                                      overflow: TextOverflow.ellipsis,
-                                    ),
+                                  ElevatedButtonSubscribe(
+                                    navigate: true,
+                                    podcast: state.podcast!,
                                   ),
-                                  Row(
-                                    mainAxisAlignment: MainAxisAlignment.center,
-                                    children: [
-                                      ElevatedButtonSubscribe(
-                                        navigate: true, podcast: state.podcast!,
-                                      ),
-                                      const SizedBox(
-                                        width: 30,
-                                      ),
-                                      IconButton(
-                                        onPressed: () => Navigator.push(
-                                            context,
-                                            ScaleRoute(
-                                              page: const PodcastDetailsPage(),
-                                            )),
-                                        icon: const Icon(
-                                          Icons.info_outline_rounded,
-                                          size: 30,
-                                        ),
-                                      ),
-                                    ],
-                                  ),
-                                  const RowIconButtonsEpisodes(),
                                   const SizedBox(
-                                    height: 12,
+                                    width: 30,
+                                  ),
+                                  IconButton(
+                                    onPressed: () => Navigator.push(
+                                        context,
+                                        ScaleRoute(
+                                          page: const PodcastDetailsPage(),
+                                        )),
+                                    icon: const Icon(
+                                      Icons.info_outline_rounded,
+                                      size: 30,
+                                    ),
                                   ),
                                 ],
                               ),
-                            ),
+                              const RowIconButtonsEpisodes(),
+                              const SizedBox(
+                                height: 12,
+                              ),
+                            ],
                           ),
-                          SliverList.builder(
-                            itemCount: episodes.length,
-                            itemBuilder: (context, index) {
-                              final item = episodes[index];
-                              return EpisodeCard(
-                                episode: item,
-                                podcast: state.podcast!,
-                              );
-                            },
-                          ),
-                          const SliverToBoxAdapter(
-                            child: SizedBox(
-                              height: 80,
-                            ),
-                          ),
-                        ],
-                      )
-                    : const Center(
-                        child: Text("This podcast has no episodes"),
+                        ),
                       ),
+                      StreamBuilder<List<EpisodeEntity>>(
+                        // The method getEpisodes checks the subscribed flag of
+                        // the podcast and returns the correct stream
+                        // (from objectBox database or from the remote server)
+                          stream: objectBox.getEpisodes(podcast: state.podcast!, onlyUnread: !state.areReadEpisodesVisible),
+                          builder: (context, snapshot) {
+                            if (snapshot.connectionState ==
+                                ConnectionState.waiting) {
+                              return const SliverFillRemaining(
+                                child: Center(
+                                  child: CircularProgressIndicator(),
+                                ),
+                              );
+                            }
+                            if (snapshot.hasError) {
+                              // Print the stack trace and show the error message.
+                              // An actual app would display a user-friendly error message
+                              // and report the error behind the scenes.
+                              debugPrintStack(stackTrace: snapshot.stackTrace);
+                              return SliverToBoxAdapter(
+                                child: Column(
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  children: [
+                                    const Icon(
+                                      Icons.error_outline,
+                                      color: Colors.red,
+                                      size: 60,
+                                    ),
+                                    Padding(
+                                      padding: const EdgeInsets.only(
+                                          top: 16, left: 16, right: 16),
+                                      child: Text('Error: ${snapshot.error}'),
+                                    ),
+                                  ],
+                                ),
+                              );
+                            } else {
+                              return SliverPadding(
+                                padding: const EdgeInsets.only(bottom: 80.0),
+                                sliver: SliverList.builder(
+                                  itemCount: snapshot.hasData
+                                      ? snapshot.data!.length
+                                      : 0,
+                                  itemBuilder: (context, index) {
+                                    List<EpisodeEntity> episodes =
+                                        snapshot.data ?? [];
+                                    final item = episodes[index];
+                                    return EpisodeCard(
+                                      episode: item,
+                                      podcast: state.podcast!,
+                                    );
+                                  },
+                                ),
+                              );
+                            }
+                          }),
+                    ],
+                  ),
           );
         },
       ),
