ce1e123e91b573b516d3c2c1dfbc9c750db9357f
bruno <1201ba@gmail.com>
2025-03-16 15:21:20 +0100

    Refactor episode fetching to support unread filter and remote source

--- lib/core/objectbox.dart
+++ lib/core/objectbox.dart
@@ -2,6 +2,9 @@ import 'package:path/path.dart' as p;
 import 'package:path_provider/path_provider.dart';
 import 'package:podcast/core/globals.dart';
 import '../domain/entities/episode_entity.dart';
+import '../domain/entities/podcast_entity.dart';
+import '../infrastructure/datasources/episode_datasources.dart';
+import '../injection.dart';
 import '../objectbox.g.dart'; // created by `flutter pub run build_runner build`
 
 class ObjectBox {
@@ -20,9 +23,31 @@ class ObjectBox {
     return ObjectBox._create(store);
   }
 
-  Stream<List<EpisodeEntity>> getEpisodes(int feedId) {
-    final qBuilderEpisodes = episodeBox.query(EpisodeEntity_.feedId.equals(feedId)).order(EpisodeEntity_.datePublished, flags: Order.descending);
-    return qBuilderEpisodes.watch(triggerImmediately: true).map((query) => query.find());
+  Stream<List<EpisodeEntity>> getEpisodes({required PodcastEntity podcast, required bool onlyUnread}){
+    if(podcast.subscribed){
+      if(onlyUnread) {
+        return getLocalUnreadEpisodesByFeedId(podcast.pId);
+      } else {
+        return getLocalEpisodesByFeedId(podcast.pId);
+      }
+    } else{
+      return getItI<EpisodeDataSources>().fetchEpisodesAsStreamByFeedId(podcast.pId);
+    }
+  }
+
+  Stream<List<EpisodeEntity>> getLocalUnreadEpisodesByFeedId(int feedId) {
+    final queryBuilder = episodeBox.query(EpisodeEntity_.feedId.equals(feedId).and(EpisodeEntity_.read.equals(false)))..order(EpisodeEntity_.datePublished, flags: Order.descending);
+    return queryBuilder.watch(triggerImmediately: true).map((query) => query.find());
+  }
+
+  Stream<List<EpisodeEntity>> getLocalEpisodesByFeedId(int feedId) {
+    final queryBuilder = episodeBox.query(EpisodeEntity_.feedId.equals(feedId))..order(EpisodeEntity_.datePublished, flags: Order.descending);
+    return queryBuilder.watch(triggerImmediately: true).map((query) => query.find());
+  }
+
+  Stream<int> unreadEpisodesCount({required int feedId}){
+    final queryBuilder = episodeBox.query(EpisodeEntity_.feedId.equals(feedId).and(EpisodeEntity_.read.equals(false)));
+    return queryBuilder.watch(triggerImmediately: true).map((query) => query.count());
   }
 
   EpisodeEntity getEpisode(int id) => episodeBox.get(id)!;
