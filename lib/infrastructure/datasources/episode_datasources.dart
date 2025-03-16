681201ec42dfcb8a93afeb923b5974caca5305fb
bruno <1201ba@gmail.com>
2025-03-16 15:24:39 +0100

    Refactor episode data source to fetch episodes as stream only
    - Removes the `fetchEpisodesByFeedId` method.
    - Updates `fetchEpisodesAsStreamByFeedId` to accept `feedId` instead of `id`.
    - Updates `fetchEpisodesAsStreamByFeedId` to print an error log in case of failure.

--- lib/infrastructure/datasources/episode_datasources.dart
+++ lib/infrastructure/datasources/episode_datasources.dart
@@ -8,8 +8,7 @@ import '../../helpers/authorization/authorization.dart';
 import '../models/episode_model.dart';
 
 abstract class EpisodeDataSources {
-  Stream<List<EpisodeEntity>> fetchEpisodesAsStreamByFeedId(int id);
-  Future<List<EpisodeEntity>> fetchEpisodesByFeedId(int id);
+  Stream<List<EpisodeEntity>> fetchEpisodesAsStreamByFeedId(int feedId);
 }
 
 class EpisodeDataSourcesImpl implements EpisodeDataSources {
@@ -17,45 +16,23 @@ class EpisodeDataSourcesImpl implements EpisodeDataSources {
 
   EpisodeDataSourcesImpl({required this.httpClient});
   @override
-  Stream<List<EpisodeEntity>> fetchEpisodesAsStreamByFeedId(int id) async* {
+  Stream<List<EpisodeEntity>> fetchEpisodesAsStreamByFeedId(int feedId) async* {
     // Authorization:
     Map<String, String> headers = headersForAuth(); // this is the real auth
 
-    final response = await httpClient.get(
-        Uri.parse('$baseUrl/episodes/byfeedid?id=$id&pretty&max=1000'),
-        headers: headers);
+    final Uri uri =
+        Uri.parse('$baseUrl/episodes/byfeedid?id=$feedId&pretty&max=1000');
+
+    final response = await httpClient.get(uri, headers: headers);
     if (response.statusCode == 200) {
       var jsonItems = json.decode(response.body);
       List<EpisodeEntity> episodes = List<EpisodeEntity>.from(
           jsonItems['items'].map((x) => EpisodeModel.fromJson(x)));
-      print("episode datasource fetchEpisodesAsStreamByFeedId: ${episodes.length}"); // OK
 
       yield episodes;
     } else {
-      // If the server did not return a 200 OK response,
-      // then throw an exception.
-      throw Exception('Failed to load episodes');
-    }
-  }
-
-  @override
-  Future<List<EpisodeEntity>> fetchEpisodesByFeedId(int id) async {
-    // Authorization:
-    Map<String, String> headers = headersForAuth(); // this is the real auth
-
-    final Uri uri = Uri.parse('$baseUrl/episodes/byfeedid?id=$id&pretty&max=1000');
-
-    final response = await httpClient.get(
-        uri,
-        headers: headers);
-    if (response.statusCode == 200) {
-      var jsonItems = json.decode(response.body);
-      List<EpisodeEntity> episodes = List<EpisodeEntity>.from(
-          jsonItems['items'].map((x) => EpisodeModel.fromJson(x)));
-
-      return episodes;
-    } else {
-      print("Error Episode datasource fetchEpisodesByFeedId: ${response.statusCode}");
+      print(
+          "Error Episode datasource fetchEpisodesAsStreamByFeedId: ${response.statusCode}");
       // If the server did not return a 200 OK response,
       // then throw an exception.
       throw Exception('Failed to load episodes');
