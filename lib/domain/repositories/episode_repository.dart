8d4a9741f09e665dbce890f835890156239a6e5a
bruno <1201ba@gmail.com>
2025-03-17 15:03:49 +0100

    Refactor episode fetching logic and add unread episode count.

--- lib/domain/repositories/episode_repository.dart
+++ lib/domain/repositories/episode_repository.dart
@@ -1,5 +1,10 @@
 import '../entities/episode_entity.dart';
 
 abstract class EpisodeRepository {
-  Stream<List<EpisodeEntity>> fetchEpisodesAsStreamByFeedId(int feedId);
+  Stream<List<EpisodeEntity>> getEpisodes({
+    required bool subscribed,
+    required int feedId,
+    required bool onlyUnread,
+  });
+  Stream<int> unreadLocalEpisodesCount({required int feedId});
 }
