a78b44e1d4fb48828addf0dd86d75a9c52677b39
bruno <1201ba@gmail.com>
2025-03-17 15:04:10 +0100

    Refactor episode fetching logic to allow filtering

--- lib/domain/usecases/episode_usecases.dart
+++ lib/domain/usecases/episode_usecases.dart
@@ -2,13 +2,22 @@ import 'package:podcast/domain/repositories/episode_repository.dart';
 
 import '../entities/episode_entity.dart';
 
-class   EpisodeUseCases{
+class EpisodeUseCases {
   final EpisodeRepository episodeRepository;
 
   EpisodeUseCases({required this.episodeRepository});
 
-  Stream<List<EpisodeEntity>> fetchEpisodesAsStream(int feedId){
-    return episodeRepository.fetchEpisodesAsStreamByFeedId(feedId);
+  Stream<List<EpisodeEntity>> getEpisodes({
+    required bool subscribed,
+    required int feedId,
+    required bool onlyUnread,
+  }) {
+    return episodeRepository.getEpisodes(
+        subscribed: subscribed, feedId: feedId, onlyUnread: onlyUnread);
+  }
+
+  Stream<int> unreadLocalEpisodesCount({required int feedId}) {
+    return episodeRepository.unreadLocalEpisodesCount(feedId: feedId);
   }
 
 }
