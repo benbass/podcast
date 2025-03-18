79e5c93af84cdb14955a1a314bfeac5ee816cc20
bruno <1201ba@gmail.com>
2025-03-18 16:00:13 +0100

    Refactor podcast events with Equatable and clear distinction
    -   Refactored `PodcastEvent` to use `Equatable` for better comparison.
    -   Categorized events into `LOCAL`, `REMOTE`, `LOCAL and REMOTE`, and `None`.
    -   Added specific events for:
        -   Loading subscribed podcasts (`LoadSubscribedPodcastsEvent`).
        -   Subscribing and unsubscribing (`SubscribeToPodcastEvent`, `UnSubscribeFromPodcastEvent`).
        -   Managing unread episodes (`GetUnreadEpisodesByFeedIdEvent`, `ToggleUnreadEpisodesVisibilityEvent`).
        -   Fetching remote podcasts by keyword (`GetRemotePodcastsByKeywordEvent`).
        - Updating query by id (`UpdateQueryEvent`).
        - Refreshing episodes by id (`RefreshEpisodesByFeedIdEvent`).
        - Getting episodes by id (`GetEpisodesByFeedIdEvent`).
        - Tapping podcast event (`PodcastTappedEvent`).
    -   Removed redundant events like `SubscribedPodcastsLoadingEvent`, `SubscribedPodcastsLoadedEvent`, `SearchPodcastsByKeywordProcessingEvent`, `SearchPodcastsByKeywordSuccessEvent`, `FetchEpisodesForPodcastProcessingEvent`, `FetchEpisodesForPodcastSuccessEvent`, `RefreshPodcastEpisodesProcessingEvent`, and `PodcastsQueryResultUpdateEvent`.
    -   Ensured each event properly overrides the props method.

--- lib/application/podcast_bloc/podcast_event.dart
+++ lib/application/podcast_bloc/podcast_event.dart
@@ -1,48 +1,101 @@
 part of 'podcast_bloc.dart';
 
 @immutable
-sealed class PodcastEvent {}
-
-final class SubscribedPodcastsLoadingEvent extends PodcastEvent {}
-
-final class SubscribedPodcastsLoadedEvent extends PodcastEvent {}
-
-final class SearchPodcastsByKeywordProcessingEvent extends PodcastEvent {
-  final String keyword;
-  SearchPodcastsByKeywordProcessingEvent({
-    required this.keyword,
-  });
+sealed class PodcastEvent extends Equatable {
+  const PodcastEvent();
+  @override
+  List<Object> get props => [];
 }
 
-final class SearchPodcastsByKeywordSuccessEvent extends PodcastEvent {}
+/// LOCAL
+// Podcast Local
+final class LoadSubscribedPodcastsEvent extends PodcastEvent {}
 
-final class FetchEpisodesForPodcastProcessingEvent extends PodcastEvent {
+final class SubscribeToPodcastEvent extends PodcastEvent {
   final PodcastEntity podcast;
-  FetchEpisodesForPodcastProcessingEvent({
+  const SubscribeToPodcastEvent({
     required this.podcast,
   });
+  @override
+  List<Object> get props => [podcast];
 }
 
-final class FetchEpisodesForPodcastSuccessEvent extends PodcastEvent {}
-
-final class RefreshPodcastEpisodesProcessingEvent extends PodcastEvent {}
-
-final class PodcastTappedEvent extends PodcastEvent {
-  final PodcastEntity podcast;
-  PodcastTappedEvent({
-    required this.podcast,
+final class UnSubscribeFromPodcastEvent extends PodcastEvent {
+  final int id;
+  const UnSubscribeFromPodcastEvent({
+    required this.id,
   });
+  @override
+  List<Object> get props => [id];
 }
 
-final class SubscribeToPodcastEvent extends PodcastEvent {}
-
-final class UnSubscribeFromPodcastEvent extends PodcastEvent {}
-
-final class PodcastsQueryResultUpdateEvent extends PodcastEvent {}
+class GetUnreadEpisodesByFeedIdEvent extends PodcastEvent {
+  final int feedId;
+  const GetUnreadEpisodesByFeedIdEvent({
+    required this.feedId,
+  });
+  @override
+  List<Object> get props => [feedId];
+}
 
 final class ToggleUnreadEpisodesVisibilityEvent extends PodcastEvent {
   final bool areReadEpisodesVisible;
-  ToggleUnreadEpisodesVisibilityEvent({
+  const ToggleUnreadEpisodesVisibilityEvent({
     required this.areReadEpisodesVisible,
   });
+  @override
+  List<Object> get props => [areReadEpisodesVisible];
+}
+
+/// REMOTE
+// Podcast Remote
+final class GetRemotePodcastsByKeywordEvent extends PodcastEvent {
+  final String keyword;
+  const GetRemotePodcastsByKeywordEvent({
+    required this.keyword,
+  });
+  @override
+  List<Object> get props => [keyword];
+}
+
+final class UpdateQueryEvent extends PodcastEvent {
+  final int feedId;
+
+  const UpdateQueryEvent({
+    required this.feedId,
+  });
+  @override
+  List<Object> get props => [feedId];
+}
+
+final class RefreshEpisodesByFeedIdEvent extends PodcastEvent {
+  final int feedId;
+  const RefreshEpisodesByFeedIdEvent({
+    required this.feedId,
+  });
+  @override
+  List<Object> get props => [feedId];
+}
+
+
+/// LOCAL and REMOTE
+// Episodes Local or remote
+class GetEpisodesByFeedIdEvent extends PodcastEvent {
+  final int feedId;
+  const GetEpisodesByFeedIdEvent({
+    required this.feedId,
+  });
+  @override
+  List<Object> get props => [feedId];
+}
+
+
+/// None
+final class PodcastTappedEvent extends PodcastEvent {
+  final PodcastEntity podcast;
+  const PodcastTappedEvent({
+    required this.podcast,
+  });
+  @override
+  List<Object> get props => [podcast];
 }
