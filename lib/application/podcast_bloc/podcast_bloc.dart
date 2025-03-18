9472071b2a69d570da540e7cde8efb22eeca44d2
bruno <1201ba@gmail.com>
2025-03-18 16:00:57 +0100

    Refactor podcast bloc and add local and remote events
    This commit refactors the podcast bloc to include local and remote events. It also adds new events for loading subscribed podcasts, getting unread episodes by feed ID, toggling unread episodes visibility, subscribing and unsubscribing to podcasts, updating query, getting remote podcasts by keyword, refreshing episodes by feed ID, getting episodes by feed ID, and handling podcast taps.

--- lib/application/podcast_bloc/podcast_bloc.dart
+++ lib/application/podcast_bloc/podcast_bloc.dart
@@ -1,193 +1,225 @@
+import 'dart:async';
+
 import 'package:bloc/bloc.dart';
+import 'package:equatable/equatable.dart';
 import 'package:meta/meta.dart';
-import 'package:podcast/domain/entities/podcast_entity.dart';
-import 'package:podcast/domain/usecases/podcast_usecases.dart';
+import 'package:podcast/core/globals.dart';
+
+import '../../domain/entities/episode_entity.dart';
+import '../../domain/entities/podcast_entity.dart';
+import '../../domain/usecases/episode_usecases.dart';
+import '../../domain/usecases/podcast_usecases.dart';
 
 part 'podcast_event.dart';
 part 'podcast_state.dart';
 
 class PodcastBloc extends Bloc<PodcastEvent, PodcastState> {
   final PodcastUseCases podcastUseCases;
+  final EpisodeUseCases episodeUseCases;
   PodcastBloc({
     required this.podcastUseCases,
-  }) : super(PodcastState.initial()) {
-    on<SubscribedPodcastsLoadingEvent>((event, emit) async {
-      emit(state.copyWith(loading: true));
-      try {
-        List<PodcastEntity> subscribedPodcasts =
-            await podcastUseCases.getSubscribedPodcasts();
+    required this.episodeUseCases,
+  }) : super(
+          PodcastState.initial(),
+        ) {
+    /// LOCAL
+    on<LoadSubscribedPodcastsEvent>(_onLoadSubscribedPodcastsEvent);
+    on<GetUnreadEpisodesByFeedIdEvent>(_onGetUnreadEpisodesByFeedIdEvent);
+    on<ToggleUnreadEpisodesVisibilityEvent>(
+        _onToggleUnreadEpisodesVisibilityEvent);
+    on<SubscribeToPodcastEvent>(_onSubscribeToPodcastEvent);
+    on<UnSubscribeFromPodcastEvent>(_onUnSubscribeToPodcastEvent);
+    on<UpdateQueryEvent>(_onUpdateQueryEvent);
 
-        // Let's refresh the episodes at app start
-        for (PodcastEntity podcast in subscribedPodcasts) {
-          await podcastUseCases.refreshPodcastEpisodes(podcast);
-        }
+    /// END LOCAL
 
-        emit(state.copyWith(
-          subscribedPodcasts: subscribedPodcasts,
-        ));
-        add(SubscribedPodcastsLoadedEvent());
-      } catch (e) {
-        emit(PodcastState.error(
-            message: 'Failed to load the podcasts from the database'));
-      }
-    });
+    /// REMOTE
+    on<GetRemotePodcastsByKeywordEvent>(_onGetRemotePodcastsByKeywordEvent);
+    on<RefreshEpisodesByFeedIdEvent>(_onRefreshEpisodesByFeedIdEvent);
 
-    on<SubscribedPodcastsLoadedEvent>((event, emit) async {
-      emit(state.copyWith(loading: false));
-    });
+    /// END REMOTE
 
-    on<SearchPodcastsByKeywordProcessingEvent>((event, emit) async {
-      emit(state.copyWith(loading: true, keyword: event.keyword));
-      try {
-        List<PodcastEntity> podcastsQueryResult =
-            await podcastUseCases.fetchPodcasts(event.keyword);
+    /// LOCAL and REMOTE
+    on<GetEpisodesByFeedIdEvent>(_onGetEpisodesByFeedIdEvent);
 
-        emit(state.copyWith(
-          podcastsQueryResult: podcastsQueryResult,
-        ));
-        add(SearchPodcastsByKeywordSuccessEvent());
-      } catch (e) {
-        emit(PodcastState.error(
-            message:
-                'Failed to load podcasts from the server.\nThe server may be down.\nError: ${e.toString()}'));
-      }
-    });
+    /// END LOCAL and REMOTE
 
-    on<SearchPodcastsByKeywordSuccessEvent>((event, emit) async {
-      emit(state.copyWith(loading: false));
-    });
+    on<PodcastTappedEvent>(_onPodcastTappedEvent);
+  }
 
-    on<FetchEpisodesForPodcastProcessingEvent>((event, emit) async {
-      if (!event.podcast.subscribed) {
-        emit(state.copyWith(loading: true));
-        try {
-          if (event.podcast.episodes.isEmpty) {
-            final PodcastEntity podcastWithEpisodes =
-                await podcastUseCases.fillPodcastWithEpisodes(event.podcast);
-            emit(state.copyWith(
-              podcast: podcastWithEpisodes,
-            ));
-          } else {
-            final PodcastEntity podcastWithEpisodes =
-                await podcastUseCases.refreshPodcastEpisodes(state.podcast!);
-            emit(state.copyWith(
-              podcast: podcastWithEpisodes,
-            ));
-          }
-          add(FetchEpisodesForPodcastSuccessEvent());
-        } catch (e) {
-          emit(PodcastState.error(
-              message:
-                  'Failed to load episodes from the server.\nThe server may be down.\nError: ${e.toString()}'));
-        }
-      }
-    });
-
-    on<FetchEpisodesForPodcastSuccessEvent>((event, emit) async {
-      emit(state.copyWith(loading: false));
-    });
-
-    on<RefreshPodcastEpisodesProcessingEvent>((event, emit) async {
-      emit(state.copyWith(loading: true));
-      PodcastEntity podcast =
-          await podcastUseCases.refreshPodcastEpisodes(state.podcast!);
+  FutureOr<void> _onLoadSubscribedPodcastsEvent(event, emit) async {
+    emit(state.copyWith(status: PodcastStatus.loading));
+    try {
+      final List<PodcastEntity> subscribedPodcasts =
+          await podcastUseCases.getSubscribedPodcasts();
       emit(state.copyWith(
-        podcast: podcast,
+        status: PodcastStatus.success,
+        subscribedPodcasts: subscribedPodcasts,
       ));
-      add(FetchEpisodesForPodcastSuccessEvent());
-    });
+      // Get the Stream from the use case
+      /* final stream = podcastUseCases.getSubscribedPodcasts();
+      // Listen to the Stream and update the state with the new data
+      await emit.forEach<List<PodcastEntity>>(
+        stream,
+        onData: (data) {
+          emit(state.copyWith(
+            status: PodcastStatus.success,
+            subscribedPodcasts: data,
+          ));
+        },
+        onError: (error, stackTrace) {
+          emit(state.copyWith(status: PodcastStatus.failure));
+        },
+      );*/
+    } catch (e) {
+      emit(state.copyWith(status: PodcastStatus.failure));
+    }
+  }
 
-    on<PodcastTappedEvent>((event, emit) async {
-      emit(state.copyWith(podcast: event.podcast));
-    });
+  FutureOr<void> _onGetUnreadEpisodesByFeedIdEvent(event, emit) async {
+    emit(state.copyWith(status: PodcastStatus.loading));
+    try {
+      // Get the Stream from the use case
+      final stream = episodeUseCases.getEpisodes(
+        subscribed: true,
+        feedId: state.currentPodcast.pId,
+        onlyUnread: true,
+      );
+      // Listen to the Stream and update the state with the new data
+      await emit.forEach<List<EpisodeEntity>>(
+        stream,
+        onData: (data) {
+          emit(state.copyWith(
+            status: PodcastStatus.success,
+            episodes: data,
+          ));
+        },
+        onError: (error, stackTrace) {
+          emit(state.copyWith(status: PodcastStatus.failure));
+        },
+      );
+    } catch (e) {
+      emit(state.copyWith(status: PodcastStatus.failure));
+    }
+  }
 
-    on<SubscribeToPodcastEvent>((event, emit) async {
-      /// Check if podcast is already subscribed
-      // key = podcast pId, value = 0  (we need only the key for the check)
-      Map<int, int> podcastPIdMap = {};
-      for (PodcastEntity podcast in state.subscribedPodcasts) {
-        podcastPIdMap[podcast.pId] = 0;
-      }
-      if (podcastPIdMap.containsKey(state.podcast!.pId)) {
-        // If key exists, podcast is already subscribed
-        emit(PodcastState.error(
-            message:
-                'You already subscribed to the podcast: ${state.podcast!.title}'));
-      } else {
-        bool objectIsSaved =
-            await podcastUseCases.subscribeToPodcast(state.podcast!);
-        if (objectIsSaved) {
-          // Saving to db was successful
-          // We get the list of subscribed podcasts from db
-          List<PodcastEntity> subscribedPodcasts =
-              await podcastUseCases.getSubscribedPodcasts();
-          // We emit the new states with last subscribed podcast from the list
-          emit(state.copyWith(subscribedPodcasts: [
-            ...state.subscribedPodcasts,
-            subscribedPodcasts.last
-          ], podcast: subscribedPodcasts.last));
-          // We update the list from the query result, if any, so list item will be correctly displayed as subscribed
-          add(PodcastsQueryResultUpdateEvent());
-        } else {
-          emit(PodcastState.error(message: 'Failed to subscribe to podcast.'));
-        }
-      }
-    });
+  void _onToggleUnreadEpisodesVisibilityEvent(event, emit) {
+    emit(state.copyWith(areReadEpisodesVisible: event.areReadEpisodesVisible));
+  }
 
-    on<UnSubscribeFromPodcastEvent>((event, emit) async {
-      emit(state.copyWith(loading: true));
-      try {
-        // We want to keep this podcast with all its episodes in case user navigates back to the episode list
-        PodcastEntity unsubscribedPodcast = state.podcast!.copyWith(subscribed: false)..episodes.addAll(state.podcast!.episodes);
-        await podcastUseCases.unsubscribeFromPodcast(state.podcast!);
-        List<PodcastEntity> subscribedPodcasts = state.subscribedPodcasts;
-        subscribedPodcasts
-            .removeWhere((element) => element.pId == state.podcast!.pId);
+  // FutureOr here because subscribing also fetches the episodes from remote
+  // if they are missing before saving to database
+  FutureOr<void> _onSubscribeToPodcastEvent(event, emit) async {
+    emit(state.copyWith(status: PodcastStatus.loading));
+    try {
+      await podcastUseCases.subscribeToPodcast(event.podcast);
+      List<PodcastEntity> subscribedPodcasts =
+          await podcastUseCases.getSubscribedPodcasts();
+      // Current podcast (emitted by prior PodcastTappedEvent is from query result.
+      // Its id is 0 since this object is not in the database.
+      // Now that podcast has been added to the database with an id != 0 and saved episodes
+      // we replace the current podcast with the one from the database:
+      PodcastEntity currentPodcast =
+          podcastBox.get(subscribedPodcasts.last.id)!;
+      emit(state.copyWith(
+        status: PodcastStatus.success,
+        subscribedPodcasts: subscribedPodcasts,
+        currentPodcast: currentPodcast,
+      ));
+    } catch (e) {
+      emit(state.copyWith(status: PodcastStatus.failure));
+    }
+  }
+
+  Future<void> _onUnSubscribeToPodcastEvent(event, emit) async {
+    emit(state.copyWith(status: PodcastStatus.loading));
+    try {
+      // We want to keep this podcast with all its episodes in case user navigates back to the episode list
+      PodcastEntity unsubscribedPodcast = state.currentPodcast.copyWith(
+        subscribed: false,
+      )..episodes.addAll(
+          state.currentPodcast.episodes,
+        );
+      await podcastUseCases.unsubscribeFromPodcast(state.currentPodcast);
+      List<PodcastEntity> subscribedPodcasts =
+          await podcastUseCases.getSubscribedPodcasts();
+      emit(state.copyWith(
+        subscribedPodcasts: subscribedPodcasts,
+        currentPodcast: unsubscribedPodcast,
+        status: PodcastStatus.success,
+      ));
+      add(UpdateQueryEvent(feedId: state.currentPodcast.pId));
+    } catch (e) {
+      emit(state.copyWith(status: PodcastStatus.failure));
+    }
+  }
+
+  Future<void> _onUpdateQueryEvent(event, emit) async {
+    List<PodcastEntity> updatedList = await podcastUseCases.updatedQueryResult(
+      state.queryResultPodcasts,
+      state.currentPodcast,
+    );
+    emit(state.copyWith(
+      queryResultPodcasts: updatedList,
+    ));
+  }
+
+  FutureOr<void> _onGetRemotePodcastsByKeywordEvent(event, emit) async {
+    emit(state.copyWith(status: PodcastStatus.loading, keyword: event.keyword));
+    try {
+      List<PodcastEntity> queryResult =
+          await podcastUseCases.fetchPodcasts(event.keyword);
+      emit(state.copyWith(
+        status: PodcastStatus.success,
+        queryResultPodcasts: queryResult,
+      ));
+    } catch (e) {
+      emit(state.copyWith(status: PodcastStatus.failure));
+    }
+  }
+
+  FutureOr<void> _onRefreshEpisodesByFeedIdEvent(event, emit) async {
+    emit(state.copyWith(status: PodcastStatus.loading));
+    try {
+      List<EpisodeEntity> updatedEpisodes = await episodeUseCases
+          .refreshEpisodes(podcast: state.currentPodcast)
+          .first;
+      emit(state.copyWith(
+        status: PodcastStatus.success,
+        episodes: updatedEpisodes,
+        currentPodcast: state.currentPodcast.copyWith()
+          ..episodes.addAll(updatedEpisodes),
+      ));
+    } catch (e) {
+      emit(state.copyWith(status: PodcastStatus.failure));
+    }
+  }
+
+  Future<void> _onPodcastTappedEvent(event, emit) async {
+    emit(state.copyWith(
+      currentPodcast: event.podcast,
+      episodes: event.podcast.episodes,
+    ));
+  }
+
+  FutureOr<void> _onGetEpisodesByFeedIdEvent(event, emit) async {
+    emit(state.copyWith(status: PodcastStatus.loading));
+    try {
+      final stream = episodeUseCases.getEpisodes(
+        subscribed: state.currentPodcast.subscribed,
+        feedId: state.currentPodcast.pId,
+        onlyUnread: false,
+      );
+      await emit.forEach<List<EpisodeEntity>>(stream, onData: (data) {
         emit(state.copyWith(
-          subscribedPodcasts: subscribedPodcasts,
-          podcast: unsubscribedPodcast,
-          loading: false,
+          status: PodcastStatus.success,
+          episodes: data,
         ));
-        add(PodcastsQueryResultUpdateEvent());
-      } catch (e) {
-        emit(PodcastState.error(
-            message:
-                'Failed to unsubscribe from podcast: ${state.podcast!.title}'));
-      }
-    });
-
-    on<PodcastsQueryResultUpdateEvent>((event, emit) async {
-      List<PodcastEntity> podcastsQueryResult = state.podcastsQueryResult;
-
-      // Replace object in query result, only if object is in query result
-      Future<List<PodcastEntity>> updatedQueryResult() async {
-        if (podcastsQueryResult.isNotEmpty) {
-          // Create a map to store the API podcastIndex ids (pId) as keys
-          Map<int, int> map = {};
-          for (PodcastEntity podcast in podcastsQueryResult) {
-            map[podcast.pId] = 0;
-          }
-          // Check if object is in map
-          if (map.containsKey(state.podcast!.pId)) {
-            // find index of object in query result
-            final int index = podcastsQueryResult
-                .indexWhere((element) => element.pId == state.podcast!.pId);
-
-            // Remove old object from query result
-            podcastsQueryResult.removeAt(index);
-            // Insert new object in query result
-            // the state of the new object (subscribed or not) was already set in the SubscribeToPodcastEvent or UnSubscribeFromPodcastEvent
-            podcastsQueryResult.insert(index, state.podcast!);
-          }
-        }
-        return podcastsQueryResult;
-      }
-
-      await updatedQueryResult();
-    });
-
-    on<ToggleUnreadEpisodesVisibilityEvent>((event, emit) async {
-        emit(state.copyWith(areReadEpisodesVisible: event.areReadEpisodesVisible));
-    });
+      }, onError: (error, stackTrace) {
+        emit(state.copyWith(status: PodcastStatus.failure));
+      });
+    } catch (e) {
+      emit(state.copyWith(status: PodcastStatus.failure));
+    }
   }
 }
