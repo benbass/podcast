791059cf8c6009e3883d3e0ef7ea9007a8641ba8
bruno <1201ba@gmail.com>
2025-03-19 18:22:07 +0100

    Refactor failure widget to remove SliverToBoxAdapter

--- lib/presentation/custom_widgets/failure_widget.dart
+++ lib/presentation/custom_widgets/failure_widget.dart
@@ -1,21 +1,19 @@
 import 'package:flutter/material.dart';
 
 Widget buildFailureWidget({required String message}) {
-  return SliverToBoxAdapter(
-    child: Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        const Icon(
-          Icons.error_outline,
-          color: Colors.red,
-          size: 60,
-        ),
-        Padding(
-          padding: const EdgeInsets.only(
-              top: 16, left: 16, right: 16),
-          child: Text(message),
-        ),
-      ],
-    ),
+  return Column(
+    mainAxisAlignment: MainAxisAlignment.center,
+    children: [
+      const Icon(
+        Icons.error_outline,
+        color: Colors.red,
+        size: 60,
+      ),
+      Padding(
+        padding: const EdgeInsets.only(
+            top: 16, left: 16, right: 16),
+        child: Text(message),
+      ),
+    ],
   );
 }
