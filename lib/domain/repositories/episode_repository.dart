2ccef0789f57a795fabf9c2fa877d59012a0b0b4
bruno <1201ba@gmail.com>
2025-03-20 16:20:21 +0100

    Remove refreshEpisodes from repository.

--- lib/domain/repositories/episode_repository.dart
+++ lib/domain/repositories/episode_repository.dart
@@ -1,5 +1,4 @@
 import '../entities/episode_entity.dart';
-import '../entities/podcast_entity.dart';
 
 abstract class EpisodeRepository {
   Stream<List<EpisodeEntity>> getEpisodes({
@@ -8,7 +7,4 @@ abstract class EpisodeRepository {
     required bool showRead,
   });
   Stream<int> unreadLocalEpisodesCount({required int feedId});
-  Stream<List<EpisodeEntity>> refreshEpisodes({
-    required PodcastEntity podcast,
-  });
 }
