424f8007935bdb672d937d21ae7f79151e651d87
bruno <1201ba@gmail.com>
2025-03-20 16:42:17 +0100

    Add method to retrieve new episodes

--- lib/domain/usecases/episode_usecases.dart
+++ lib/domain/usecases/episode_usecases.dart
@@ -22,4 +22,31 @@ class EpisodeUseCases {
   Stream<int> unreadLocalEpisodesCount({required int feedId}) {
     return episodeRepository.unreadLocalEpisodesCount(feedId: feedId);
   }
+
+  Future<List<EpisodeEntity>> newEpisodesList({required int feedId}) async {
+    final List<EpisodeEntity> currentLocalEpisodes = await getEpisodes(
+      subscribed: true, // we refresh on subscribed podcasts only
+      feedId: feedId,
+      showRead: true, // must be true, otherwise we don't get all episodes into the set
+    )
+        .first;
+
+    List<EpisodeEntity> currentEpisodesOnRemote = await getEpisodes(
+      subscribed:
+      false, // false because we want to fetch episodes from remote, not from database only
+      feedId: feedId,
+      showRead: true,
+    )
+        .first;
+
+    final Set<int> currentEpisodeIds =
+    currentLocalEpisodes.map((ep) => ep.eId).toSet();
+
+    final List<EpisodeEntity> newEpisodes = currentEpisodesOnRemote
+        .where((episode) => !currentEpisodeIds.contains(episode.eId))
+        .toList()
+      ..sort((a, b) => a.datePublished.compareTo(b.datePublished));
+
+    return newEpisodes;
+  }
 }
