cea87cb4c96c2fc36ad80de90004f0f78fea2a44
bruno <1201ba@gmail.com>
2025-03-21 13:15:51 +0100

    Update SubscribedPodcastCard to use local file paths for images.

--- lib/presentation/subscribed_podcasts_homepage/widgets/subscribed_podcast_card.dart
+++ lib/presentation/subscribed_podcasts_homepage/widgets/subscribed_podcast_card.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:podcast/domain/usecases/episode_usecases.dart';
@@ -5,7 +7,6 @@ import 'package:podcast/injection.dart';
 import 'package:podcast/presentation/subscribed_podcasts_homepage/widgets/rounded_text_widget.dart';
 import '../../../application/podcast_bloc/podcast_bloc.dart';
 import '../../../domain/entities/podcast_entity.dart';
-import '../../../helpers/core/image_provider.dart';
 import '../../custom_widgets/page_transition.dart';
 import '../../episodes_list_page/episodes_list_page.dart';
 
@@ -19,56 +20,50 @@ class SubscribedPodcastCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final podcastBloc = BlocProvider.of<PodcastBloc>(context);
-    return FutureBuilder<ImageProvider>(
-        future: MyImageProvider(url: podcast.artwork).imageProvider,
-        builder: (BuildContext context, AsyncSnapshot<ImageProvider> snapshot) {
-          final ImageProvider imageProvider = snapshot.hasData
-              ? snapshot.data!
-              : const AssetImage('assets/placeholder.png');
-          return InkResponse(
-            onTap: () {
-              podcastBloc.add(PodcastTappedEvent(podcast: podcast));
-              Navigator.push(
-                context,
-                ScaleRoute(
-                  page: const EpisodesListPage(),
-                ),
-              );
-            },
-            child: Material(
-              elevation: 16,
-              borderRadius: BorderRadius.circular(15),
-              color: Colors.transparent,
-              child: Container(
-                decoration: BoxDecoration(
-                  image: DecorationImage(
-                    image: imageProvider,
-                    fit: BoxFit.fitHeight,
-                  ),
-                  borderRadius: BorderRadius.circular(15),
-                ),
-                child: Stack(
-                  children: [
-                    Positioned(
-                      bottom: 0,
-                      right: 0,
-                      child: SizedBox(
-                          width: 50,
-                          height: 50,
-                          child: StreamBuilder<int>(
-                            stream: getIt<EpisodeUseCases>().unreadLocalEpisodesCount(feedId: podcast.pId),
-                            builder: (context, snapshot) {
-                              return RoundedTextWidget(
-                                text: snapshot.data.toString()
-                              );
-                            }
-                          )),
-                    )
-                  ],
-                ),
-              ),
+    return InkResponse(
+      onTap: () {
+        podcastBloc.add(PodcastTappedEvent(podcast: podcast));
+        Navigator.push(
+          context,
+          ScaleRoute(
+            page: const EpisodesListPage(),
+          ),
+        );
+      },
+      child: Material(
+        elevation: 16,
+        borderRadius: BorderRadius.circular(15),
+        color: Colors.transparent,
+        child: Container(
+          decoration: BoxDecoration(
+            image: DecorationImage(
+              image: podcast.artworkFilePath != null
+                  ? FileImage(File(podcast.artworkFilePath!))
+                  : const AssetImage('assets/placeholder.png'),
+              fit: BoxFit.fitHeight,
             ),
-          );
-        });
+            borderRadius: BorderRadius.circular(15),
+          ),
+          child: Stack(
+            children: [
+              Positioned(
+                bottom: 0,
+                right: 0,
+                child: SizedBox(
+                    width: 50,
+                    height: 50,
+                    child: StreamBuilder<int>(
+                        stream: getIt<EpisodeUseCases>()
+                            .unreadLocalEpisodesCount(feedId: podcast.pId),
+                        builder: (context, snapshotCount) {
+                          return RoundedTextWidget(
+                              text: snapshotCount.data.toString());
+                        })),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
