05e239171ba53862600cf1d4aef3ec24958c6e99
bruno <1201ba@gmail.com>
2025-03-21 13:16:37 +0100

    Add podcast artwork file saving and deletion
    This commit introduces the ability to save podcast artwork to a file when subscribing and delete it when unsubscribing. It also updates podcast subscription status, and episode unread count.

--- lib/infrastructure/repositories/podcast_repository_impl.dart
+++ lib/infrastructure/repositories/podcast_repository_impl.dart
@@ -1,8 +1,11 @@
+import 'dart:io';
+
 import 'package:podcast/core/globals.dart';
 import 'package:podcast/domain/entities/podcast_entity.dart';
 import 'package:podcast/infrastructure/datasources/episode_datasources.dart';
 import '../../domain/entities/episode_entity.dart';
 import '../../domain/repositories/podcast_repository.dart';
+import '../../helpers/core/save_artwork_to_file.dart';
 import '../datasources/podcast_datasources.dart';
 
 class PodcastRepositoryImpl implements PodcastRepository {
@@ -21,13 +24,18 @@ class PodcastRepositoryImpl implements PodcastRepository {
   @override
   Future<bool> subscribeToPodcast(PodcastEntity podcast) async {
     try {
+      // Save artwork to file.
+      final String artworkFilePath =
+          await saveArtworkToFile(podcast.artwork) ?? "null";
+
       // Determine if episodes need to be fetched.
       final podcastWithEpisodes = podcast.episodes.isEmpty
           ? await _fetchEpisodesIfMissing(podcast)
           : podcast;
 
-      // Update the podcast.
-      final subscribedPodcast = _markPodcastAsSubscribed(podcastWithEpisodes);
+      // Update the subscribed status of the podcast.
+      final subscribedPodcast =
+          _markPodcastAsSubscribed(podcastWithEpisodes, artworkFilePath);
 
       // Persist the updated podcast data.
       podcastBox.put(subscribedPodcast);
@@ -47,18 +55,40 @@ class PodcastRepositoryImpl implements PodcastRepository {
   }
 
   /// Marks a podcast as subscribed and updates the episodes with the unread episode count.
-  PodcastEntity _markPodcastAsSubscribed(PodcastEntity podcast) {
-    final PodcastEntity subscribedPodcast = podcast.copyWith(
-      subscribed: true,
-    )..episodes.addAll(podcast.episodes);
-    return subscribedPodcast;
+  PodcastEntity _markPodcastAsSubscribed(
+      PodcastEntity podcast, String? filePath) {
+    if (filePath != "null") {
+      final PodcastEntity subscribedPodcast = podcast.copyWith(
+        subscribed: true,
+        artworkFilePath: filePath,
+      )..episodes.addAll(podcast.episodes);
+      return subscribedPodcast;
+    } else {
+      final PodcastEntity subscribedPodcast = podcast.copyWith(
+        subscribed: true,
+      )..episodes.addAll(podcast.episodes);
+      return subscribedPodcast;
+    }
   }
 
   @override
   Future<void> unsubscribeFromPodcast(PodcastEntity podcast) async {
-    for(var episode in podcast.episodes){
+    // Delete episodes in db
+    for (var episode in podcast.episodes) {
       episodeBox.remove(episode.id);
     }
+    // Delete artwork file in app directory
+    if (podcast.artworkFilePath != null) {
+      final file = File(podcast.artworkFilePath!);
+      if (await file.exists()) {
+        try {
+          await file.delete();
+        } catch (e) {
+          print("Error deleting file: $e");
+        }
+      }
+    }
+    // Delete podcast in db
     podcastBox.remove(podcast.id);
   }
 
@@ -82,7 +112,8 @@ class PodcastRepositoryImpl implements PodcastRepository {
   }
 
   @override
-  Future<List<PodcastEntity>> updatedQueryResult(queryResult, currentPodcast) async {
+  Future<List<PodcastEntity>> updatedQueryResult(
+      queryResult, currentPodcast) async {
     List<PodcastEntity> currentQueryResult = queryResult;
     if (currentQueryResult.isNotEmpty) {
       // Create a map to store the API podcastIndex ids (pId) as keys
@@ -109,4 +140,3 @@ class PodcastRepositoryImpl implements PodcastRepository {
     return currentQueryResult;
   }
 }
-
