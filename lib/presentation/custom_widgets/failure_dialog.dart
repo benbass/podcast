fb70f63e418f1218257bf5140fadaa67eafcd2e1
bruno <1201ba@gmail.com>
2025-03-23 18:25:06 +0100

    Add failure dialog for error handling.

---
+++ lib/presentation/custom_widgets/failure_dialog.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import '../../application/podcast_bloc/podcast_bloc.dart';
+
+class FailureDialog extends StatelessWidget {
+  final String message;
+
+  const FailureDialog({super.key, required this.message,});
+
+  @override
+  Widget build(BuildContext context) {
+    return AlertDialog(
+      elevation: 30,
+      shadowColor: Colors.black,
+      content: Column(
+        mainAxisSize: MainAxisSize.min,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          const Icon(
+            Icons.error_outline,
+            color: Colors.red,
+            size: 60,
+          ),
+          Padding(
+            padding: const EdgeInsets.only(top: 16, left: 16, right: 16),
+            child: Text(message),
+          ),
+        ],
+      ),
+      actions: <Widget>[
+        TextButton(
+          child: const Text('Close'),
+          onPressed: () {
+            BlocProvider.of<PodcastBloc>(context).add(ToggleStateToSuccessAfterFailureEvent());
+            Navigator.of(context).pop();
+          },
+        ),
+      ],
+    );
+  }
+}
