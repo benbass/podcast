c29962de39578c5a8560d2efaec5d6ca9dccc920
bruno <1201ba@gmail.com>
2025-03-24 14:19:30 +0100

    Implement no-internet handling when subscribing to a podcast.

--- lib/presentation/custom_widgets/elevated_button_subscribe.dart
+++ lib/presentation/custom_widgets/elevated_button_subscribe.dart
@@ -3,7 +3,10 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:podcast/domain/entities/podcast_entity.dart';
 import 'package:podcast/presentation/custom_widgets/page_transition.dart';
 import '../../application/podcast_bloc/podcast_bloc.dart';
+import '../../helpers/core/connectivity_manager.dart';
+import '../../injection.dart';
 import '../subscribed_podcasts_homepage/subscribed_podcasts_homepage.dart';
+import 'failure_dialog.dart';
 
 class ElevatedButtonSubscribe extends StatelessWidget {
   final PodcastEntity podcast;
@@ -17,11 +20,26 @@ class ElevatedButtonSubscribe extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    void subscribeToPodcast() async {
+      final String connectionType =
+          await getIt<ConnectivityManager>().getConnectionTypeAsString();
+      if (connectionType != 'none' && context.mounted) {
+        BlocProvider.of<PodcastBloc>(context)
+            .add(SubscribeToPodcastEvent(podcast: podcast));
+      } else {
+        if (context.mounted) {
+          showDialog(
+              context: context,
+              builder: (context) =>
+                  const FailureDialog(message: "No internet connection!"));
+        }
+      }
+    }
+
     return ElevatedButton(
-      onPressed: () {
+      onPressed: () async {
         !podcast.subscribed
-            ? BlocProvider.of<PodcastBloc>(context)
-                .add(SubscribeToPodcastEvent(podcast: podcast))
+            ? subscribeToPodcast()
             : BlocProvider.of<PodcastBloc>(context)
                 .add(UnSubscribeFromPodcastEvent(id: podcast.id));
         if (navigate) {
@@ -35,7 +53,8 @@ class ElevatedButtonSubscribe extends StatelessWidget {
       },
       child: BlocBuilder<PodcastBloc, PodcastState>(
         builder: (context, state) {
-          return Text(state.currentPodcast.subscribed ? "Unsubscribe" : "Subscribe");
+          return Text(
+              state.currentPodcast.subscribed ? "Unsubscribe" : "Subscribe");
         },
       ),
     );
