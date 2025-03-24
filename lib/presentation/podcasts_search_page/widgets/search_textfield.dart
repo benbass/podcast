41f73258e94d9afe300bbfc10c7a9bd41dceaa3b
bruno <1201ba@gmail.com>
2025-03-24 13:32:38 +0100

    Refactor search functionality to use TextFieldCubit and add network check
    - Migrate search keyword management to TextFieldCubit for better state handling.
    - Implement network connection checks before performing searches.
    - Display a failure dialog if no network or empty keyword.
    - Update search text field to use TextFieldCubit's state.

--- lib/presentation/podcasts_search_page/widgets/search_textfield.dart
+++ lib/presentation/podcasts_search_page/widgets/search_textfield.dart
@@ -1,54 +1,61 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:podcast/presentation/custom_widgets/failure_dialog.dart';
 
 import '../../../application/podcast_bloc/podcast_bloc.dart';
+import '../../../application/textfield_cubit/text_field_cubit.dart';
+import '../../../helpers/core/connectivity_manager.dart';
+import '../../../injection.dart';
 
-class SearchTextField extends StatefulWidget {
+class SearchTextField extends StatelessWidget {
   const SearchTextField({super.key});
 
   @override
-  State<SearchTextField> createState() => _SearchTextFieldState();
-}
-
-class _SearchTextFieldState extends State<SearchTextField> {
-  final TextEditingController _textEditingController = TextEditingController();
-  String hintText = "Search";
-
-  Future<void> _performSearch(BuildContext context) async {
-    if (_textEditingController.text.isNotEmpty) {
-      FocusScope.of(context).unfocus();
-
-      BlocProvider.of<PodcastBloc>(context)
-          .add(GetRemotePodcastsByKeywordEvent(
-          keyword: _textEditingController.text));
-
-    } else {
-      hintText = "Please enter a keyword";
-    }
-  }
-
-  @override
-  void dispose() {
-    _textEditingController.dispose();
-    super.dispose();
-  }
-
-  @override
   Widget build(BuildContext context) {
-    return BlocBuilder<PodcastBloc, PodcastState>(
+
+    String hintText = "Search";
+
+    Future<void> performSearch(BuildContext context) async {
+      if (context.read<TextFieldCubit>().state != null && context.read<TextFieldCubit>().state!.isNotEmpty) {
+        FocusScope.of(context).unfocus();
+        final String connectionType =
+        await getIt<ConnectivityManager>().getConnectionTypeAsString();
+        if (connectionType != 'none' && context.mounted) {
+          BlocProvider.of<PodcastBloc>(context).add(
+              GetRemotePodcastsByKeywordEvent(
+                  keyword: context.read<TextFieldCubit>().state!));
+        } else {
+          if (context.mounted) {
+            showDialog(
+                context: context,
+                builder: (context) =>
+                const FailureDialog(message: "No internet connection!"));
+          }
+        }
+      } else {
+        if (context.mounted) {
+          showDialog(
+              context: context,
+              builder: (context) =>
+              const FailureDialog(message: "Please enter a keyword"));
+        }
+      }
+    }
+
+    return BlocBuilder<TextFieldCubit, String?>(
       builder: (context, state) {
         return TextField(
-          controller: _textEditingController,
           onTapOutside: (_) {
             FocusManager.instance.primaryFocus?.unfocus();
           },
+          onChanged: (value) => BlocProvider.of<TextFieldCubit>(context).setKeyword(value),
           decoration: InputDecoration(
             filled: true,
             fillColor: Colors.white,
-            hintText: state.keyword.isNotEmpty ? state.keyword : hintText,
+            hintText: state ?? hintText,
             suffixIcon: IconButton(
               icon: const Icon(Icons.search),
-              onPressed: () => _performSearch(context),
+              onPressed: () => performSearch(context),
             ),
           ),
           style: const TextStyle(
