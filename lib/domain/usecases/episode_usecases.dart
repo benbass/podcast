0f93b883f4fcfa4149f9316230afc23c5c1ba13d
bruno <1201ba@gmail.com>
2025-03-25 16:40:26 +0100

    Refactor new episode fetching
    The changes introduce a refactor to the way new episodes are fetched and managed:
    -   Rename `newEpisodesList` to `getNewEpisodesByFeedId`: This method now directly retrieves new episodes based on the feed ID through the repository.

--- lib/domain/usecases/episode_usecases.dart
+++ lib/domain/usecases/episode_usecases.dart
@@ -7,6 +7,8 @@ class EpisodeUseCases {
 
   EpisodeUseCases({required this.episodeRepository});
 
+  // Depending on parameters, we either return local or remote data,
+  // and for local data we may filter episodes based on read status
   Stream<List<EpisodeEntity>> getEpisodes({
     required bool subscribed,
     required int feedId,
@@ -23,30 +25,7 @@ class EpisodeUseCases {
     return episodeRepository.unreadLocalEpisodesCount(feedId: feedId);
   }
 
-  Future<List<EpisodeEntity>> newEpisodesList({required int feedId}) async {
-    final List<EpisodeEntity> currentLocalEpisodes = await getEpisodes(
-      subscribed: true, // we refresh on subscribed podcasts only
-      feedId: feedId,
-      showRead: true, // must be true, otherwise we don't get all episodes into the set
-    )
-        .first;
-
-    List<EpisodeEntity> currentEpisodesOnRemote = await getEpisodes(
-      subscribed:
-      false, // false because we want to fetch episodes from remote, not from database only
-      feedId: feedId,
-      showRead: true,
-    )
-        .first;
-
-    final Set<int> currentEpisodeIds =
-    currentLocalEpisodes.map((ep) => ep.eId).toSet();
-
-    final List<EpisodeEntity> newEpisodes = currentEpisodesOnRemote
-        .where((episode) => !currentEpisodeIds.contains(episode.eId))
-        .toList()
-      ..sort((a, b) => a.datePublished.compareTo(b.datePublished));
-
-    return newEpisodes;
+  Future<List<EpisodeEntity>> getNewEpisodesByFeedId({required int feedId}) {
+    return episodeRepository.getNewEpisodesByFeedId(feedId: feedId);
   }
 }
