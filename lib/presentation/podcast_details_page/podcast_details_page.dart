4a278672929b35003da01974a0dbf851ca9b1598
bruno <1201ba@gmail.com>
2025-03-26 16:47:31 +0100

    Add dialog to handle podcast detail loading failure
    Adds a failure dialog to be shown when loading the podcast details fails for unsubscribed podcasts.

--- lib/presentation/podcast_details_page/podcast_details_page.dart
+++ lib/presentation/podcast_details_page/podcast_details_page.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:podcast/presentation/custom_widgets/failure_widget.dart';
 
 import 'package:podcast/presentation/podcast_details_page/widgets/podcast_category.dart';
 import 'package:podcast/presentation/podcast_details_page/widgets/row_icon_buttons_podcasts.dart';
 import '../../application/podcast_bloc/podcast_bloc.dart';
+import '../custom_widgets/failure_dialog.dart';
 import '../custom_widgets/flexible_space.dart';
 
 class PodcastDetailsPage extends StatelessWidget {
@@ -12,102 +12,110 @@ class PodcastDetailsPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    return BlocListener<PodcastBloc, PodcastState>(
+      listener: (context, state) {
+        if (state.status == PodcastStatus.failure &&
+            !state.currentPodcast.subscribed) {
+          showDialog(
+            context: context,
+            builder: (context) => const FailureDialog(
+                message: "Error loading podcast details. Please try again."),
+          ).whenComplete(() {
+            if (context.mounted) {
+              Navigator.pop(context);
+            }
+          });
+        }
+      },
+      child: _buildPage(context),
+    );
+  }
+
+  Scaffold _buildPage(BuildContext context) {
+    PodcastState state = context.watch<PodcastBloc>().state;
     return Scaffold(
-      body: BlocBuilder<PodcastBloc, PodcastState>(
-        builder: (context, state) {
-          return state.status == PodcastStatus.loading
-              ? const Center(
-                  child: CircularProgressIndicator(),
-                )
-              : state.status == PodcastStatus.failure && !state.currentPodcast.subscribed
-                  ? buildFailureWidget(
-                      message: "Error loading podcast details.")
-                  : SafeArea(
-                      child: CustomScrollView(
-                        slivers: [
-                          FlexibleSpace(
-                            podcast: state.currentPodcast,
-                            episode: null,
-                            title: state.currentPodcast.title,
-                          ),
-                          SliverPadding(
-                              padding: const EdgeInsets.fromLTRB(
-                                  10.0, 0.0, 20.0, 10.0),
-                              sliver: SliverToBoxAdapter(
-                                child: Column(
-                                  mainAxisAlignment: MainAxisAlignment.start,
-                                  crossAxisAlignment: CrossAxisAlignment.start,
-                                  children: [
-                                    ...state.currentPodcast.categories
-                                        .map((value) => PodcastCategory(
-                                              value: value,
-                                            )),
-                                  ],
-                                ),
-                              )),
-                          SliverPadding(
-                            padding: const EdgeInsets.fromLTRB(
-                                40.0, 10.0, 40.0, 10.0),
-                            sliver: SliverToBoxAdapter(
-                              child: RowIconButtonsPodcasts(
-                                  podcast: state.currentPodcast),
+      body: state.status == PodcastStatus.loading
+          ? const Center(
+              child: CircularProgressIndicator(),
+            )
+          : SafeArea(
+              child: CustomScrollView(
+                slivers: [
+                  FlexibleSpace(
+                    podcast: state.currentPodcast,
+                    episode: null,
+                    title: state.currentPodcast.title,
+                  ),
+                  SliverPadding(
+                      padding: const EdgeInsets.fromLTRB(10.0, 0.0, 20.0, 10.0),
+                      sliver: SliverToBoxAdapter(
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            ...state.currentPodcast.categories
+                                .map((value) => PodcastCategory(
+                                      value: value,
+                                    )),
+                          ],
+                        ),
+                      )),
+                  SliverPadding(
+                    padding: const EdgeInsets.fromLTRB(40.0, 10.0, 40.0, 10.0),
+                    sliver: SliverToBoxAdapter(
+                      child:
+                          RowIconButtonsPodcasts(podcast: state.currentPodcast),
+                    ),
+                  ),
+                  SliverPadding(
+                    padding: const EdgeInsets.fromLTRB(20.0, 0.0, 20.0, 20.0),
+                    sliver: SliverToBoxAdapter(
+                      child: Text(
+                        state.currentPodcast.description,
+                        style: const TextStyle(
+                          fontSize: 16,
+                        ),
+                      ),
+                    ),
+                  ),
+                  SliverPadding(
+                    padding: const EdgeInsets.fromLTRB(20.0, 0.0, 20.0, 100.0),
+                    sliver: SliverToBoxAdapter(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            state.currentPodcast.author,
+                            style: const TextStyle(
+                              fontSize: 16,
                             ),
                           ),
-                          SliverPadding(
-                            padding: const EdgeInsets.fromLTRB(
-                                20.0, 0.0, 20.0, 20.0),
-                            sliver: SliverToBoxAdapter(
-                              child: Text(
-                                state.currentPodcast.description,
-                                style: const TextStyle(
-                                  fontSize: 16,
+                          Padding(
+                            padding: const EdgeInsets.symmetric(vertical: 10.0),
+                            child: Row(
+                              children: [
+                                const Icon(
+                                  Icons.language,
                                 ),
-                              ),
-                            ),
-                          ),
-                          SliverPadding(
-                            padding: const EdgeInsets.fromLTRB(
-                                20.0, 0.0, 20.0, 100.0),
-                            sliver: SliverToBoxAdapter(
-                              child: Column(
-                                crossAxisAlignment: CrossAxisAlignment.start,
-                                children: [
-                                  Text(
-                                    state.currentPodcast.author,
-                                    style: const TextStyle(
-                                      fontSize: 16,
-                                    ),
+                                const SizedBox(
+                                  width: 10,
+                                ),
+                                Text(
+                                  state.currentPodcast.language.toUpperCase(),
+                                  style: const TextStyle(
+                                    fontSize: 16,
                                   ),
-                                  Padding(
-                                    padding: const EdgeInsets.symmetric(
-                                        vertical: 10.0),
-                                    child: Row(
-                                      children: [
-                                        const Icon(
-                                          Icons.language,
-                                        ),
-                                        const SizedBox(
-                                          width: 10,
-                                        ),
-                                        Text(
-                                          state.currentPodcast.language
-                                              .toUpperCase(),
-                                          style: const TextStyle(
-                                            fontSize: 16,
-                                          ),
-                                        ),
-                                      ],
-                                    ),
-                                  ),
-                                ],
-                              ),
+                                ),
+                              ],
                             ),
                           ),
                         ],
                       ),
-                    );
-        },
-      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
     );
   }
 }
