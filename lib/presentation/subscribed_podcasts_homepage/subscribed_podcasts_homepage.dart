059ddefd07752afbf99bc639375dba21d5b96c26
bruno <1201ba@gmail.com>
2025-03-26 16:46:55 +0100

    Refactor podcast page listeners and add failure dialog
    - Move player and connectivity listeners to separate files.
    - Remove player state listener that was managing episode completion.
    - Add a listener for the podcast bloc to display a failure dialog in case of errors.
    - Refactor the subscribed podcast page build.

--- lib/presentation/subscribed_podcasts_homepage/subscribed_podcasts_homepage.dart
+++ lib/presentation/subscribed_podcasts_homepage/subscribed_podcasts_homepage.dart
@@ -1,14 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:just_audio/just_audio.dart';
 
-import '../../application/episode_playback_cubit/episode_playback_cubit.dart';
+import '../../helpers/listeners/player_listener.dart';
+import '../../helpers/listeners/connectivity_listener.dart';
 import '../../application/podcast_bloc/podcast_bloc.dart';
-import '../../helpers/core/connectivity_manager.dart';
-import '../../helpers/player/audiohandler.dart';
-import '../../injection.dart';
-import '../audioplayer_overlays/audioplayer_overlays.dart';
-import '../custom_widgets/failure_widget.dart';
+import '../custom_widgets/failure_dialog.dart';
 import '../custom_widgets/page_transition.dart';
 import 'widgets/subscribed_podcast_card.dart';
 import '../podcasts_search_page/podcasts_search_page.dart';
@@ -21,105 +17,63 @@ class SubscribedPodcastsHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     // Listen for player state changes (e.g., playing, paused, buffering)
-    getIt<MyAudioHandler>().player.playerStateStream.listen((playerState) {
-      final processingState = playerState.processingState;
+    listenToPlayer(context);
+    // Listen for connectivity changes
+    listenToConnectivity(context);
 
-      if (processingState == ProcessingState.completed) {
-        getIt<MyAudioHandler>().stop();
-        removeOverlay();
-        if (context.mounted) {
-          BlocProvider.of<EpisodePlaybackCubit>(context)
-              .setPlaybackEpisode(null);
-        }
-      }
-    });
-
-    getIt<ConnectivityManager>().connectionType.listen((type) {
-      if (type == ConnectionType.none) {
-        if (context.mounted) {
+    return BlocListener<PodcastBloc, PodcastState>(
+      listener: (context, state) {
+        if (state.status == PodcastStatus.failure) {
           showDialog(
             context: context,
-            barrierDismissible: false,
-            builder: (context) => AlertDialog(
-              title: const Text('No Internet Connection'),
-              content: const Text('Please check your internet connection.'),
-              actions: [
-                TextButton(
-                  child: const Text('OK'),
-                  onPressed: () {
-                    Navigator.of(context).pop();
-                  },
-                )
-              ],
-            ),
+            builder: (context) => const FailureDialog(
+                message: "'Unexpected error. Please restart the app."),
           );
         }
-      }
-      if (type == ConnectionType.mobile) {
-        if (context.mounted) {
-          showDialog(
-            context: context,
-            barrierDismissible: false,
-            builder: (context) => AlertDialog(
-              title: const Text('Mobile Data Detected'),
-              content: const Text(
-                  'You are currently using mobile data. Downloading may incur costs.'),
-              actions: [
-                TextButton(
-                  child: const Text('Ok'),
-                  onPressed: () {
-                    Navigator.of(context).pop();
-                  },
-                )
-              ],
-            ),
-          );
-        }
-      }
-    });
-
-    return Scaffold(
-      body: SafeArea(
-        child: BlocBuilder<PodcastBloc, PodcastState>(
-          builder: (context, state) {
-            return Padding(
-              padding: const EdgeInsets.symmetric(horizontal: _spacing),
-              child: CustomScrollView(
-                physics: const BouncingScrollPhysics(),
-                slivers: [
-                  SliverToBoxAdapter(
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Text(
-                          "Podcasts",
-                          style: Theme.of(context).textTheme.displayLarge,
-                        ),
-                        IconButton(
-                          onPressed: () {
-                            Navigator.push(
-                              context,
-                              ScaleRoute(
-                                page: const PodcastsSearchPage(),
-                              ),
-                            );
-                          },
-                          icon: const Icon(Icons.search_rounded),
-                        ),
-                      ],
-                    ),
-                  ),
-                  _buildPodcastGrid(state),
-                ],
-              ),
-            );
-          },
-        ),
-      ),
+      },
+      child: _buildPage(context),
     );
   }
 
-  Widget _buildPodcastGrid(PodcastState state) {
+  Scaffold _buildPage(BuildContext context) {
+    return Scaffold(
+        body: SafeArea(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: _spacing),
+        child: CustomScrollView(
+          physics: const BouncingScrollPhysics(),
+          slivers: [
+            SliverToBoxAdapter(
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    "Podcasts",
+                    style: Theme.of(context).textTheme.displayLarge,
+                  ),
+                  IconButton(
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        ScaleRoute(
+                          page: const PodcastsSearchPage(),
+                        ),
+                      );
+                    },
+                    icon: const Icon(Icons.search_rounded),
+                  ),
+                ],
+              ),
+            ),
+            _buildMainContent(context),
+          ],
+        ),
+      ),
+    ));
+  }
+
+  Widget _buildMainContent(BuildContext context) {
+    PodcastState state = context.watch<PodcastBloc>().state;
     // Define the empty state widget
     const emptyStateWidget = SliverToBoxAdapter(
       child: Column(
@@ -169,13 +123,8 @@ class SubscribedPodcastsHomePage extends StatelessWidget {
     // Build the grid based on the state
     return state.status == PodcastStatus.loading
         ? buildCircularProgressIndicator()
-        : state.status == PodcastStatus.failure
-            ? SliverToBoxAdapter(
-                child: buildFailureWidget(
-                    message: 'Unexpected error. Please restart the app.'),
-              )
-            : state.subscribedPodcasts.isEmpty
-                ? emptyStateWidget
-                : buildGrid(state.subscribedPodcasts);
+        : state.subscribedPodcasts.isEmpty
+            ? emptyStateWidget
+            : buildGrid(state.subscribedPodcasts);
   }
 }
