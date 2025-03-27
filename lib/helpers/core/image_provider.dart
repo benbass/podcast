6d9e70e6455394b997dc325d7b89f6fba335e561
bruno <1201ba@gmail.com>
2025-03-27 13:51:48 +0100

    Handle invalid image URLs, invalid image data (!) and file paths.

--- lib/helpers/core/image_provider.dart
+++ lib/helpers/core/image_provider.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'dart:io';
 import 'package:http/http.dart' as http;
 
 class MyImageProvider {
@@ -11,18 +13,30 @@ class MyImageProvider {
       return const AssetImage("assets/placeholder.png");
     }
 
-    final response = await http.head(Uri.parse(url));
+    try {
+      if (url.startsWith('http')) {
+        final response = await http.head(Uri.parse(url));
+        if (response.statusCode == 404 || response.statusCode != 200) {
+          return const AssetImage("assets/placeholder.png");
+        }
+          try {
+            //check if it is a valid image
+            final data = await NetworkAssetBundle(Uri.parse(url)).load(url);
+            await decodeImageFromList(data.buffer.asUint8List());
+            // url is a valid image url
+            return NetworkImage(url);
+          } catch (_) {
+            return const AssetImage("assets/placeholder.png");
+          }
 
-    if (response.statusCode == 404) {
+      } else {
+        // url is a file path when podcast is subscribed
+        File(url);
+        return FileImage(File(url));
+      }
+    } catch (_) {
       return const AssetImage("assets/placeholder.png");
     }
-
-    if (response.statusCode != 200) {
-        // Handle other non-200 status codes (e.g., 500) if needed
-        return const AssetImage("assets/placeholder.png");
-      }
-
-    return NetworkImage(url);
 
   }
 
