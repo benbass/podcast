0cc7898fe7e5730cea8d240a993e71eda6046df1
bruno <1201ba@gmail.com>
2025-03-29 17:00:30 +0100

    Add podcast title parameter to episode repository methods

--- lib/domain/repositories/episode_repository.dart
+++ lib/domain/repositories/episode_repository.dart
@@ -4,8 +4,9 @@ abstract class EpisodeRepository {
   Stream<List<EpisodeEntity>> getEpisodes({
     required bool subscribed,
     required int feedId,
+    required String podcastTitle,
     required bool showRead,
   });
-  Future<List<EpisodeEntity>> getNewEpisodesByFeedId({required int feedId});
+  Future<List<EpisodeEntity>> getNewEpisodesByFeedId({required int feedId, required String podcastTitle});
   Stream<int> unreadLocalEpisodesCount({required int feedId});
 }
