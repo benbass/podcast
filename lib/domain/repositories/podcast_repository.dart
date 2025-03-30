b5a34f0dcd4c376fe2642a3eeda97f309bff446c
bruno <1201ba@gmail.com>
2025-03-30 16:47:05 +0200

    Add method to fetch podcast by feed ID

--- lib/domain/repositories/podcast_repository.dart
+++ lib/domain/repositories/podcast_repository.dart
@@ -2,6 +2,7 @@ import 'package:podcast/domain/entities/podcast_entity.dart';
 
 abstract class PodcastRepository {
   Future<List<PodcastEntity>> fetchPodcastsByKeywords(String keyword);
+  Future<PodcastEntity> fetchPodcastByFeedId(int feedId);
   Future<bool> subscribeToPodcast(PodcastEntity podcast);
   Future<void>  unsubscribeFromPodcast(PodcastEntity podcast);
   Future<List<PodcastEntity>> getSubscribedPodcasts();
