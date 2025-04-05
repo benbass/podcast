5de3a2a6317663a8d7d65236c8cabf2e6451bcbe
bruno <1201ba@gmail.com>
2025-04-05 16:29:22 +0200

    Refactor EpisodeProgressIndicatorOverlay to use CircularProgressIndicator

--- lib/presentation/custom_widgets/episode_progress_indicator_overlay.dart
+++ lib/presentation/custom_widgets/episode_progress_indicator_overlay.dart
@@ -9,62 +9,59 @@ class EpisodeProgressIndicatorOverlay extends StatelessWidget {
   final EpisodeEntity episode;
   final bool isCurrentlyPlaying;
   final EpisodeEntity? currentlyPlayingEpisode;
-  final double overlayHeight;
-  final double overlayWidth;
+  final double? paddingHoriz;
+  final double? paddingVert;
+  final double? dimension;
+  final double? strokeWidth;
   const EpisodeProgressIndicatorOverlay({
     super.key,
     required this.themeData,
     required this.episode,
     required this.isCurrentlyPlaying,
-    required this.overlayHeight,
-    required this.overlayWidth,
     required this.currentlyPlayingEpisode,
-  });
+    this.paddingHoriz,
+    this.paddingVert,
+    this.dimension,
+    this.strokeWidth,});
 
   @override
   Widget build(BuildContext context) {
-    return StreamBuilder<Duration>(
-      stream: getIt<MyAudioHandler>().player.positionStream,
-      builder: (context, snapshot) {
-        if (snapshot.hasData &&
-            isCurrentlyPlaying &&
-            currentlyPlayingEpisode != null) {
-          final currentDuration = snapshot.data!;
-          final totalDuration =
-              Duration(seconds: currentlyPlayingEpisode!.duration!);
-          final progress =
-              currentDuration.inMilliseconds / totalDuration.inMilliseconds;
-          return Positioned(
-            top: 0,
-            left: 0,
-            child: SizedBox(
-              height: overlayHeight,
-              width: overlayWidth,
-              child: LinearProgressIndicator(
-                value: progress.clamp(0.0, 1.0),
-                color: themeData.colorScheme.secondary.withValues(alpha: 0.4),
-                backgroundColor: Colors.transparent,
-              ),
-            ),
+    return Container(
+      padding: EdgeInsets.only(bottom: paddingVert ?? 4.0, left: paddingHoriz ?? 4.0),
+      height: dimension ?? 36,
+      width: dimension ?? 36,
+      child: StreamBuilder<Duration>(
+        stream: getIt<MyAudioHandler>().player.positionStream,
+        builder: (context, snapshot) {
+          double progress = _calculateProgress(snapshot);
+          Color backgroundColor = _getBackgroundColor(progress);
+          return CircularProgressIndicator(
+            strokeWidth: strokeWidth ?? 3.0,
+            strokeCap: StrokeCap.round,
+            value: progress,
+            color: themeData.colorScheme.secondary,
+            backgroundColor: backgroundColor,
           );
-        } else {
-          return Positioned(
-            top: 0,
-            left: 0,
-            child: SizedBox(
-              height: overlayHeight,
-              width: overlayWidth,
-              child: LinearProgressIndicator(
-                value:
-                    (episode.position.toDouble() / episode.duration!.toDouble())
-                        .clamp(0.0, 1.0),
-                color: themeData.colorScheme.secondary.withValues(alpha: 0.4),
-                backgroundColor: Colors.transparent,
-              ),
-            ),
-          );
-        }
-      },
+        },
+      ),
     );
+  }
+
+  double _calculateProgress(AsyncSnapshot<Duration> snapshot) {
+    if (snapshot.hasData && isCurrentlyPlaying && currentlyPlayingEpisode != null) {
+      final currentPosition = snapshot.data!;
+      final totalDuration = Duration(seconds: currentlyPlayingEpisode!.duration!);
+      return (currentPosition.inMilliseconds / totalDuration.inMilliseconds).clamp(0.0, 1.0);
+    } else if (episode.duration != null && episode.duration! > 0) {
+      return (episode.position / episode.duration!).clamp(0.0, 1.0);
+    }
+    return 0.0;
+  }
+
+  Color _getBackgroundColor(double progress) {
+    if (progress > 0 || (episode.position > 0 && episode.duration != null && episode.duration! > 0)) {
+      return themeData.colorScheme.primary.withValues(alpha: 0.2);
+    }
+    return Colors.transparent;
   }
 }
