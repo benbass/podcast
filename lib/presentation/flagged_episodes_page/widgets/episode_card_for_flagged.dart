ba63c6642c3f48b9e954c06ea323a7b300004491
bruno <1201ba@gmail.com>
2025-04-05 16:25:08 +0200

    Add widget EpisodeCardForFlagged: just returns common EpisodeCard widget

---
+++ lib/presentation/flagged_episodes_page/widgets/episode_card_for_flagged.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:podcast/presentation/custom_widgets/episode_card.dart';
+
+import '../../../domain/entities/episode_entity.dart';
+import '../../../domain/entities/podcast_entity.dart';
+
+class EpisodeCardForFlagged extends StatelessWidget {
+  final EpisodeEntity episode;
+  final PodcastEntity podcast;
+  const EpisodeCardForFlagged({
+    super.key,
+    required this.episode,
+    required this.podcast,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return EpisodeCard(episode: episode, podcast: podcast);
+  }
+}
