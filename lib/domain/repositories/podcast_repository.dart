7f38f7af75a8c7fe748dc6a79545db9ebe6d544e
bruno <1201ba@gmail.com>
2025-04-11 10:55:12 +0200

    Add a method to fetch trending podcasts.

--- lib/domain/repositories/podcast_repository.dart
+++ lib/domain/repositories/podcast_repository.dart
@@ -2,6 +2,7 @@ import 'package:podcast/domain/entities/podcast_entity.dart';
 
 abstract class PodcastRepository {
   Future<List<PodcastEntity>> fetchPodcastsByKeywords(String keyword);
+  Future<List<PodcastEntity>> fetchTrendingPodcasts();
   Future<PodcastEntity> fetchPodcastByFeedId(int feedId);
   Future<bool> subscribeToPodcast(PodcastEntity podcast);
   Future<void>  unsubscribeFromPodcast(PodcastEntity podcast);
