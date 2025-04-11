eebe06279e4faf519dbd907ca93850d01dd052ba
bruno <1201ba@gmail.com>
2025-04-11 10:56:07 +0200

    Feature: Fetch trending podcasts and refactor fetchPodcastByEntryPoint

--- lib/infrastructure/datasources/podcast_datasources.dart
+++ lib/infrastructure/datasources/podcast_datasources.dart
@@ -1,4 +1,5 @@
 import 'dart:convert';
+import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'package:podcast/core/globals.dart';
 
@@ -8,10 +9,14 @@ import '../models/podcast_model.dart';
 
 abstract class PodcastDataSource {
   /// Fetches podcasts based on a keyword, considering subscribed podcasts.
-  Future<List<PodcastEntity>> fetchPodcastsByKeyword(
-      String keyword);
+  Future<List<PodcastEntity>> fetchPodcastsByKeyword(String keyword);
+
+  /// Fetch trending podcasts
+  Future<List<PodcastEntity>> fetchTrendingPodcasts();
+
   /// Fetch a podcast by its known feedId
   Future<PodcastEntity> fetchPodcastByFeedId(int feedId);
+
   /// Retrieves the list of subscribed podcasts.
   Future<List<PodcastEntity>?> getSubscribedPodcasts();
 }
@@ -21,21 +26,13 @@ class PodcastDataSourceImpl implements PodcastDataSource {
 
   PodcastDataSourceImpl({required this.httpClient});
 
-  /// Fetches podcasts based on a keyword, considering subscribed podcasts.
-  @override
-  Future<List<PodcastEntity>> fetchPodcastsByKeyword(
-      String keyword) async {
-    // Prepare keyword for url
-    String encodedKeyword = _encodeKeywordForUrl(keyword);
-
+  Future<List<PodcastEntity>> _fetchPodcastsByEntryPoint(
+      String entryPoint) async {
     // Authorization headers
     Map<String, String> headers = headersForAuth();
 
-    final Uri uri = Uri.parse('$baseUrl/search/byterm?q=$encodedKeyword&pretty&max=1000');
-
-    final response = await httpClient.get(
-        uri,
-        headers: headers);
+    final Uri uri = Uri.parse('$baseUrl/$entryPoint');
+    final response = await httpClient.get(uri, headers: headers);
 
     if (response.statusCode == 200) {
       // Parse the JSON response if the server returns a 200 OK status code.
@@ -44,15 +41,48 @@ class PodcastDataSourceImpl implements PodcastDataSource {
       // Convert the JSON feeds to PodcastModel objects, then explicitly convert them to PodcastEntity objects.
       // Explicit conversion is necessary because the list of podcasts can change during the application's lifetime.
       // Mixing PodcastEntity and PodcastModel types would lead to inconsistencies.
-      final List<PodcastEntity> foundPodcasts =
-          feeds.map((feed) => PodcastModel.fromJson(feed).toPodcastEntity()).toList();
-      // Merge the found podcasts with subscribed podcasts, giving precedence to subscribed ones.
-      return _mergeWithSubscribedPodcasts(foundPodcasts);
+      final List<PodcastEntity> podcasts = feeds
+          .map((feed) => PodcastModel.fromJson(feed).toPodcastEntity())
+          .toList();
+
+      if (entryPoint.contains('search/byterm')) {
+        // Merge the found podcasts with subscribed podcasts, giving precedence to subscribed ones.
+        return _mergeWithSubscribedPodcasts(podcasts);
+      } else {
+        return podcasts;
+      }
     } else {
+      String errorMessage = '';
+      if (entryPoint.contains('search/byterm')) {
+        errorMessage =
+            'Failed to load podcasts by keyword: Status code ${response.statusCode}';
+      } else {
+        errorMessage =
+            'Failed to load trending podcasts: Status code ${response.statusCode}';
+      }
       // If the server did not return a 200 OK response,
       // then throw an exception.
-      throw Exception('Failed to load podcasts by keyword: Status code ${response.statusCode}');
+      throw Exception(errorMessage);
     }
+  }
+
+  /// Fetches podcasts based on a keyword, considering subscribed podcasts.
+  @override
+  Future<List<PodcastEntity>> fetchPodcastsByKeyword(String keyword) async {
+    // Prepare keyword for url
+    String encodedKeyword = _encodeKeywordForUrl(keyword);
+    // fetch podcasts by keyword
+    String entryPoint = 'search/byterm?q=$encodedKeyword&pretty&max=1000';
+    return _fetchPodcastsByEntryPoint(entryPoint);
+  }
+
+  @override
+  Future<List<PodcastEntity>> fetchTrendingPodcasts() async {
+    String languageCode = "de";
+    String entryPoint = 'podcasts/trending?lang=$languageCode&pretty&max=10';
+    return _fetchPodcastsByEntryPoint(entryPoint);
+
+    // To do?: dynamically insert language code based on app language?
   }
 
   /// Fetch a podcast by its known feedId
@@ -62,22 +92,22 @@ class PodcastDataSourceImpl implements PodcastDataSource {
     Map<String, String> headers = headersForAuth();
     final Uri uri = Uri.parse('$baseUrl/podcasts/byfeedid?id=$feedId&pretty');
 
-    final response = await httpClient.get(
-        uri,
-        headers: headers);
+    final response = await httpClient.get(uri, headers: headers);
 
     if (response.statusCode == 200) {
       // Parse the JSON response if the server returns a 200 OK status code.
       final Map<String, dynamic> jsonFeed = json.decode(response.body);
       final feed = jsonFeed['feed'];
       // Convert the JSON feed to PodcastModel object: lookup by feed id returns only one podcast within "feed"
-      final PodcastEntity podcast = PodcastModel.fromJson(feed).toPodcastEntity();
+      final PodcastEntity podcast =
+          PodcastModel.fromJson(feed).toPodcastEntity();
       // Merge the found podcasts with subscribed podcasts, giving precedence to subscribed ones.
       return podcast;
     } else {
       // If the server did not return a 200 OK response,
       // then throw an exception.
-      throw Exception('Failed to load podcast by feed id: Status code ${response.statusCode}');
+      throw Exception(
+          'Failed to load podcast by feed id: Status code ${response.statusCode}');
     }
   }
 
@@ -85,7 +115,7 @@ class PodcastDataSourceImpl implements PodcastDataSource {
   @override
   Future<List<PodcastEntity>> getSubscribedPodcasts() async {
     List<PodcastEntity> podcasts = podcastBox.getAll();
-    return podcasts ;
+    return podcasts;
   }
 
   /// Merges the list of found podcasts with subscribed podcasts.
@@ -115,8 +145,6 @@ class PodcastDataSourceImpl implements PodcastDataSource {
     } else {
       return foundPodcasts;
     }
-
-
   }
 
   /// Encodes the keyword for use in a URL.
