b0e6e820082540846ed97cee0361337f0d9d14e4
bruno <1201ba@gmail.com>
2025-04-25 18:11:01 +0200

    Refactor episode update logic on flag change and improve state management

--- lib/helpers/database/handle_episode.dart
+++ lib/helpers/database/handle_episode.dart
@@ -6,25 +6,31 @@ import '../../core/globals.dart';
 import '../../domain/entities/episode_entity.dart';
 
 void updateEpisodeOnFlagChanged(BuildContext context, EpisodeEntity episode) {
+  final episodes = BlocProvider.of<EpisodesCubit>(context).state;
+  int index = episodes.indexWhere((element) => element.eId == episode.eId);
+
   // Get the latest episode version from db since we just changed a flag value
   final episodeDb = episodeBox.get(episode.id)!;
-  // We delete episode from db if it doesn't belong to any subscribed podcasts and all flag values are reset to initial
-  if ((episodeDb.isSubscribed == false) &
-  (episodeDb.favorite == false) &
-  (episodeDb.filePath == null) &
-  (episodeDb.position == 0)) {
-    episodeBox.remove(episodeDb.id);
-  }
-  if((episodeDb.isSubscribed == false) &
-  (episodeDb.favorite == true) ||
-      (episodeDb.filePath != null) ||
-      (episodeDb.position != 0)){
-    final episodes = BlocProvider.of<EpisodesCubit>(context).state;
+
+  updateState() {
     if (episodes.isNotEmpty) {
-      int index = episodes.indexWhere((element) => element.eId == episodeDb.eId);
       episodes.insert(index, episodeDb);
-      episodes.removeAt(index+1);
+      episodes.removeAt(index + 1);
       BlocProvider.of<EpisodesCubit>(context).setEpisodes(episodes);
     }
+  }
+
+  // We delete episode from db if it doesn't belong to any subscribed podcasts and all flag values are reset to initial
+  if ((episodeDb.isSubscribed == false) &
+      (episodeDb.favorite == false) &
+      (episodeDb.filePath == null) &
+      (episodeDb.position == 0)) {
+    episodeBox.remove(episodeDb.id);
+    updateState();
+  }
+  if ((episodeDb.isSubscribed == false) & (episodeDb.favorite == true) ||
+      (episodeDb.filePath != null) ||
+      (episodeDb.position != 0)) {
+    updateState();
   }
 }
