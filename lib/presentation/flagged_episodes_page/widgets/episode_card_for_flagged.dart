0fad6ff1665bdb1a4bdc5829d2fad6f51b71c13e
bruno <1201ba@gmail.com>
2025-04-25 16:06:38 +0200

    Pass flag to episode card.

--- lib/presentation/flagged_episodes_page/widgets/episode_card_for_flagged.dart
+++ lib/presentation/flagged_episodes_page/widgets/episode_card_for_flagged.dart
@@ -7,14 +7,16 @@ import '../../../domain/entities/podcast_entity.dart';
 class EpisodeCardForFlagged extends StatelessWidget {
   final EpisodeEntity episode;
   final PodcastEntity podcast;
+  final String flag;
   const EpisodeCardForFlagged({
     super.key,
     required this.episode,
     required this.podcast,
+    required this.flag,
   });
 
   @override
   Widget build(BuildContext context) {
-    return EpisodeCard(episode: episode, podcast: podcast);
+    return EpisodeCard(episode: episode, podcast: podcast, flag: flag);
   }
 }
