b07fc84bf7deab390e1a2f75eeaf7341b8a1800f
bruno <1201ba@gmail.com>
2025-05-01 14:01:08 +0200

    Remove ShowFlaggedListCubit

--- lib/application/show_flagged_list/show_flagged_list_cubit.dart
+++
@@ -1,9 +0,0 @@
-import 'package:bloc/bloc.dart';
-
-class ShowFlaggedListCubit extends Cubit<String?> {
-  ShowFlaggedListCubit() : super(null);
-
-  void setFlag(String? onlyFlagged){
-    emit(onlyFlagged);
-  }
-}
