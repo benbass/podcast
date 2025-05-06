40bf926ea9c2be398b0a9eb5eba1dcfcc1df12ba
bruno <1201ba@gmail.com>
2025-05-06 09:26:23 +0200

    Clean unsubscribed podcast data on app close and show error if failed.

--- lib/helpers/core/lifecycle_oberserver.dart
+++ lib/helpers/core/lifecycle_oberserver.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 
+import '../../core/globals.dart';
 import '../../injection.dart';
 import '../database/delete_episodes.dart';
+import '../database/delete_podcasts.dart';
 import '../player/audiohandler.dart';
 import 'connectivity_manager.dart';
 
@@ -9,11 +11,38 @@ class MyAppLifecycleObserver extends WidgetsBindingObserver {
 
   @override
   void didChangeAppLifecycleState(AppLifecycleState state) {
+    super.didChangeAppLifecycleState(state);
     if (state == AppLifecycleState.detached) {
       // App is being closed
       getIt<MyAudioHandler>().dispose();
       getIt<ConnectivityManager>().dispose();
+      // Clean database from objects related to unsubscribed podcasts
       deleteEpisodes();
+      final bool = deletePodcastsAndArtworkFiles();
+      if (!bool) {
+        _showErrorDialog("A problem occurred while cleaning the app storage from unneeded data\nWe will try again at the next app closing.");
+      }
     }
+  }
+
+  void _showErrorDialog(String errorMessage) {
+    // We are not in the Widget-tree anymore, so we use the global navigatorKey.
+    // We show an Overlay-Dialog over all other widgets.
+    navigatorKey.currentState?.overlay?.insert(
+      OverlayEntry(
+        builder: (context) => AlertDialog(
+          title: const Text('Error!'),
+          content: Text(errorMessage),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+              child: const Text('OK'),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
