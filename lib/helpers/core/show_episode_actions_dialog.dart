d1dd91a766df1b764de06299cd76b4c2b7e6dab6
bruno <1201ba@gmail.com>
2025-05-10 14:39:07 +0200

    _ Remove context parameter from episode action functions. - Use new AudioDownloadService for download action

--- lib/helpers/core/show_episode_actions_dialog.dart
+++ lib/helpers/core/show_episode_actions_dialog.dart
@@ -1,21 +1,25 @@
 import 'package:flutter/material.dart';
-import 'package:podcast/helpers/core/perform_action_on_episode.dart';
+import 'package:podcast/helpers/core/audio_download_service.dart';
 
+import 'package:podcast/helpers/core/perform_action_on_episode.dart';
 import '../../domain/entities/episode_entity.dart';
 import '../../presentation/custom_widgets/action_feedback/action_feedback.dart';
 
 void showEpisodeActionsDialog(BuildContext context, EpisodeEntity episode) {
+  // Reset for debugging purposes
+ /* episode.filePath = null;
+  episodeBox.put(episode);*/
+
   final List<Map<String, dynamic>> menuItems = [
     if (episode.isSubscribed)
       {
         "title": episode.read ? "Unmark as read" : "Mark as read",
         "onPressed": () {
           final bool isRead = episode.read;
-          performActionOnEpisode(episode, "read", isRead, context);
+          performActionOnEpisode(episode, "read", isRead);
           Navigator.pop(context);
           ActionFeedback.show(context,
-              icon:
-              episode.read ? Icons.check : Icons.radio_button_unchecked);
+              icon: episode.read ? Icons.check : Icons.radio_button_unchecked);
         }
       },
     {"title": "Share", "onPressed": () {}},
@@ -23,7 +27,7 @@ void showEpisodeActionsDialog(BuildContext context, EpisodeEntity episode) {
       "title": episode.favorite ? "Unmark as favorite" : "Mark as favorite",
       "onPressed": () {
         final bool isFavorite = episode.favorite;
-        performActionOnEpisode(episode, "favorite", isFavorite, context);
+        performActionOnEpisode(episode, "favorite", isFavorite);
         Navigator.pop(context);
         ActionFeedback.show(context,
             icon: episode.favorite ? Icons.star : Icons.star_border);
@@ -32,17 +36,8 @@ void showEpisodeActionsDialog(BuildContext context, EpisodeEntity episode) {
     {
       "title": episode.filePath == null ? "Download" : "Delete",
       "onPressed": () {
-        // TODO: Handle download/delete
-        String? filePath = episode.filePath;
-        if (filePath == null) {
-          // implement logic for download and set filePath as String for performActionOnEpisode
-          filePath = "file path on device";
-        } else {
-          // implement logic for delete and set filePath as null for performActionOnEpisode
-          filePath = null;
-        }
-        performActionOnEpisode(episode, "download", filePath, context);
-        Navigator.pop(context);
+        AudioDownloadService audioDownloadService = AudioDownloadService(episode);
+        audioDownloadService.showDownloadDialog();
       },
     },
   ];
