cf00d1ddd0d1e167004b4526f1c092991015da96
bruno <1201ba@gmail.com>
2025-05-10 14:34:53 +0200

    Remove unused audio handler and add a download notification channel.

--- lib/helpers/notifications/initialize_awesome_notifications.dart
+++ lib/helpers/notifications/initialize_awesome_notifications.dart
@@ -1,10 +1,8 @@
 import 'package:awesome_notifications/awesome_notifications.dart';
 import 'package:flutter/material.dart';
-import 'package:podcast/helpers/player/audiohandler.dart';
-
 import 'notifications_controller.dart';
 
-void initAwesomeNotifications(MyAudioHandler audioHandler) {
+void initAwesomeNotifications() {
   AwesomeNotifications().initialize(
     // set the icon to null if you want to use the default app icon
     null,
@@ -12,10 +10,24 @@ void initAwesomeNotifications(MyAudioHandler audioHandler) {
       NotificationChannel(
         channelGroupKey: 'basic_channel_group',
         channelKey: 'basic_channel',
-        channelName: 'Orange Podcast playing',
-        channelDescription: 'Notification channel for the Orange Podcast Player',
+        channelName: 'Podcast playing',
+        channelDescription: 'Notification channel for the Podcast Player',
         defaultColor: const Color(0xFFFF8100),
-        //ledColor: Colors.white,
+        playSound: false,
+        enableVibration: false,
+        importance: NotificationImportance.High,
+        channelShowBadge: false,
+        locked: true,
+        defaultPrivacy: NotificationPrivacy.Public,
+        icon: "resource://drawable/launcher_icon",
+      ),
+      NotificationChannel(
+        groupKey: 'basic_channel_group',
+        channelGroupKey: 'basic_channel_group',
+        channelKey: 'progress_channel',
+        channelName: 'Podcast downloading',
+        channelDescription: 'Notification channel for the Podcast Player Downloads',
+        defaultColor: const Color(0xFFFF8100),
         playSound: false,
         enableVibration: false,
         importance: NotificationImportance.High,
@@ -31,11 +43,9 @@ void initAwesomeNotifications(MyAudioHandler audioHandler) {
   // Only after at least the action method is set, the notification events are delivered
   AwesomeNotifications().setListeners(
     onActionReceivedMethod: NotificationController.onActionReceivedMethod,
-    /*
-    onNotificationCreatedMethod: NotificationController.onNotificationCreatedMethod,
+    //onNotificationCreatedMethod: NotificationController.onNotificationCreatedMethod,
     onDismissActionReceivedMethod: NotificationController.onDismissActionReceivedMethod,
-    onNotificationDisplayedMethod: NotificationController.onNotificationDisplayedMethod,
-    */
+    //onNotificationDisplayedMethod: NotificationController.onNotificationDisplayedMethod,
   );
 
   ///TODO: Check if user granted permmission for notifications
