e2cc7d5b57f7145a561a52828dde69b9b7795b9e
bruno <1201ba@gmail.com>
2025-05-11 14:56:07 +0200

    Rename connectivity listener file and move it to the dialogs folder.

---
+++ lib/presentation/custom_widgets/dialogs/connectivity_dialogs.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+
+import '../../../injection.dart';
+import '../../../helpers/core/connectivity_manager.dart';
+
+class ConnectivityDialog {
+  static void showConnectivityDialogs(BuildContext context){
+    getIt<ConnectivityManager>().connectionType.listen((type) {
+      if (type == ConnectionType.none) {
+        if (context.mounted) {
+          showDialog(
+            context: context,
+            barrierDismissible: false,
+            builder: (context) => AlertDialog(
+              title: const Text('No Internet Connection'),
+              content: const Text('Please check your internet connection.'),
+              actions: [
+                TextButton(
+                  child: const Text('OK'),
+                  onPressed: () {
+                    Navigator.of(context).pop();
+                  },
+                )
+              ],
+            ),
+          );
+        }
+      }
+      if (type == ConnectionType.mobile) {
+        if (context.mounted) {
+          showDialog(
+            context: context,
+            barrierDismissible: false,
+            builder: (context) => AlertDialog(
+              title: const Text('Mobile Data Detected'),
+              content: const Text(
+                  'You are currently using mobile data. Downloading may incur costs.'),
+              actions: [
+                TextButton(
+                  child: const Text('Ok'),
+                  onPressed: () {
+                    Navigator.of(context).pop();
+                  },
+                )
+              ],
+            ),
+          );
+        }
+      }
+    });
+  }
+}
