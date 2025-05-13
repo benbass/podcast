a6de21f381b1c7129a62da621c5e1177e021ba66
bruno <1201ba@gmail.com>
2025-05-13 18:10:52 +0200

    Initialize `AudioDownloadQueueManager` and set "Show Queue" notification action.

--- lib/main.dart
+++ lib/main.dart
@@ -12,15 +12,18 @@ import 'package:podcast/application/textfield_cubit/text_field_cubit.dart';
 import 'package:podcast/domain/entities/episode_entity.dart';
 import 'package:podcast/domain/entities/podcast_entity.dart';
 import 'package:podcast/helpers/notifications/notifications_controller.dart';
+import 'package:podcast/presentation/audio_download_queue_page/audio_download_queue_page.dart';
 import 'package:podcast/presentation/episodes_list_page/episodes_list_page.dart';
 import 'package:podcast/presentation/homepage/homepage.dart';
 import 'package:podcast/presentation/podcast_details_page/podcast_details_page.dart';
 import 'package:podcast/presentation/podcasts_search_page/podcasts_search_page.dart';
 import 'package:podcast/theme.dart';
+import 'package:provider/provider.dart';
 import 'application/episode_playback_cubit/episode_playback_cubit.dart';
 import 'application/podcast_bloc/podcast_bloc.dart';
 import 'core/globals.dart';
 import 'core/objectbox.dart';
+import 'helpers/audio_download/audio_download_queue_manager.dart';
 import 'helpers/core/lifecycle_oberserver.dart';
 import 'injection.dart' as di;
 import 'injection.dart';
@@ -46,6 +49,14 @@ void main() async {
   UtilitiesNotifications.initAwesomeNotifications();
   // Initialize the notification controller
   NotificationController();
+  // Initialize the Queue Manager
+  final audioDownloadQueueManager = AudioDownloadQueueManager();
+
+  // Set the callback for the "Show Queue" action in the notification
+  audioDownloadQueueManager.onShowQueuePage = () {
+    MyApp.navigatorKey.currentState?.push(MaterialPageRoute(builder: (_) => const AudioDownloadQueuePage()));
+  };
+
 
   // Design status and bottom bar
   SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(
@@ -59,17 +70,20 @@ void main() async {
 
   SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]).then(
     (value) => runApp(
-      MultiBlocProvider(
-        providers: [
-          BlocProvider(
-              create: (BuildContext context) => getIt<EpisodePlaybackCubit>()),
-          BlocProvider(
-              create: (BuildContext context) => getIt<TextFieldCubit>()),
-          BlocProvider(
-              create: (BuildContext context) =>
-                  getIt<PodcastBloc>()..add(LoadSubscribedPodcastsEvent())),
-        ],
-        child: const MyApp(),
+      ChangeNotifierProvider(
+        create: (_) => audioDownloadQueueManager,
+        child: MultiBlocProvider(
+          providers: [
+            BlocProvider(
+                create: (BuildContext context) => getIt<EpisodePlaybackCubit>()),
+            BlocProvider(
+                create: (BuildContext context) => getIt<TextFieldCubit>()),
+            BlocProvider(
+                create: (BuildContext context) =>
+                    getIt<PodcastBloc>()..add(LoadSubscribedPodcastsEvent())),
+          ],
+          child: const MyApp(),
+        ),
       ),
     ),
   );
