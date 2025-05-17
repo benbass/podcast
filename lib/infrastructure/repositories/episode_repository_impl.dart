e3b5e05f1a3338db5ded3a7569631a0188d1ac07
bruno <1201ba@gmail.com>
2025-05-17 18:36:39 +0200

    Add filterText parameter to getEpisodes method.

--- lib/infrastructure/repositories/episode_repository_impl.dart
+++ lib/infrastructure/repositories/episode_repository_impl.dart
@@ -18,6 +18,7 @@ class EpisodeRepositoryImpl implements EpisodeRepository {
     required String podcastTitle,
     required String filterStatus,
     required bool refresh,
+    String? filterText,
   }) {
     return episodeLocalDatasource.getEpisodes(
       subscribed: subscribed,
@@ -25,6 +26,7 @@ class EpisodeRepositoryImpl implements EpisodeRepository {
       podcastTitle: podcastTitle,
       filterStatus: filterStatus,
       refresh: refresh,
+      filterText: filterText,
     );
   }
 
