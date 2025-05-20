764a9596cee1718e7bb4f6c4c914a414096aff09
bruno <1201ba@gmail.com>
2025-05-20 18:01:23 +0200

    Add multi-select actions for episodes
    This commit introduces the ability to perform actions on multiple selected episodes. Users can now mark multiple episodes as read/unread or favorite/unfavorite simultaneously.
    
    The following changes were made:
    - Added `showSelectedEpisodesActionDialog` to `EpisodeActionsDialog` to display a dialog with actions for selected episodes.
    - Implemented logic to update the `read` and `favorite` status of selected episodes in the `episodeBox`.
    - Integrated with `EpisodeSelectionCubit` to toggle selection mode after performing an action.
    - Displayed `ActionFeedback` to confirm the executed action.

--- lib/presentation/custom_widgets/dialogs/episode_actions_dialog.dart
+++ lib/presentation/custom_widgets/dialogs/episode_actions_dialog.dart
@@ -1,19 +1,107 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 import 'package:podcast/helpers/core/episode_action_helper.dart';
+import '../../../application/episode_selection_cubit/episode_selection_cubit.dart';
+import '../../../core/globals.dart';
 import '../../../domain/entities/episode_entity.dart';
 import '../../../helpers/audio_download/audio_file_utility.dart';
 import '../action_feedback/action_feedback.dart';
 import 'audio_file_dialog.dart';
 
 class EpisodeActionsDialog {
+  static void showSelectedEpisodesActionDialog(
+      BuildContext context, List<EpisodeEntity> episodes) {
+    final List<Map<String, dynamic>> menuItems = [
+      {
+        "title": "Mark as read",
+        "onPressed": () {
+          for (var episode in episodes) {
+            episode.read = true;
+            episodeBox.put(episode);
+            ActionFeedback.show(
+              context,
+              icon: Icons.check,
+            );
+          }
+          BlocProvider.of<EpisodeSelectionCubit>(context).toggleSelectionMode();
+          Navigator.pop(context);
+        }
+      },
+      {
+        "title": "Unmark as read",
+        "onPressed": () {
+          for (var episode in episodes) {
+            episode.read = false;
+            episodeBox.put(episode);
+            ActionFeedback.show(
+              context,
+              icon: Icons.radio_button_unchecked,
+            );
+          }
+          BlocProvider.of<EpisodeSelectionCubit>(context).toggleSelectionMode();
+          Navigator.pop(context);
+        }
+      },
+      {
+        "title": "Mark as favorite",
+        "onPressed": () {
+          for (var episode in episodes) {
+            episode.favorite = true;
+            episodeBox.put(episode);
+            ActionFeedback.show(
+              context,
+              icon: Icons.star,
+            );
+          }
+          BlocProvider.of<EpisodeSelectionCubit>(context).toggleSelectionMode();
+          Navigator.pop(context);
+        }
+      },
+      {
+        "title": "Unmark as favorite",
+        "onPressed": () {
+          for (var episode in episodes) {
+            episode.favorite = false;
+            episodeBox.put(episode);
+            ActionFeedback.show(
+              context,
+              icon: Icons.star_border,
+            );
+          }
+          BlocProvider.of<EpisodeSelectionCubit>(context).toggleSelectionMode();
+          Navigator.pop(context);
+        }
+      },
+    ];
+    showDialog(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          content: Column(
+            mainAxisSize: MainAxisSize.min,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              for (var menuItem in menuItems)
+                TextButton(
+                  onPressed: () => menuItem["onPressed"](),
+                  child: Text(menuItem["title"]),
+                ),
+            ],
+          ),
+        );
+      },
+    );
+  }
+
   static void showEpisodeActionsDialog(
       BuildContext context, EpisodeEntity episode) {
     // Reset for debugging purposes
     /* episode.filePath = null;
   episodeBox.put(episode);*/
 
-    Map<String, bool> downloadStatus = AudioFileUtility.getDownloadStatus(episode);
+    Map<String, bool> downloadStatus =
+        AudioFileUtility.getDownloadStatus(episode);
 
     final List<Map<String, dynamic>> menuItems = [
       if (episode.isSubscribed)
@@ -49,7 +137,8 @@ class EpisodeActionsDialog {
                     ? "Download"
                     : "Delete",
         "onPressed": () {
-          (downloadStatus["isDownloading"] == true || downloadStatus["isPending"] == true)
+          (downloadStatus["isDownloading"] == true ||
+                  downloadStatus["isPending"] == true)
               ? null
               : AudioFileDialog.showAudioFileDialog(context, episode);
         },
