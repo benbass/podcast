76af035501162b8a9198018ba105916ed9e40d04
bruno <1201ba@gmail.com>
2025-05-25 14:41:57 +0200

    Refactor `RoundedTextWidget` to use theme colors.

--- lib/presentation/homepage/widgets/rounded_text_widget.dart
+++ lib/presentation/homepage/widgets/rounded_text_widget.dart
@@ -2,33 +2,30 @@ import 'package:flutter/material.dart';
 
 class RoundedTextWidget extends StatelessWidget {
   final String text;
-  final Color backgroundColor;
-  //final double fontSize;
   final double paddingValue;
 
   const RoundedTextWidget({
     super.key,
     required this.text,
-    this.backgroundColor = const Color(0xFFB2E92E),
-    //this.fontSize = 13.0,
     this.paddingValue = 5.0,
   });
 
   @override
   Widget build(BuildContext context) {
+    final themeData = Theme.of(context);
     return Padding(
       padding: const EdgeInsets.all(4.0),
       child: Container(
         decoration: BoxDecoration(
           shape: BoxShape.circle,
-          color: backgroundColor,
+          color: themeData.colorScheme.onPrimary//backgroundColor,
         ),
         alignment: Alignment.center,
         padding: EdgeInsets.all(paddingValue), // Dynamisches Padding
         child: Text(
           text,
           style: Theme.of(context).textTheme.bodyMedium!.copyWith(
-                color: const Color(0xFF202531),
+                color: themeData.colorScheme.primaryContainer,
             fontWeight: FontWeight.bold,
               ),
           textAlign: TextAlign.center,
