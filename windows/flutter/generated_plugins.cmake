de0a446a72b153ba0f2fd31ffec45c8e1c025ea6
bruno <1201ba@gmail.com>
2025-05-26 12:47:04 +0200

    Add Share Plus and URL Launcher Windows plugins.

--- windows/flutter/generated_plugins.cmake
+++ windows/flutter/generated_plugins.cmake
@@ -6,6 +6,8 @@ list(APPEND FLUTTER_PLUGIN_LIST
   awesome_notifications
   connectivity_plus
   objectbox_flutter_libs
+  share_plus
+  url_launcher_windows
 )
 
 list(APPEND FLUTTER_FFI_PLUGIN_LIST
