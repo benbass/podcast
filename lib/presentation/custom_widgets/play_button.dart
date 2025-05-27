013905f52faf63df913bfa0f5b2f2856dc5f0136
bruno <1201ba@gmail.com>
2025-05-27 18:31:32 +0200

    Refactor play button logic into MyAudioHandler.

--- lib/presentation/custom_widgets/play_button.dart
+++ lib/presentation/custom_widgets/play_button.dart
@@ -1,17 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:just_audio/just_audio.dart';
 
 import '../../application/episode_playback_cubit/episode_playback_cubit.dart';
-import '../../core/globals.dart';
 import '../../domain/entities/episode_entity.dart';
 import '../../domain/entities/podcast_entity.dart';
-import '../../helpers/core/connectivity_manager.dart';
-import '../../helpers/notifications/utilities_notifications.dart';
 import '../../helpers/player/audiohandler.dart';
 import '../../injection.dart';
-import '../audioplayer_overlays/audioplayer_overlays.dart';
-import 'dialogs/failure_dialog.dart';
 
 class PlayButton extends StatelessWidget {
   const PlayButton({
@@ -65,57 +59,7 @@ class PlayButtonActive extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return IconButton(
-      onPressed: () async {
-        final currentPosition =
-            getIt<MyAudioHandler>().player.position.inSeconds;
-        final String connectionType =
-            await getIt<ConnectivityManager>().getConnectionTypeAsString();
-        String filePath = episode.filePath ?? episode.enclosureUrl;
-        if (connectionType == 'none' && filePath == episode.enclosureUrl) {
-          if (context.mounted) {
-            showDialog(
-              context: context,
-              builder: (context) =>
-                  const FailureDialog(message: "No internet connection!"),
-            );
-          }
-        } else {
-          removeOverlay();
-          // source error?
-          try {
-            if (context.mounted) {
-              // Save position of previous episode before changing to new one
-              if (BlocProvider.of<EpisodePlaybackCubit>(context).state !=
-                  null) {
-                final previousEpisode =
-                    BlocProvider.of<EpisodePlaybackCubit>(context).state!.values.first;
-                previousEpisode.position = currentPosition;
-                episodeBox.put(previousEpisode);
-              }
-            }
-
-            await getIt<MyAudioHandler>().player.setUrl(filePath);
-            getIt<MyAudioHandler>().play();
-            if (episode.position > 0) {
-              getIt<MyAudioHandler>()
-                  .player
-                  .seek(Duration(seconds: episode.position));
-            }
-
-            if (context.mounted) {
-              BlocProvider.of<EpisodePlaybackCubit>(context)
-                  .setPlaybackEpisode({podcast: episode});
-              UtilitiesNotifications.createNotificationPlayback(context, false, episode.position);
-            }
-
-          } on PlayerException {
-            if (context.mounted) {
-              showOverlayError(context,
-                  "Error: No valid file exists under the requested url.");
-            }
-          }
-        }
-      },
+      onPressed: () => getIt<MyAudioHandler>().handlePlayButtonPressed(context, episode, podcast),
       padding: EdgeInsets.zero,
       constraints: const BoxConstraints(),
       icon: const Icon(
@@ -124,4 +68,6 @@ class PlayButtonActive extends StatelessWidget {
       ),
     );
   }
+
+
 }
