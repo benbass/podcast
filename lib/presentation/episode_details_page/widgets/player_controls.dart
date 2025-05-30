65fddc7568583aa26609cc18dea7509a1229f42c
bruno <1201ba@gmail.com>
2025-05-30 16:23:56 +0200

    Refactor PlayerControls to use EpisodePlaybackCubit state for navigation and playback.
    This commit refactors the PlayerControls widget to utilize the `EpisodePlaybackCubit`'s state for managing episode navigation (previous/next) and playback.
    
    Key changes include:
    
    - Removing `episodeListForNav` and `currentIndexInListForNav` as direct parameters, as this information is now available in the `EpisodePlaybackCubit` state.
    - Updating the `onPressed` handlers for the skip previous and skip next buttons to use the `playPrevious()` and `playNext()` methods from `MyAudioHandler`, which now also handles updating the `EpisodePlaybackCubit`.
    - Disabling the skip previous and skip next buttons when at the beginning or end of the playlist respectively, based on the `EpisodePlaybackCubit` state.
    - Updating the `PlaybackPositionSlider` to receive the `episode` from the `EpisodePlaybackCubit` state.
    - Changing the `BlocBuilder` to listen to `EpisodePlaybackState` instead of `Map<PodcastEntity, EpisodeEntity>?`.

--- lib/presentation/episode_details_page/widgets/player_controls.dart
+++ lib/presentation/episode_details_page/widgets/player_controls.dart
@@ -4,7 +4,6 @@ import 'package:just_audio/just_audio.dart';
 import 'package:podcast/presentation/episode_details_page/widgets/playback_position_slider.dart';
 
 import '../../../application/episode_playback_cubit/episode_playback_cubit.dart';
-import '../../../domain/entities/episode_entity.dart';
 import '../../../domain/entities/podcast_entity.dart';
 import '../../../helpers/notifications/utilities_notifications.dart';
 import '../../../helpers/player/audiohandler.dart';
@@ -12,135 +11,151 @@ import '../../../injection.dart';
 
 class PlayerControls extends StatelessWidget {
   final PageController? pageController;
-  final List<EpisodeEntity> episodeListForNav;
-  final int currentIndexInListForNav;
   final PodcastEntity podcast;
   const PlayerControls({
     super.key,
     this.pageController,
-    required this.episodeListForNav,
-    required this.currentIndexInListForNav,
     required this.podcast,
   });
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Theme.of(context).colorScheme.primaryContainer,
-      height: MediaQuery.of(context).size.height / 7,
-      constraints: const BoxConstraints(minHeight: 150),
-      child: Center(
-        child: Column(
-          children: [
-            BlocBuilder<EpisodePlaybackCubit,
-                Map<PodcastEntity, EpisodeEntity>?>(
-              builder: (context, state) {
-                if (state != null) {
-                  return PlaybackPositionSlider(episode: state.values.first);
-                } else {
-                  return const SizedBox.shrink();
-                }
-              },
-            ),
-            Padding(
-              padding: const EdgeInsets.symmetric(vertical: 8.0),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  IconButton(
-                    onPressed: () async {
-                      if (currentIndexInListForNav > 0) {
-                        final previousEpisode =
-                            episodeListForNav[currentIndexInListForNav - 1];
-                        await getIt<MyAudioHandler>().handlePlayButtonPressed(context, previousEpisode, podcast);
-                        Future.delayed(const Duration(milliseconds: 50), () {
-                          if (pageController != null && pageController!.hasClients) {
-                            pageController!.previousPage(
-                              duration: const Duration(milliseconds: 300),
-                              curve: Curves.easeInOut,
+    final themeData = Theme.of(context);
+    return BlocBuilder<EpisodePlaybackCubit, EpisodePlaybackState>(
+      builder: (context, state) {
+        return Container(
+          color: themeData.colorScheme.primaryContainer,
+          height: MediaQuery.of(context).size.height / 7,
+          constraints: const BoxConstraints(minHeight: 150),
+          child: Center(
+            child: Column(
+              children: [
+                PlaybackPositionSlider(episode: state.episode!),
+                Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 8.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      IconButton(
+                        onPressed: state.currentIndexInPlaylist == 0
+                            ? null
+                            : () async {
+                                // playPrevious() (and playNext()) handle the EpisodePlaybackCubit
+                                // update that will return true if success or false.
+                                // If true, the audioHandler will handle the playback
+                                // of the new episode.
+                                bool result = await getIt<MyAudioHandler>()
+                                    .playPrevious();
+                                if (result) {
+                                  Future.delayed(
+                                      const Duration(milliseconds: 50), () {
+                                    if (pageController != null &&
+                                        pageController!.hasClients) {
+                                      pageController!.previousPage(
+                                        duration:
+                                            const Duration(milliseconds: 300),
+                                        curve: Curves.easeInOut,
+                                      );
+                                    }
+                                  });
+                                }
+                              },
+                        icon: Icon(
+                          Icons.skip_previous_rounded,
+                          size: 40,
+                          color: state.currentIndexInPlaylist == 0
+                              ? themeData.colorScheme.onPrimary
+                                  .withValues(alpha: 0.5)
+                              : themeData.colorScheme.onPrimary,
+                        ),
+                      ),
+                      IconButton(
+                        onPressed: () {
+                          getIt<MyAudioHandler>().seekBackward();
+                        },
+                        icon: const Icon(
+                          Icons.fast_rewind_rounded,
+                          size: 40,
+                        ),
+                      ),
+                      IconButton(
+                        onPressed: () {
+                          getIt<MyAudioHandler>().stop();
+                          BlocProvider.of<EpisodePlaybackCubit>(context)
+                              .resetPlayback();
+                        },
+                        icon: const Icon(
+                          Icons.stop_rounded,
+                          size: 50,
+                        ),
+                      ),
+                      StreamBuilder<PlayerState>(
+                          stream:
+                              getIt<MyAudioHandler>().player.playerStateStream,
+                          builder: (context, stream) {
+                            final isPlaying = stream.data?.playing;
+                            return IconButton(
+                              onPressed: () {
+                                UtilitiesNotifications
+                                    .cancelNotificationPlayback();
+                                getIt<MyAudioHandler>().handlePlayPause();
+                              },
+                              icon: Icon(
+                                isPlaying == true
+                                    ? Icons.pause_rounded
+                                    : Icons.play_arrow_rounded,
+                                size: 50,
+                              ),
                             );
-                          }
-                        });
-                      }
-                    },
-                    icon: const Icon(
-                      Icons.skip_previous_rounded,
-                      size: 40,
-                    ),
+                          }),
+                      IconButton(
+                        onPressed: () {
+                          getIt<MyAudioHandler>().seekForward();
+                        },
+                        icon: const Icon(
+                          Icons.fast_forward_rounded,
+                          size: 40,
+                        ),
+                      ),
+                      IconButton(
+                        onPressed: state.currentIndexInPlaylist ==
+                                state.episodes!.length - 1
+                            ? null
+                            : () async {
+                                bool result =
+                                    await getIt<MyAudioHandler>().playNext();
+                                if (result) {
+                                  Future.delayed(
+                                      const Duration(milliseconds: 50), () {
+                                    if (pageController != null &&
+                                        pageController!.hasClients) {
+                                      pageController!.nextPage(
+                                        duration:
+                                            const Duration(milliseconds: 300),
+                                        curve: Curves.easeInOut,
+                                      );
+                                    }
+                                  });
+                                }
+                              },
+                        icon: Icon(
+                          Icons.skip_next_rounded,
+                          size: 40,
+                          color: state.currentIndexInPlaylist ==
+                                  state.episodes!.length - 1
+                              ? themeData.colorScheme.onPrimary
+                                  .withValues(alpha: 0.5)
+                              : themeData.colorScheme.onPrimary,
+                        ),
+                      ),
+                    ],
                   ),
-                  IconButton(
-                    onPressed: () {
-                      getIt<MyAudioHandler>().seekBackward();
-                    },
-                    icon: const Icon(
-                      Icons.fast_rewind_rounded,
-                      size: 40,
-                    ),
-                  ),
-                  IconButton(
-                    onPressed: () {
-                      getIt<MyAudioHandler>().stop();
-
-                      BlocProvider.of<EpisodePlaybackCubit>(context)
-                          .setPlaybackEpisode(null);
-                    },
-                    icon: const Icon(
-                      Icons.stop_rounded,
-                      size: 50,
-                    ),
-                  ),
-                  StreamBuilder<PlayerState>(
-                      stream: getIt<MyAudioHandler>().player.playerStateStream,
-                      builder: (context, stream) {
-                        final isPlaying = stream.data?.playing;
-                        return IconButton(
-                          onPressed: () {
-                            UtilitiesNotifications.cancelNotificationPlayback();
-                            getIt<MyAudioHandler>().handlePlayPause();
-                          },
-                          icon: Icon(
-                            isPlaying == true
-                                ? Icons.pause_rounded
-                                : Icons.play_arrow_rounded,
-                            size: 50,
-                          ),
-                        );
-                      }),
-                  IconButton(
-                    onPressed: () {
-                      getIt<MyAudioHandler>().seekForward();
-                    },
-                    icon: const Icon(
-                      Icons.fast_forward_rounded,
-                      size: 40,
-                    ),
-                  ),
-                  IconButton(
-                    onPressed: () async {
-                      if(currentIndexInListForNav < episodeListForNav.length - 1) {
-                        final nextEpisode = episodeListForNav[currentIndexInListForNav + 1];
-                        await getIt<MyAudioHandler>().handlePlayButtonPressed(context, nextEpisode, podcast);
-                        Future.delayed(const Duration(milliseconds: 50), () {
-                          if (pageController != null && pageController!.hasClients) {
-                            pageController!.nextPage(
-                              duration: const Duration(milliseconds: 300),
-                              curve: Curves.easeInOut,
-                            );
-                          }
-                        });
-                      }
-                    },
-                    icon: const Icon(
-                      Icons.skip_next_rounded,
-                      size: 40,
-                    ),
-                  ),
-                ],
-              ),
+                ),
+              ],
             ),
-          ],
-        ),
-      ),
+          ),
+        );
+      },
     );
   }
 }
