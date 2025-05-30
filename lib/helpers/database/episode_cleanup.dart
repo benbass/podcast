02103643cb59e9b37a01c87a9ae52750e36023c6
bruno <1201ba@gmail.com>
2025-05-30 16:37:27 +0200

    Preserve unsubscribed episodes during cleanup

--- lib/helpers/database/episode_cleanup.dart
+++ lib/helpers/database/episode_cleanup.dart
@@ -10,9 +10,12 @@ class EpisodeCleanup{
   static void deleteEpisodes(){
     final queryBuilder = episodeBox.query(EpisodeEntity_.isSubscribed
         .equals(false)
-        .and(EpisodeEntity_.favorite.equals(false))
+    // the 3 following conditions will be useful in a future version of the app
+    // when app allows to manipulate episodes when not subscribed,
+    // so we won't delete those in order to show them on a dedicated page.
+        /*.and(EpisodeEntity_.favorite.equals(false))
         .and(EpisodeEntity_.filePath.isNull())
-        .and(EpisodeEntity_.position.equals(0)),
+        .and(EpisodeEntity_.position.equals(0)),*/
     ).build();
     final results = queryBuilder.findIds();
     episodeBox.removeMany(results);
