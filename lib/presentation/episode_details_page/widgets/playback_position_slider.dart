2f28c14ba5dba614f27357afe35e9dad7de2254d
bruno <1201ba@gmail.com>
2025-06-01 11:50:12 +0200

    Make current and remaining duration text monospaced
    This prevents the text from jumping around as the numbers change, as each digit will now occupy the same width.
    Also, align remaining duration text to the end.

--- lib/presentation/episode_details_page/widgets/playback_position_slider.dart
+++ lib/presentation/episode_details_page/widgets/playback_position_slider.dart
@@ -19,41 +19,47 @@ class PlaybackPositionSlider extends StatelessWidget {
       stream: getIt<MyAudioHandler>().player.positionStream,
       builder: (context, snapshot) {
         final position = snapshot.data ?? Duration.zero;
-        final Duration totalDuration =
-        Duration(seconds: episode.duration ?? 0);
+        final Duration totalDuration = Duration(seconds: episode.duration ?? 0);
         final Duration remainingDuration = totalDuration - position;
         String formattedRemainingDuration =
-        FormatUtilities.remainingDurationFormatted(remainingDuration);
+            FormatUtilities.remainingDurationFormatted(remainingDuration);
         return Column(
           children: [
             Slider(
-              activeColor: Theme.of(context).colorScheme.onPrimary,
-              inactiveColor: Theme.of(context).colorScheme.primary,
-              value: position.inSeconds.toDouble(),
-              min: 0.0,
-              max: getIt<MyAudioHandler>()
-                  .player
-                  .duration
-                  ?.inSeconds
-                  .toDouble() ??
-                  0.0,
-              onChanged: (value)
-              {
-                final bool playerState = getIt<MyAudioHandler>().player.playerState.playing;
-                      getIt<MyAudioHandler>()
-                          .player
-                          .seek(Duration(seconds: value.toInt()));
-                UtilitiesNotifications.createNotificationPlayback(context, playerState, value.toInt());
-                    }),
+                activeColor: Theme.of(context).colorScheme.onPrimary,
+                inactiveColor: Theme.of(context).colorScheme.primary,
+                value: position.inSeconds.toDouble(),
+                min: 0.0,
+                max: getIt<MyAudioHandler>()
+                        .player
+                        .duration
+                        ?.inSeconds
+                        .toDouble() ??
+                    0.0,
+                onChanged: (value) {
+                  final bool playerState =
+                      getIt<MyAudioHandler>().player.playerState.playing;
+                  getIt<MyAudioHandler>()
+                      .player
+                      .seek(Duration(seconds: value.toInt()));
+                  UtilitiesNotifications.createNotificationPlayback(
+                      context, playerState, value.toInt());
+                }),
             Padding(
               padding: const EdgeInsets.symmetric(horizontal: 8.0),
               child: Row(
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
                   Text(
-                    FormatUtilities.durationWithMillisecondsRemovedFormatted(position),
+                    FormatUtilities.durationWithMillisecondsRemovedFormatted(
+                        position),
                     style: const TextStyle(
                       fontWeight: FontWeight.bold,
+                      fontFeatures: [
+                        // Not necessary here but we align with style
+                        // of remaining duration text widget
+                        FontFeature.tabularFigures(),
+                      ],
                     ),
                   ),
                   Text(
@@ -62,7 +68,13 @@ class PlaybackPositionSlider extends StatelessWidget {
                         : formattedRemainingDuration,
                     style: const TextStyle(
                       fontWeight: FontWeight.bold,
+                      fontFeatures: [
+                        // We force all signs to have the same width (monospaced)
+                        // so text will not jump around as numbers change
+                        FontFeature.tabularFigures(),
+                      ],
                     ),
+                    textAlign: TextAlign.end,
                   ),
                 ],
               ),
