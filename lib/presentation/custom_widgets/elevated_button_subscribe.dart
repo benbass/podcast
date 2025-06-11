1a34a562c3c73455ec478778812cae3cede1e970
bruno <1201ba@gmail.com>
2025-06-11 15:52:03 +0200

    Refactor subscribe button to fetch and save episodes on subscribe.
    The `ElevatedButtonSubscribe` widget now fetches episodes from the remote source and saves them to the database when a user subscribes to a podcast. This ensures that episodes are available locally after subscribing.
    
    Error handling has been added to display a snackbar if fetching episodes fails and a dialog if the subscription process fails.
    
    The `navigate` property has been removed as it was unused.

--- lib/presentation/custom_widgets/elevated_button_subscribe.dart
+++ lib/presentation/custom_widgets/elevated_button_subscribe.dart
@@ -5,7 +5,6 @@ import 'package:podcast/core/globals.dart';
 import 'package:podcast/domain/entities/podcast_entity.dart';
 import 'package:podcast/presentation/custom_widgets/page_transition.dart';
 import '../../application/podcast_bloc/podcast_bloc.dart';
-import '../../domain/entities/episode_entity.dart';
 import '../../domain/usecases/episode_usecases.dart';
 import '../../helpers/core/connectivity_manager.dart';
 import '../../injection.dart';
@@ -15,12 +14,10 @@ import 'dialogs/failure_dialog.dart';
 
 class ElevatedButtonSubscribe extends StatelessWidget {
   final PodcastEntity podcast;
-  final bool navigate;
 
   const ElevatedButtonSubscribe({
     super.key,
     required this.podcast,
-    required this.navigate,
   });
 
   @override
@@ -30,14 +27,6 @@ class ElevatedButtonSubscribe extends StatelessWidget {
         !podcast.subscribed
             ? await _subscribeToPodcast(context)
             : await _unsubscribe(context);
-        /*if (navigate && context.mounted) {
-          Navigator.push(
-            context,
-            ScaleRoute(
-              page: const HomePage(),
-            ),
-          );
-        }*/
       },
       child: BlocBuilder<PodcastBloc, PodcastState>(
         builder: (context, state) {
@@ -70,33 +59,41 @@ class ElevatedButtonSubscribe extends StatelessWidget {
   }
 
   /// SUBSCRIBE
-  _handleEpisodes(BuildContext context) async {
-    // Make sure we have episodes when we subscribe before calling the episode list page
-    List<EpisodeEntity> episodes = await getIt<EpisodeUseCases>()
-        .getEpisodes(
-          subscribed: false,
-          feedId: podcast.pId,
-          podcastTitle: podcast.title,
-          filterStatus: "all",
-          refresh: false,
-        )
-        .first;
-    // Set flag isSubscribed to true
-    for (var episode in episodes) {
-      episode.isSubscribed = true;
+  Future<void> _handleEpisodes(BuildContext context) async {
+    try {
+      await getIt<EpisodeUseCases>().fetchRemoteEpisodesByFeedIdAndSaveToDb(
+        feedId: podcast.pId,
+        podcastTitle: podcast.title,
+        markAsSubscribed: true,
+      );
+    } catch (e) {
+      if (context.mounted) {
+        ScaffoldMessenger.of(context).showSnackBar(
+          const SnackBar(
+              content: Text("Error fetching episodes.}")),
+        );
+      }
+      rethrow; // For try-catch bloc in _subscribeToPodcast (the caller)!
     }
-    // Save episodes to db
-    episodeBox.putMany(episodes);
   }
 
   _subscribeToPodcast(BuildContext context) async {
     final String connectionType =
         await getIt<ConnectivityManager>().getConnectionTypeAsString();
     if (connectionType != 'none' && context.mounted) {
-      await _handleEpisodes(context);
-      if (context.mounted) {
-        BlocProvider.of<PodcastBloc>(context)
-            .add(SubscribeToPodcastEvent(podcast: podcast));
+      try {
+        await _handleEpisodes(context);
+        if (context.mounted) {
+          BlocProvider.of<PodcastBloc>(context)
+              .add(SubscribeToPodcastEvent(podcast: podcast));
+        }
+      } catch (e) {
+        if (context.mounted) {
+          showDialog(
+              context: context,
+              builder: (context) => const FailureDialog(
+                  message: "Failed to subscribe. Please try again later."));
+        }
       }
     } else {
       if (context.mounted) {
