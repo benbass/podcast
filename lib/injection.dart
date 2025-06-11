95fbf1455726f3427248bd8bdecbaa61b6829217
bruno <1201ba@gmail.com>
2025-06-11 15:39:53 +0200

    Register new Blocs and Cubits for DI and update repository injection.

--- lib/injection.dart
+++ lib/injection.dart
@@ -1,5 +1,6 @@
 import 'package:get_it/get_it.dart';
 import 'package:http/http.dart' as http;
+import 'package:podcast/application/episodes_bloc/episodes_bloc.dart';
 import 'package:podcast/domain/repositories/podcast_repository.dart';
 import 'package:podcast/domain/usecases/podcast_usecases.dart';
 import 'package:podcast/helpers/listeners/player_listener.dart';
@@ -9,6 +10,7 @@ import 'package:podcast/infrastructure/repositories/podcast_repository_impl.dart
 import 'application/episode_playback_cubit/episode_playback_cubit.dart';
 import 'application/episode_selection_cubit/episode_selection_cubit.dart';
 import 'application/podcast_bloc/podcast_bloc.dart';
+import 'application/podcast_settings_cubit/podcast_settings_cubit.dart';
 import 'application/textfield_cubit/text_field_cubit.dart';
 import 'domain/repositories/episode_repository.dart';
 import 'domain/usecases/episode_usecases.dart';
@@ -28,6 +30,8 @@ Future<void> init() async {
   getIt.registerFactory(() => TextFieldCubit());
   getIt.registerFactory(() => EpisodePlaybackCubit());
   getIt.registerFactory(() => EpisodeSelectionCubit());
+  getIt.registerFactory(() => PodcastSettingsCubit());
+  getIt.registerFactory(() => EpisodesBloc(episodeUseCases: getIt(), podcastSettingsCubit: getIt()));
 
   // Use cases
   getIt
@@ -42,6 +46,7 @@ Future<void> init() async {
       )); // remote (fetching)
   getIt.registerLazySingleton<EpisodeRepository>(() => EpisodeRepositoryImpl(
         episodeLocalDatasource: getIt(),
+        episodeRemoteDatasource: getIt(),
       ));
 
   // DataSources
