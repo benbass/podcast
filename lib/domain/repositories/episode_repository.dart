85eeda6084d6fa64c2c41f862994804b69d13c9f
bruno <1201ba@gmail.com>
2025-06-11 15:58:55 +0200

    Refactor `EpisodeRepository` to clarify method names and responsibilities.
    The `getEpisodes` method is renamed to `getEpisodesStream` and its parameters are updated to better reflect its purpose of retrieving a stream of episodes based on feed ID, podcast title, subscription status, and filter settings.
    
    Two new methods are introduced:
    - `fetchRemoteEpisodesByFeedIdAndSaveToDb`: Fetches episodes from a remote source by feed ID, saves them to the database, and optionally marks them as subscribed.
    - `refreshEpisodesFromServer`: Refreshes episodes from the server for a given feed ID and podcast title.

--- lib/domain/repositories/episode_repository.dart
+++ lib/domain/repositories/episode_repository.dart
@@ -1,14 +1,26 @@
 import '../entities/episode_entity.dart';
+import '../entities/podcast_filter_settings_entity.dart';
 
 abstract class EpisodeRepository {
-  Stream<List<EpisodeEntity>> getEpisodes({
-    required bool subscribed,
+
+  Stream<List<EpisodeEntity>> getEpisodesStream({
     required int feedId,
     required String podcastTitle,
-    required String filterStatus,
-    required bool refresh,
-    String? filterText,
+    required bool isSubscribed,
+    required PodcastFilterSettingsEntity filterSettings,
   });
+
+  Future<void> fetchRemoteEpisodesByFeedIdAndSaveToDb({
+    required int feedId,
+    required String podcastTitle,
+    bool? markAsSubscribed,
+  });
+
+  Future<void> refreshEpisodesFromServer({
+    required int feedId,
+    required String podcastTitle,
+  });
+
   Stream<EpisodeEntity?> getEpisodeStream({required int episodeId});
   Stream<int> unreadLocalEpisodesCount({required int feedId});
 }
