354e8cc5af83903a752cdf4008f7f62ba40cd55d
bruno <1201ba@gmail.com>
2025-06-11 15:59:48 +0200

    Refactor EpisodeRepository and add remote data source
    This commit refactors the `EpisodeRepository` interface and its implementation.
    
    Key changes:
    - Renamed `getEpisodes` to `getEpisodesStream` to better reflect its stream-based nature.
    - Added `EpisodeRemoteDataSource` to `EpisodeRepositoryImpl`.
    - Introduced two new methods:
        - `fetchRemoteEpisodesByFeedIdAndSaveToDb`: Fetches episodes from a remote source by feed ID and saves them to the local database.
        - `refreshEpisodesFromServer`: Refreshes episodes from the server for a given feed ID.
    - Updated `getEpisodesStream` to accept `feedId`, `podcastTitle`, `isSubscribed`, and `filterSettings` parameters.

--- lib/infrastructure/repositories/episode_repository_impl.dart
+++ lib/infrastructure/repositories/episode_repository_impl.dart
@@ -1,30 +1,53 @@
 import 'package:podcast/domain/entities/episode_entity.dart';
+import '../../domain/entities/podcast_filter_settings_entity.dart';
 import '../../domain/repositories/episode_repository.dart';
 import '../datasources/episode_datasources.dart';
 
 class EpisodeRepositoryImpl implements EpisodeRepository {
   final EpisodeLocalDatasource episodeLocalDatasource;
+  final EpisodeRemoteDataSource episodeRemoteDatasource;
 
   EpisodeRepositoryImpl({
     required this.episodeLocalDatasource,
+    required this.episodeRemoteDatasource,
   });
 
   @override
-  Stream<List<EpisodeEntity>> getEpisodes({
-    required bool subscribed,
+  Stream<List<EpisodeEntity>> getEpisodesStream({
     required int feedId,
     required String podcastTitle,
-    required String filterStatus,
-    required bool refresh,
-    String? filterText,
+    required bool isSubscribed,
+    required PodcastFilterSettingsEntity filterSettings,
   }) {
-    return episodeLocalDatasource.getEpisodes(
-      subscribed: subscribed,
+    return episodeLocalDatasource.getEpisodesStream(
       feedId: feedId,
       podcastTitle: podcastTitle,
-      filterStatus: filterStatus,
-      refresh: refresh,
-      filterText: filterText,
+      isSubscribed: isSubscribed,
+      filterSettings: filterSettings,
+    );
+  }
+
+  @override
+  Future<void> fetchRemoteEpisodesByFeedIdAndSaveToDb({
+    required int feedId,
+    required String podcastTitle,
+    bool? markAsSubscribed,
+  }) {
+    return episodeRemoteDatasource.fetchRemoteEpisodesByFeedIdAndSaveToDb(
+      feedId: feedId,
+      podcastTitle: podcastTitle,
+      markAsSubscribed: markAsSubscribed,
+    );
+  }
+
+  @override
+  Future<void> refreshEpisodesFromServer({
+    required int feedId,
+    required String podcastTitle,
+  }) {
+    return episodeLocalDatasource.refreshEpisodesFromServer(
+      feedId: feedId,
+      podcastTitle: podcastTitle,
     );
   }
 
