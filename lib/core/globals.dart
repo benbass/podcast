3d2828b903d733f885887b8bcfcb2378a17181b2
bruno <1201ba@gmail.com>
2025-06-11 15:39:40 +0200

    Store settings for individual podcasts.

--- lib/core/globals.dart
+++ lib/core/globals.dart
@@ -2,12 +2,14 @@ import 'package:objectbox/objectbox.dart';
 import 'package:podcast/domain/entities/episode_entity.dart';
 import 'package:podcast/domain/entities/podcast_entity.dart';
 
+import '../domain/entities/persistent_podcast_settings_entity.dart';
 import 'objectbox.dart';
 
 const String baseUrl = 'https://api.podcastindex.org/api/1.0';
 late ObjectBox objectBox;
 late Box<PodcastEntity> podcastBox;
 late Box<EpisodeEntity> episodeBox;
+late Box<PersistentPodcastSettingsEntity> settingsBox;
 const String kCurrentDownloadFilePath = "current_download_filepath";
 
 /// For testing: create a fake episode
