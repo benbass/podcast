1b8c1b03abd654c18502d01f11ce66975664a195
bruno <1201ba@gmail.com>
2025-06-11 16:10:00 +0200

    Refactor episode refresh and filtering logic.
    The episode refresh functionality now uses the `EpisodesBloc` and displays a loading indicator and snackbar messages for success or error.
    The episode filtering logic has been moved to use `PodcastSettingsCubit` for managing filter states.

--- lib/presentation/episodes_list_page/widgets/row_icon_buttons_episodes.dart
+++ lib/presentation/episodes_list_page/widgets/row_icon_buttons_episodes.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:podcast/application/episodes_bloc/episodes_bloc.dart';
 
 import '../../../application/episode_selection_cubit/episode_selection_cubit.dart';
 import '../../../application/podcast_bloc/podcast_bloc.dart';
+import '../../../application/podcast_settings_cubit/podcast_settings_cubit.dart';
 import 'iconbutton_with_popup_text.dart';
 
 class RowIconButtonsEpisodes extends StatelessWidget {
@@ -12,21 +14,29 @@ class RowIconButtonsEpisodes extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final podcastBloc = BlocProvider.of<PodcastBloc>(context);
-    PodcastState state = context.watch<PodcastBloc>().state;
+    final PodcastState podcastState = context.watch<PodcastBloc>().state;
+
+    final PodcastSettingsCubit podcastSettingsCubit =
+        BlocProvider.of<PodcastSettingsCubit>(context);
+    final PodcastSettingsState podcastSettingsState =
+        podcastSettingsCubit.state;
+
+    final EpisodesBloc episodesBloc = BlocProvider.of<EpisodesBloc>(context);
+    final EpisodesState episodesState = context.watch<EpisodesBloc>().state;
+
     return Row(
       mainAxisAlignment: MainAxisAlignment.spaceAround,
       children: [
         const IconButtonWithPopupText(),
         BlocBuilder<EpisodeSelectionCubit, EpisodeSelectionState>(
-          builder: (context, state) {
+          builder: (context, episodeSelectionState) {
             return IconButton(
               onPressed: () {
                 BlocProvider.of<EpisodeSelectionCubit>(context)
                     .toggleSelectionMode();
               },
               icon: Icon(
-                state.isSelectionModeActive
+                episodeSelectionState.isSelectionModeActive
                     ? Icons.close_rounded
                     : Icons.library_add_check_rounded,
                 size: 30,
@@ -34,24 +44,93 @@ class RowIconButtonsEpisodes extends StatelessWidget {
             );
           },
         ),
-        if (state.currentPodcast.subscribed)
-          IconButton(
-            onPressed: () {
-              podcastBloc.add(RefreshEpisodesByFeedIdEvent(
-                  feedId: state.currentPodcast.pId));
-            },
-            icon: const Icon(
-              Icons.refresh_rounded,
-              size: 30,
-            ),
+        if (podcastState.currentPodcast.subscribed)
+          SizedBox(
+            width: 30,
+            child: Stack(children: [
+              if (episodesState.status != EpisodesStatus.refreshing)
+                IconButton(
+                  onPressed: () async {
+                    final int currentCount = episodesState.episodes.length;
+                    episodesBloc.add(RefreshEpisodes(
+                      feedId: podcastState.currentPodcast.pId,
+                      podcastTitle: podcastState.currentPodcast.title,
+                      isSubscribed: podcastState.currentPodcast.subscribed,
+                    ));
+                    Duration duration = const Duration(milliseconds: 1500);
+                    try {
+                      final finalState = await episodesBloc.stream.firstWhere(
+                          (state) =>
+                              state.status != EpisodesStatus.refreshing &&
+                              state.status != EpisodesStatus.loading);
+
+                      if (context.mounted) {
+                        if (finalState.status == EpisodesStatus.success) {
+                          final int newCount = finalState.episodes.length;
+                          final int diff = newCount - currentCount;
+
+                          if (diff > 0) {
+                            ScaffoldMessenger.of(context).showSnackBar(
+                              SnackBar(
+                                  duration: duration,
+                                  content: Text("$diff new episodes added.")),
+                            );
+                          } else if (diff == 0) {
+                            ScaffoldMessenger.of(context).showSnackBar(
+                              SnackBar(
+                                  duration: duration,
+                                  content: const Text(
+                                      "No new episodes were found.")),
+                            );
+                          } else {
+                            // diff < 0 (episodes removed??),
+                            ScaffoldMessenger.of(context).showSnackBar(
+                              SnackBar(
+                                  duration: duration,
+                                  content:
+                                      const Text("Episodes were updated.")),
+                            );
+                          }
+                        }
+                      }
+                    } catch (e) {
+                      if (context.mounted) {
+                        ScaffoldMessenger.of(context).showSnackBar(
+                          SnackBar(
+                              duration: duration,
+                              content: const Text("An error occurred.")),
+                        );
+                      }
+                    }
+                  },
+                  padding: EdgeInsets.zero,
+                  constraints: const BoxConstraints(),
+                  icon: const Icon(
+                    Icons.refresh_rounded,
+                    size: 30,
+                  ),
+                ),
+              if (episodesState.status == EpisodesStatus.refreshing)
+                const SizedBox(
+                  width: 30,
+                  height: 30,
+                  child: CircularProgressIndicator(
+                    strokeWidth: 3,
+                  ),
+                ),
+            ]),
           ),
-        if (state.currentPodcast.subscribed)
+        if (podcastState.currentPodcast.subscribed &&
+            podcastSettingsState is PodcastSettingsLoaded)
           IconButton(
             onPressed: () {
-              _showFilterDialog(context, podcastBloc, state);
+              _showFilterDialog(context, podcastSettingsCubit);
             },
             icon: Icon(
-              state.episodesFilterStatus.name == "read"
+              podcastSettingsState.settings.filterByText ||
+                      podcastSettingsState.settings.showOnlyUnfinished ||
+                      podcastSettingsState.settings.showOnlyFavorites ||
+                      podcastSettingsState.settings.showOnlyDownloaded
                   ? Icons.filter_alt_off_outlined
                   : Icons.filter_alt_outlined,
               size: 30,
@@ -61,98 +140,101 @@ class RowIconButtonsEpisodes extends StatelessWidget {
     );
   }
 
-  _showFilterDialog(
-      BuildContext context, PodcastBloc podcastBloc, PodcastState state) {
+  _showFilterDialog(BuildContext context, PodcastSettingsCubit episodesBloc) {
+    final episodesState = episodesBloc.state;
     showDialog(
         context: context,
         builder: (BuildContext context) {
-          return AlertDialog(
-            title: const Text("Filter Episodes"),
-            content: const Text("Select the filter option"),
-            actionsAlignment: MainAxisAlignment.center,
-            actionsOverflowAlignment: OverflowBarAlignment.center,
-            alignment: Alignment.center,
-            shape:
-                RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
-            actions: [
-              TextButton(
-                onPressed: () {
-                  podcastBloc.add(
-                    ToggleEpisodesFilterStatusEvent(
-                      filterStatus:
-                          state.episodesFilterStatus.name == "hideRead"
-                              ? "all"
-                              : "hideRead",
-                    ),
-                  );
-                  Navigator.of(context).pop();
-                },
-                child: Text(
-                  state.episodesFilterStatus.name == "hideRead"
-                      ? "Show read episodes"
-                      : "Hide read episodes",
-                  textAlign: TextAlign.center,
+          if (episodesState is PodcastSettingsLoaded) {
+            return AlertDialog(
+              title: const Text("Filter Episodes"),
+              content: const Text("Select the filter option"),
+              actionsAlignment: MainAxisAlignment.center,
+              actionsOverflowAlignment: OverflowBarAlignment.center,
+              alignment: Alignment.center,
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10)),
+              actions: [
+                TextButton(
+                  onPressed: () {
+                    context.read<PodcastSettingsCubit>().updateUiFilterSettings(
+                          filterRead:
+                              episodesState.settings.filterRead ? false : true,
+                        );
+                    Navigator.of(context).pop();
+                  },
+                  child: Text(
+                    episodesState.settings.filterRead
+                        ? "Show read episodes"
+                        : "Hide read episodes",
+                    textAlign: TextAlign.center,
+                  ),
                 ),
-              ),
-              TextButton(
-                onPressed: () {
-                  podcastBloc.add(
-                    ToggleEpisodesFilterStatusEvent(
-                      filterStatus:
-                          state.episodesFilterStatus.name == "unfinished"
-                              ? "hideRead"
-                              : "unfinished",
-                    ),
-                  );
-                  Navigator.of(context).pop();
-                },
-                child: Text(
-                  state.episodesFilterStatus.name == "unfinished"
-                      ? "Show all"
-                      : "Show only unfinished",
-                  textAlign: TextAlign.center,
+                TextButton(
+                  onPressed: () {
+                    context.read<PodcastSettingsCubit>().updateUiFilterSettings(
+                          showOnlyUnfinished:
+                              episodesState.settings.showOnlyUnfinished
+                                  ? false
+                                  : true,
+                          showOnlyFavorites: false,
+                          showOnlyDownloaded: false,
+                        );
+                    Navigator.of(context).pop();
+                  },
+                  child: Text(
+                    episodesState.settings.showOnlyUnfinished
+                        ? "Show all"
+                        : "Show only unfinished",
+                    textAlign: TextAlign.center,
+                  ),
                 ),
-              ),
-              TextButton(
-                onPressed: () {
-                  podcastBloc.add(
-                    ToggleEpisodesFilterStatusEvent(
-                      filterStatus:
-                          state.episodesFilterStatus.name == "favorites"
-                              ? "hideRead"
-                              : "favorites",
-                    ),
-                  );
-                  Navigator.of(context).pop();
-                },
-                child: Text(
-                  state.episodesFilterStatus.name == "favorites"
-                      ? "Show all"
-                      : "Show only favorites",
-                  textAlign: TextAlign.center,
+                TextButton(
+                  onPressed: () {
+                    context.read<PodcastSettingsCubit>().updateUiFilterSettings(
+                          showOnlyFavorites:
+                              episodesState.settings.showOnlyFavorites
+                                  ? false
+                                  : true,
+                          showOnlyDownloaded: false,
+                          showOnlyUnfinished: false,
+                        );
+                    Navigator.of(context).pop();
+                  },
+                  child: Text(
+                    episodesState.settings.showOnlyFavorites
+                        ? "Show all"
+                        : "Show only favorites",
+                    textAlign: TextAlign.center,
+                  ),
                 ),
-              ),
-              TextButton(
-                onPressed: () {
-                  podcastBloc.add(
-                    ToggleEpisodesFilterStatusEvent(
-                      filterStatus:
-                          state.episodesFilterStatus.name == "downloaded"
-                              ? "hideRead"
-                              : "downloaded",
-                    ),
-                  );
-                  Navigator.of(context).pop();
-                },
-                child: Text(
-                  state.episodesFilterStatus.name == "downloaded"
-                      ? "Show all"
-                      : "Show only downloaded",
-                  textAlign: TextAlign.center,
+                TextButton(
+                  onPressed: () {
+                    context.read<PodcastSettingsCubit>().updateUiFilterSettings(
+                          showOnlyDownloaded:
+                              episodesState.settings.showOnlyDownloaded
+                                  ? false
+                                  : true,
+                          showOnlyUnfinished: false,
+                          showOnlyFavorites: false,
+                        );
+                    Navigator.of(context).pop();
+                  },
+                  child: Text(
+                    episodesState.settings.showOnlyDownloaded
+                        ? "Show all"
+                        : "Show only downloaded",
+                    textAlign: TextAlign.center,
+                  ),
                 ),
-              ),
-            ],
-          );
+              ],
+            );
+          } else {
+            return const AlertDialog(
+              title: Text("Filter Episodes"),
+              content: CircularProgressIndicator(),
+            );
+          }
         });
   }
 }
