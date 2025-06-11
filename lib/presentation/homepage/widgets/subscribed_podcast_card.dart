661e047ae085e306b58d8b16cef24ba6fc2dbc45
bruno <1201ba@gmail.com>
2025-06-11 18:55:31 +0200

    Display unread episodes count badge on subscribed podcast card.

--- lib/presentation/homepage/widgets/subscribed_podcast_card.dart
+++ lib/presentation/homepage/widgets/subscribed_podcast_card.dart
@@ -4,6 +4,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import '../../../application/podcast_bloc/podcast_bloc.dart';
 import '../../../domain/entities/podcast_entity.dart';
+import '../../../domain/usecases/episode_usecases.dart';
+import '../../../injection.dart';
 import '../../custom_widgets/page_transition.dart';
 import '../../episodes_list_page/episodes_list_page.dart';
 
@@ -42,6 +44,49 @@ class SubscribedPodcastCard extends StatelessWidget {
               fit: BoxFit.fitHeight,
             ),
             borderRadius: BorderRadius.circular(15),
+          ),
+          child: Stack(
+            children: [
+              Positioned(
+                bottom: 2.0,
+                right: 2.0,
+                child: SizedBox(
+                  width: 44,
+                  height: 44,
+                  child: StreamBuilder<int>(
+                    stream: getIt<EpisodeUseCases>()
+                        .unreadLocalEpisodesCount(feedId: podcast.pId),
+                    builder: (context, snapshot) {
+                      if (snapshot.hasData) {
+                        final unreadEpisodesCount = snapshot.data;
+                        return Container(
+                          decoration: BoxDecoration(
+                              shape: BoxShape.circle,
+                              color: Theme.of(context).colorScheme.onPrimary),
+                          alignment: Alignment.center,
+                          padding: const EdgeInsets.all(5.0),
+                          child: Text(
+                            unreadEpisodesCount.toString(),
+                            style: Theme.of(context)
+                                .textTheme
+                                .bodyMedium!
+                                .copyWith(
+                                  color: Theme.of(context)
+                                      .colorScheme
+                                      .primaryContainer,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                            textAlign: TextAlign.center,
+                          ),
+                        );
+                      } else {
+                        return const SizedBox();
+                      }
+                    },
+                  ),
+                ),
+              ),
+            ],
           ),
         ),
       ),
