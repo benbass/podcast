367e1f97e6bc38f4ad86b8191c6090a39ebd3db2
bruno <1201ba@gmail.com>
2025-06-12 18:25:41 +0200

    Update BoxDecoration with fixed color and shadow properties.

--- lib/presentation/custom_widgets/decoration/box_decoration.dart
+++ lib/presentation/custom_widgets/decoration/box_decoration.dart
@@ -3,10 +3,10 @@ import 'package:flutter/material.dart';
 BoxDecoration buildBoxDecoration(BuildContext context) {
   return BoxDecoration(
     borderRadius: BorderRadius.circular(10.0),
-    color: Theme.of(context).colorScheme.primaryContainer,
+    color: Colors.white10,//Theme.of(context).colorScheme.primaryContainer,
     boxShadow: const [
       BoxShadow(
-        color: Colors.black,
+        color: Colors.black12,
         blurRadius: 5.0,
         offset: Offset(0, 6),
         spreadRadius: 1.0,
