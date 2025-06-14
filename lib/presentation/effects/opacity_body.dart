fc3daa7671d6d4033b800b4ffda2c56cb835ff50
bruno <1201ba@gmail.com>
2025-06-14 18:53:24 +0200

    Create an `OpacityBody` widget.
    The `OpacityBody` widget displays the artwork of the currently playing podcast with an opacity of 0.4. It falls back to a placeholder image if no podcast is playing.

---
+++ lib/presentation/effects/opacity_body.dart
@@ -0,0 +1,34 @@
+import 'dart:io';
+
+import 'package:flutter/cupertino.dart';
+
+import '../../application/podcast_bloc/podcast_bloc.dart';
+
+class OpacityBody extends StatelessWidget {
+  const OpacityBody({
+    super.key,
+    required this.state,
+    required this.assetImage,
+  });
+
+  final PodcastState? state;
+  final AssetImage? assetImage;
+
+  @override
+  Widget build(BuildContext context) {
+    return Opacity(
+      opacity: 0.4,
+      child: state != null ? Image.file(
+        File(state!.currentPodcast.artworkFilePath!),
+        fit: BoxFit.cover,
+        errorBuilder: (BuildContext context, Object error,
+            StackTrace? stackTrace) {
+          return const SizedBox();
+        },
+      ) : Image.asset(
+        'assets/placeholder.png',
+        fit: BoxFit.cover,
+      ),
+    );
+  }
+}
