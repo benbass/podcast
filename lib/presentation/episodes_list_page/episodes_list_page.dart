a6ff1c758d630acd7bb7039ab0866c801605ccc5
bruno <1201ba@gmail.com>
2025-06-17 18:21:47 +0200

    Refactor EpisodesListPage layout and UI elements.
    This commit refactors the layout of the `EpisodesListPage` to use a `SliverAppBar` for better scrolling behavior and a more standard UI.
    
    Specific changes include:
    - Replaced `Positioned` widgets with `AppBar` and `SliverAppBar` for UI elements like title, back button, and action icons.
    - Moved the unread episodes count indicator to the `AppBar`'s title.
    - Relocated action buttons (info, download) to a `SliverAppBar`'s flexible space.
    - The `RowIconButtonsEpisodes` widget is now conditionally displayed in the `AppBar`'s actions based on podcast subscription status.
    - Made the `unreadLocalEpisodesCount` stream a broadcast stream to ensure proper cancellation when a podcast is unsubscribed.
    - Updated failure and no-episodes messages to include the podcast title.
    - Removed unused `ScrollController` and related padding calculations.
    - Removed unused custom `ElevatedButtonSubscribe` and `BoxDecoration` imports.

--- lib/presentation/episodes_list_page/episodes_list_page.dart
+++ lib/presentation/episodes_list_page/episodes_list_page.dart
@@ -4,7 +4,6 @@ import 'package:podcast/application/episode_selection_cubit/episode_selection_cu
 import 'package:podcast/domain/usecases/episode_usecases.dart';
 import 'package:podcast/injection.dart';
 
-import 'package:podcast/presentation/custom_widgets/elevated_button_subscribe.dart';
 import 'package:podcast/presentation/episodes_list_page/widgets/animated_download_icon.dart';
 import 'package:podcast/presentation/episodes_list_page/widgets/conditional_floating_action_buttons.dart';
 import 'package:podcast/presentation/episodes_list_page/widgets/episode_card.dart';
@@ -12,7 +11,6 @@ import '../../application/episodes_bloc/episodes_bloc.dart';
 import '../../application/podcast_bloc/podcast_bloc.dart';
 import '../../application/podcast_settings_cubit/podcast_settings_cubit.dart';
 import '../../domain/entities/podcast_filter_settings_entity.dart';
-import '../custom_widgets/decoration/box_decoration.dart';
 import '../custom_widgets/effects/backdropfilter_body.dart';
 import '../custom_widgets/effects/opacity_body.dart';
 import '../custom_widgets/failure_widget.dart';
@@ -38,7 +36,8 @@ class _EpisodesListPageWrapperState extends State<EpisodesListPageWrapper> {
     final currentPodcast = podcastState.currentPodcast;
     context.read<PodcastSettingsCubit>().loadSettings(currentPodcast.id);
     _unreadEpisodesStream = getIt<EpisodeUseCases>()
-        .unreadLocalEpisodesCount(feedId: currentPodcast.pId);
+        .unreadLocalEpisodesCount(feedId: currentPodcast.pId)
+        .asBroadcastStream(); // asBroadcastStream() so stream will be correctly canceled when podcast is being unsubscribed
   }
 
   @override
@@ -102,8 +101,6 @@ class EpisodesListPage extends StatelessWidget {
   Widget build(BuildContext context) {
     final podcastState = context.watch<PodcastBloc>().state;
     final currentPodcast = podcastState.currentPodcast;
-    ScrollController scrollController = ScrollController();
-    const double paddingChangeThreshold = 10;
 
     return PopScope(
       onPopInvokedWithResult: (didPop, result) async {
@@ -118,6 +115,41 @@ class EpisodesListPage extends StatelessWidget {
         }
       },
       child: Scaffold(
+        appBar: AppBar(
+          title: StreamBuilder<int>(
+            stream: unreadEpisodesStream,
+            builder: (context, snapshot) {
+              if (snapshot.hasError) {
+                return const SizedBox();
+              }
+              if (snapshot.hasData) {
+                final unreadEpisodesCount = snapshot.data;
+                return Container(
+                  decoration: BoxDecoration(
+                      shape: BoxShape.circle,
+                      color: Theme.of(context).colorScheme.primary),
+                  alignment: Alignment.center,
+                  padding: const EdgeInsets.all(10.0),
+                  child: Text(
+                    unreadEpisodesCount.toString(),
+                    style: Theme.of(context).textTheme.bodyMedium!.copyWith(
+                          color: Theme.of(context).colorScheme.primaryContainer,
+                          fontWeight: FontWeight.bold,
+                        ),
+                    textAlign: TextAlign.center,
+                  ),
+                );
+              } else {
+                return const SizedBox();
+              }
+            },
+          ),
+          actions: [
+            const SizedBox(width: 20),
+            if (podcastState.currentPodcast.subscribed)
+              const RowIconButtonsEpisodes(),
+          ],
+        ),
         body: Stack(
           fit: StackFit.expand,
           children: [
@@ -127,164 +159,79 @@ class EpisodesListPage extends StatelessWidget {
                 assetImage: null,
               ),
             const BackdropFilterBody(),
-            // Title
-            Positioned(
-              top: 0,
-              left: 0,
-              right: 0,
-              child: SafeArea(
-                child: Padding(
-                  padding: const EdgeInsets.all(12.0),
-                  child: Text(
-                    currentPodcast.title,
-                    style: Theme.of(context).textTheme.displayLarge,
-                    textAlign: TextAlign.center,
-                    maxLines: 1,
-                    overflow: TextOverflow.ellipsis,
-                  ),
-                ),
-              ),
-            ),
-            // Row icon buttons
-            Positioned(
-              top: 94,
-              left: 0,
-              right: 0,
-              child: AnimatedBuilder(
-                  animation: scrollController,
-                  builder: (context, child) {
-                    const double minPadding = 12;
-                    const double maxPadding = 65;
-                    double calculatePadding = 12;
-                    if (scrollController.hasClients &&
-                        scrollController.offset > 0) {
-                      calculatePadding =
-                          (scrollController.offset / paddingChangeThreshold);
-                    }
-                    final double padding =
-                        calculatePadding.clamp(minPadding, maxPadding);
-                    return Padding(
-                      padding: EdgeInsets.symmetric(horizontal: padding),
-                      child: Wrap(
-                        alignment: WrapAlignment.center,
-                        crossAxisAlignment: WrapCrossAlignment.center,
-                        spacing: 10,
-                        runSpacing: 10,
-                        children: [
-                          Container(
-                            decoration: buildBoxDecoration(context),
-                            child: Padding(
-                              padding: const EdgeInsets.all(8.0),
-                              child: Row(
-                                mainAxisAlignment: MainAxisAlignment.center,
-                                mainAxisSize: MainAxisSize.min,
-                                children: [
-                                  const BackButton(),
-                                  const Spacer(),
-                                  ElevatedButtonSubscribe(
-                                    podcast: currentPodcast,
+            BlocBuilder<EpisodesBloc, EpisodesState>(
+              builder: (context, episodesState) {
+                if (episodesState.status == EpisodesStatus.loading &&
+                    episodesState.episodes.isEmpty) {
+                  return const Center(child: CircularProgressIndicator());
+                } else if (episodesState.status == EpisodesStatus.failure) {
+                  return Center(
+                    child: buildFailureWidget(
+                        message: episodesState.errorMessage ??
+                            'Error loading episodes for\n${currentPodcast.title}'),
+                  );
+                } else if (episodesState.episodes.isEmpty &&
+                    (episodesState.status == EpisodesStatus.success ||
+                        episodesState.status == EpisodesStatus.refreshing)) {
+                  return Center(
+                      child: Text(
+                          "No episodes found for\n${currentPodcast.title}"));
+                } else {
+                  return SafeArea(
+                    child: CustomScrollView(
+                      physics: const BouncingScrollPhysics(),
+                      slivers: [
+                        SliverAppBar(
+                          primary: false,
+                          automaticallyImplyLeading: false,
+                          backgroundColor: Colors.transparent,
+                          pinned: false,
+                          flexibleSpace: Padding(
+                            padding: const EdgeInsets.all(4.0),
+                            child: Row(
+                              mainAxisAlignment:
+                                  MainAxisAlignment.end,
+                              children: [
+                                const AnimatedDownloadIcon(),
+                                const SizedBox(width: 30),
+                                IconButton(
+                                  onPressed: () => Navigator.push(
+                                      context,
+                                      ScaleRoute(
+                                        page:
+                                            const PodcastDetailsPage(),
+                                      )),
+                                  icon: const Icon(
+                                    Icons.info_outline_rounded,
+                                    size: 30,
                                   ),
-                                  const Spacer(),
-                                  IconButton(
-                                    onPressed: () => Navigator.push(
-                                        context,
-                                        ScaleRoute(
-                                          page: const PodcastDetailsPage(),
-                                        )),
-                                    icon: const Icon(
-                                      Icons.info_outline_rounded,
-                                      size: 30,
-                                    ),
-                                  ),
-                                  const Spacer(
-                                  ),
-                                  const AnimatedDownloadIcon(),
-                                  const Spacer(),
-                                  StreamBuilder<int>(
-                                    stream: unreadEpisodesStream,
-                                    builder: (context, snapshot) {
-                                      if (snapshot.hasData) {
-                                        final unreadEpisodesCount =
-                                            snapshot.data;
-                                        return Container(
-                                          decoration: BoxDecoration(
-                                              shape: BoxShape.circle,
-                                              color: Theme.of(context)
-                                                  .colorScheme
-                                                  .primary),
-                                          alignment: Alignment.center,
-                                          padding: const EdgeInsets.all(10.0),
-                                          child: Text(
-                                            unreadEpisodesCount.toString(),
-                                            style: Theme.of(context)
-                                                .textTheme
-                                                .bodyMedium!
-                                                .copyWith(
-                                                  color: Theme.of(context)
-                                                      .colorScheme
-                                                      .primaryContainer,
-                                                  fontWeight: FontWeight.bold,
-                                                ),
-                                            textAlign: TextAlign.center,
-                                          ),
-                                        );
-                                      } else {
-                                        return const SizedBox();
-                                      }
-                                    },
-                                  ),
-                                  const Spacer(),
-                                ],
-                              ),
+                                ),
+                              ],
                             ),
                           ),
-                          if (currentPodcast.subscribed)
-                            Padding(
-                              padding: const EdgeInsets.all(8.0),
-                              child: Container(
-                                  decoration: buildBoxDecoration(context),
-                                  child: const RowIconButtonsEpisodes()),
-                            ),
-                        ],
-                      ),
-                    );
-                  }),
+                        ),
+                        SliverPadding(
+                          padding: const EdgeInsets.only(
+                            bottom: 80.0,
+                          ),
+                          sliver: SliverList.builder(
+                            itemCount: episodesState.episodes.length,
+                            itemBuilder: (context, index) {
+                              final item = episodesState.episodes[index];
+                              return EpisodeCard(
+                                episodes: episodesState.episodes,
+                                episode: item,
+                                podcast: currentPodcast,
+                              );
+                            },
+                          ),
+                        ),
+                      ],
+                    ),
+                  );
+                }
+              },
             ),
-            BlocBuilder<EpisodesBloc, EpisodesState>(
-                builder: (context, episodesState) {
-              if (episodesState.status == EpisodesStatus.loading &&
-                  episodesState.episodes.isEmpty) {
-                return const Center(child: CircularProgressIndicator());
-              } else if (episodesState.status == EpisodesStatus.failure) {
-                return Center(
-                  child: buildFailureWidget(
-                      message: episodesState.errorMessage ??
-                          'Error loading episodes'),
-                );
-              } else if (episodesState.episodes.isEmpty &&
-                  (episodesState.status == EpisodesStatus.success ||
-                      episodesState.status == EpisodesStatus.refreshing)) {
-                return const Center(child: Text("No episodes found"));
-              } else {
-                return Padding(
-                  padding: EdgeInsets.only(
-                      bottom: 80.0, top: currentPodcast.subscribed ? 230 : 170),
-                  child: ListView.builder(
-                    controller: scrollController,
-                    physics: const BouncingScrollPhysics(),
-                    itemCount: episodesState.episodes.length,
-                    itemBuilder: (context, index) {
-                      final item = episodesState.episodes[index];
-                      return EpisodeCard(
-                        episodes: episodesState.episodes,
-                        episode: item,
-                        podcast: currentPodcast,
-                      );
-                    },
-                  ),
-                );
-              }
-            }),
             const ConditionalFloatingActionButtons(),
           ],
         ),
