15d9dc661d223e71480e993eb5bb6cf486983334
bruno <1201ba@gmail.com>
2025-06-17 18:22:23 +0200

    Refactor `FlexibleSpace` for episode details page.
    - Removed custom back button and rely on default navigation.
    - Removed `AnimatedDownloadIcon`.
    - Moved `PlayButton` and removed `PlaybackLinearProgressIndicator` to simplify the UI.
    - Adjusted `expandedHeight` and `collapsedHeight` for the `SliverAppBar`.
    - Removed unnecessary `BlocBuilder` and `StreamBuilder` for episode playback state as the functionality is handled differently now.
    - Simplified `_getImageUrl` logic.
    - The `SliverAppBar` is now configured to be `floating` and `snap` for a more standard user experience.
    - The `backgroundColor` is set to transparent to allow the background image to show through.

--- lib/presentation/episode_details_page/widgets/flexible_space.dart
+++ lib/presentation/episode_details_page/widgets/flexible_space.dart
@@ -2,22 +2,11 @@ import 'dart:io';
 import 'dart:ui';
 
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:just_audio/just_audio.dart';
 import 'package:podcast/domain/entities/episode_entity.dart';
 import 'package:podcast/domain/entities/podcast_entity.dart';
-import 'package:podcast/domain/usecases/episode_usecases.dart';
-import 'package:podcast/presentation/custom_widgets/page_transition.dart';
 import 'package:podcast/presentation/custom_widgets/play_button.dart';
-import 'package:podcast/presentation/custom_widgets/playback_linear_progress_indicator.dart';
-import 'package:podcast/presentation/episodes_list_page/episodes_list_page.dart';
 
-import '../../../application/episode_playback_cubit/episode_playback_cubit.dart';
 import '../../../helpers/core/utilities/image_provider.dart';
-import '../../../helpers/player/audiohandler.dart';
-import '../../../injection.dart';
-import '../../audioplayer_overlays/audioplayer_overlays.dart';
-import '../../episodes_list_page/widgets/animated_download_icon.dart';
 
 class FlexibleSpace extends StatelessWidget {
   final PodcastEntity podcast;
@@ -39,79 +28,50 @@ class FlexibleSpace extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    var themeData = Theme.of(context);
     final String imageUrl = _getImageUrl();
 
     return SliverAppBar(
-      backgroundColor: Colors.black12,
-      expandedHeight: MediaQuery.of(context).size.height * 0.25,
-      pinned: true,
-      leading: null,
-      automaticallyImplyLeading:
-          false, // we remove the default back-button, together with leading: null
+      primary: false,
+      automaticallyImplyLeading: false,
+      backgroundColor: Colors.transparent,
+      pinned: false,
+      expandedHeight: MediaQuery.of(context).size.height * 0.33,
+      collapsedHeight: MediaQuery.of(context).size.height * 0.15,
+      floating: true,
+      snap: true,
+      stretch: true,
       flexibleSpace: FlexibleSpaceBar(
+        centerTitle: true,
+        collapseMode: CollapseMode.parallax,
         background: Stack(
           children: [
             _buildBackgroundImage(imageUrl),
-            const Positioned(
-              top: 70,
-              left: 12,
-              child: AnimatedDownloadIcon(size: 42),
-            ),
-              Positioned(
-                bottom: 12,
-                right: 12,
-                child: PlayButton(
-                  episode: episode,
-                  episodeIndex: episodeIndex!,
-                  playlist: playlist!,
-                  podcast: podcast,
-                ),
-              ),
-              BlocBuilder<EpisodePlaybackCubit, EpisodePlaybackState>(
-                buildWhen: (previous, current) {
-                  return previous.episode?.eId != current.episode?.eId;
-                },
-                builder: (context, currentlyPlayingEpisodeState) {
-                  return Positioned(
-                    bottom: 0,
-                    left: 0,
-                    child: StreamBuilder<EpisodeEntity?>(
-                        stream: getIt<EpisodeUseCases>()
-                            .getEpisodeStream(episodeId: episode.id),
-                        initialData: episode,
-                        builder: (context, snapshot) {
-                          final episodeInProgress = snapshot.data ?? episode;
-                          return PlaybackLinearProgressIndicator(
-                            themeData: themeData,
-                            episode: episodeInProgress,
-                            currentlyPlayingEpisode:
-                                currentlyPlayingEpisodeState.episode,
-                            paddingHoriz: 0.0,
-                            paddingVert: 0.0,
-                          );
-                        }),
-                  );
-                },
-              ),
           ],
         ),
         titlePadding: const EdgeInsets.all(4.0),
         expandedTitleScale: 1.5,
-        title: _buildBackButton(context),
+        title: Row(
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: [
+            PlayButton(
+              episode: episode,
+              episodeIndex: episodeIndex!,
+              playlist: playlist!,
+              podcast: podcast,
+            ),
+          ],
+        ),
       ),
     );
   }
 
   /// Determines the image URL based on whether an episode or podcast is provided.
   String _getImageUrl() {
-
-      return episode.image.isNotEmpty
-          ? episode.image
-          : podcast.artworkFilePath != null
-              ? podcast.artworkFilePath!
-              : podcast.artwork;
-
+    return episode.image.isNotEmpty
+        ? episode.image
+        : podcast.artworkFilePath != null
+            ? podcast.artworkFilePath!
+            : podcast.artwork;
   }
 
   /// Builds the background image with blur effect.
@@ -141,41 +101,6 @@ class FlexibleSpace extends StatelessWidget {
           ),
         );
       },
-    );
-  }
-
-  Widget _buildBackButton(BuildContext context) {
-    return SizedBox(
-      width: MediaQuery.of(context).size.width,
-      child: Column(
-        mainAxisSize: MainAxisSize.min,
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          const Spacer(
-            flex: 3,
-          ),
-          IconButton(
-            onPressed: () {
-                final audioHandler = getIt<MyAudioHandler>();
-                if (audioHandler.player.processingState ==
-                        ProcessingState.ready &&
-                    overlayEntry == null) {
-                  showOverlayPlayerMin(context);
-                }
-
-              Navigator.of(context).pushAndRemoveUntil(
-                ScaleRoute(
-                  page: const EpisodesListPageWrapper(),
-                ),
-                ModalRoute.withName('/'),
-              );
-            },
-            icon: const BackButtonIcon(),
-          ),
-          const Spacer(flex: 5),
-        ],
-      ),
     );
   }
 }
