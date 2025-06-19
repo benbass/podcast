2d6676a3640dd60cd2f2251963ed7443ae545b94
bruno <1201ba@gmail.com>
2025-06-19 16:48:31 +0200

    Add button to show/hide read episodes.

---
+++ lib/presentation/episodes_list_page/widgets/toggle_read_visibility.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import '../../../application/podcast_settings_cubit/podcast_settings_cubit.dart';
+
+class ToggleReadVisibility extends StatelessWidget {
+  const ToggleReadVisibility({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocBuilder<PodcastSettingsCubit,
+        PodcastSettingsState>(
+        builder: (context, settingsState) {
+          if (settingsState
+          is PodcastSettingsLoaded) {
+            return IconButton(
+              onPressed: () {
+                context
+                    .read<PodcastSettingsCubit>()
+                    .updateUiFilterSettings(
+                  filterRead: settingsState
+                      .settings.filterRead
+                      ? false
+                      : true,
+                  showOnlyRead: false,
+                );
+                ScaffoldMessenger.of(context)
+                    .showSnackBar(SnackBar(
+                    content: Text(
+                        "Read episodes are ${settingsState.settings.filterRead ? "not hidden" : "hidden"}"),
+                    duration: const Duration(
+                        milliseconds: 1500)));
+              },
+              icon: Icon(
+                settingsState.settings.filterRead
+                    ? Icons.visibility_outlined
+                    : Icons.visibility_off_outlined,
+                size: 30,
+              ),
+            );
+          } else {
+            return const SizedBox();
+          }
+        });
+  }
+}
