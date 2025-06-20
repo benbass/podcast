aef04b45c0990e65e0c2246d8abcdb1a5f049dfe
bruno <1201ba@gmail.com>
2025-06-20 17:12:56 +0200

    Add playlistBox and globalPlaylistId to globals.dart.

--- lib/core/globals.dart
+++ lib/core/globals.dart
@@ -3,6 +3,7 @@ import 'package:podcast/domain/entities/episode_entity.dart';
 import 'package:podcast/domain/entities/podcast_entity.dart';
 
 import '../domain/entities/persistent_podcast_settings_entity.dart';
+import '../domain/entities/playlist_entity.dart';
 import 'objectbox.dart';
 
 const String baseUrl = 'https://api.podcastindex.org/api/1.0';
@@ -10,7 +11,9 @@ late ObjectBox objectBox;
 late Box<PodcastEntity> podcastBox;
 late Box<EpisodeEntity> episodeBox;
 late Box<PersistentPodcastSettingsEntity> settingsBox;
+late Box<AppPlaylist> playlistBox;
 const String kCurrentDownloadFilePath = "current_download_filepath";
+const int globalPlaylistId = 1;
 
 /// For testing: create a fake episode
 DateTime now = DateTime.now();
