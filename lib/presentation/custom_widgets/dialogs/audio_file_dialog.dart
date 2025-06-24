a0e0b92d4466ea36bb7f378bc526d1d3723271fc
bruno <1201ba@gmail.com>
2025-06-24 15:16:42 +0200

    Refactor audio file dialog to use backdrop filter widget.

--- lib/presentation/custom_widgets/dialogs/audio_file_dialog.dart
+++ lib/presentation/custom_widgets/dialogs/audio_file_dialog.dart
@@ -1,24 +1,19 @@
-import 'dart:ui';
-
 import 'package:flutter/material.dart';
+import 'package:podcast/presentation/custom_widgets/effects/backdropfilter.dart';
 
 import '../../../domain/entities/episode_entity.dart';
 import '../../../helpers/audio_download/audio_file_utility.dart';
 
-class AudioFileDialog{
-  static void showAudioFileDialog(BuildContext context, EpisodeEntity episode, String? filePath) {
+class AudioFileDialog {
+  static void showAudioFileDialog(
+      BuildContext context, EpisodeEntity episode, String? filePath) {
     Navigator.pop(context); // close previous dialog
     showDialog(
       context: context,
       builder: (context) {
         return Stack(
           children: [
-            BackdropFilter(
-              filter: ImageFilter.blur(sigmaX: 4.0, sigmaY: 4.0),
-              child: Container(
-                color: Colors.black26,
-              ),
-            ),
+            const BackdropFilterWidget(sigma: 4.0),
             AlertDialog(
                 title: Text(filePath == null
                     ? "Download episode audio file to device"
