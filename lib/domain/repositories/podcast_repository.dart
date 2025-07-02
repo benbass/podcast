174db65390bd7be14935ae571df242e986341033
bruno <1201ba@gmail.com>
2025-07-02 18:36:24 +0200

    Refactor: Rename `savePodcastAndArtwork` to `savedPodcastWithArtwork` in `PodcastRepository`.
    This change renames the method for clarity and consistency.

--- lib/domain/repositories/podcast_repository.dart
+++ lib/domain/repositories/podcast_repository.dart
@@ -4,7 +4,7 @@ abstract class PodcastRepository {
   Future<List<PodcastEntity>> fetchPodcastsByKeywords(String keyword);
   Future<List<PodcastEntity>> fetchTrendingPodcasts();
   Future<PodcastEntity> fetchPodcastByFeedId(int feedId);
-  Future<PodcastEntity> savePodcastAndArtwork(PodcastEntity podcast);
+  Future<PodcastEntity> savedPodcastWithArtwork(PodcastEntity podcast);
   Future<bool> subscribeToPodcast(PodcastEntity podcast);
   Future<void>  unsubscribeFromPodcast(PodcastEntity podcast);
   Future<List<PodcastEntity>> getSubscribedPodcasts();
