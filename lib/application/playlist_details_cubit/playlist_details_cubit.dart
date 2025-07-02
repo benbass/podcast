ca7e3a0baf7f12f02066178c36bf2606bf135bf5
bruno <1201ba@gmail.com>
2025-07-02 18:39:04 +0200

    Remove `currentPlayingIndex` from `PlaylistDetailsLoaded`.
    This information is now managed by `PlaybackCubit`.
    Renamed `setAutoplay` to `updatePersistentSettings` for clarity.

--- lib/application/playlist_details_cubit/playlist_details_cubit.dart
+++ lib/application/playlist_details_cubit/playlist_details_cubit.dart
@@ -28,9 +28,8 @@ class PlaylistDetailsCubit extends Cubit<PlaylistDetailsState> {
 
       if (playlist.episodeIds.isEmpty) {
         emit(PlaylistDetailsLoaded(
-          const [],
-          playlist.currentPlayingIndex,
-          playlist.autoPlayEnabled,
+          playlist: const [],
+          autoPlayEnabled: playlist.autoPlayEnabled,
         ));
         return;
       }
@@ -44,9 +43,8 @@ class PlaylistDetailsCubit extends Cubit<PlaylistDetailsState> {
       // In that case order the episodes as in playlist.episodeIds!
 
       emit(PlaylistDetailsLoaded(
-        List<EpisodeEntity>.from(episodeEntities),
-        playlist.currentPlayingIndex,
-        playlist.autoPlayEnabled,
+        playlist: List<EpisodeEntity>.from(episodeEntities),
+        autoPlayEnabled: playlist.autoPlayEnabled,
       ));
     } catch (e) {
       emit(PlaylistDetailsError("Error loading the playlist."));
@@ -79,8 +77,10 @@ class PlaylistDetailsCubit extends Cubit<PlaylistDetailsState> {
       final List<EpisodeEntity> allEpisodeEntities =
           allPlaylistEpisodes.whereType<EpisodeEntity>().toList();
 
-      emit(PlaylistDetailsLoaded(List<EpisodeEntity>.from(allEpisodeEntities),
-          playlist.currentPlayingIndex, playlist.autoPlayEnabled));
+      emit(PlaylistDetailsLoaded(
+        playlist: List<EpisodeEntity>.from(allEpisodeEntities),
+        autoPlayEnabled: playlist.autoPlayEnabled,
+      ));
     } catch (e) {
       emit(PlaylistDetailsError("Error adding episode(s) to playlist."));
     }
@@ -97,7 +97,6 @@ class PlaylistDetailsCubit extends Cubit<PlaylistDetailsState> {
         return;
       }
 
-      final int indexOfRemovedEpisode = playlist.episodeIds.indexOf(episodeId);
       final bool wasRemoved = playlist.episodeIds.remove(episodeId);
 
       if (!wasRemoved) {
@@ -106,42 +105,17 @@ class PlaylistDetailsCubit extends Cubit<PlaylistDetailsState> {
         return;
       }
 
-      // Update currentPlayingIndex
-      if (playlist.episodeIds.isEmpty) {
-        playlist.currentPlayingIndex = null;
-      } else if (playlist.currentPlayingIndex != null) {
-        if (indexOfRemovedEpisode < playlist.currentPlayingIndex!) {
-          playlist.currentPlayingIndex = playlist.currentPlayingIndex! - 1;
-        } else if (indexOfRemovedEpisode == playlist.currentPlayingIndex!) {
-          if (playlist.currentPlayingIndex! >= playlist.episodeIds.length) {
-            playlist.currentPlayingIndex = playlist.episodeIds.isNotEmpty
-                ? playlist.episodeIds.length - 1
-                : null;
-          }
-        }
-      }
-
-      if (playlist.currentPlayingIndex != null &&
-          (playlist.currentPlayingIndex! < 0 ||
-              playlist.currentPlayingIndex! >= playlist.episodeIds.length)) {
-        playlist.currentPlayingIndex = null;
-      }
-
       playlistBox.put(playlist);
-
-      if (playlist.episodeIds.isEmpty) {
-        emit(PlaylistDetailsLoaded(
-            const [], playlist.currentPlayingIndex, playlist.autoPlayEnabled));
-        return;
-      }
 
       final List<EpisodeEntity?> allPlaylistEpisodes =
           episodeBox.getMany(playlist.episodeIds);
       final List<EpisodeEntity> allEpisodeEntities =
           allPlaylistEpisodes.whereType<EpisodeEntity>().toList();
 
-      emit(PlaylistDetailsLoaded(List<EpisodeEntity>.from(allEpisodeEntities),
-          playlist.currentPlayingIndex, playlist.autoPlayEnabled));
+      emit(PlaylistDetailsLoaded(
+        playlist: allEpisodeEntities,
+        autoPlayEnabled: playlist.autoPlayEnabled,
+      ));
     } catch (e) {
       emit(PlaylistDetailsError("Error removing episode from playlist."));
     }
@@ -173,29 +147,14 @@ class PlaylistDetailsCubit extends Cubit<PlaylistDetailsState> {
       final int episodeIdToMove = playlist.episodeIds.removeAt(oldIndex);
       playlist.episodeIds.insert(adjustedNewIndex, episodeIdToMove);
 
-      // --- Update currentPlayingIndex ---
-      if (playlist.currentPlayingIndex != null) {
-        int currentIndex = playlist.currentPlayingIndex!;
-
-        if (oldIndex == currentIndex) {
-          playlist.currentPlayingIndex = adjustedNewIndex;
-        } else {
-          if (oldIndex < currentIndex && adjustedNewIndex >= currentIndex) {
-            playlist.currentPlayingIndex = currentIndex - 1;
-          } else if (oldIndex > currentIndex &&
-              adjustedNewIndex <= currentIndex) {
-            playlist.currentPlayingIndex = currentIndex + 1;
-          }
-        }
-      }
-      // --- End Update currentPlayingIndex ---
-
       playlistBox.put(playlist);
       final List<EpisodeEntity?> allPlaylistEpisodes =
           episodeBox.getMany(playlist.episodeIds);
 
-      emit(PlaylistDetailsLoaded(List<EpisodeEntity>.from(allPlaylistEpisodes),
-          playlist.currentPlayingIndex, playlist.autoPlayEnabled));
+      emit(PlaylistDetailsLoaded(
+        playlist: List<EpisodeEntity>.from(allPlaylistEpisodes),
+        autoPlayEnabled: playlist.autoPlayEnabled,
+      ));
     } catch (e) {
       emit(PlaylistDetailsError("Error reordering playlist."));
       loadPlaylist();
@@ -212,17 +171,18 @@ class PlaylistDetailsCubit extends Cubit<PlaylistDetailsState> {
       }
 
       playlist.episodeIds.clear();
-      playlist.currentPlayingIndex = null;
       playlistBox.put(playlist);
 
       emit(PlaylistDetailsLoaded(
-          const [], playlist.currentPlayingIndex, playlist.autoPlayEnabled));
+        playlist: const [],
+        autoPlayEnabled: playlist.autoPlayEnabled,
+      ));
     } catch (e) {
       emit(PlaylistDetailsError("Error clearing playlist."));
     }
   }
 
-  Future<void> setAutoplay(bool enabled) async {
+  Future<void> updatePersistentSettings(bool enabled) async {
     try {
       final AppPlaylist? playlist = playlistBox.get(globalPlaylistId);
       if (playlist == null) {
@@ -231,64 +191,15 @@ class PlaylistDetailsCubit extends Cubit<PlaylistDetailsState> {
       playlist.autoPlayEnabled = enabled;
       playlistBox.put(playlist);
 
-      if (state is PlaylistDetailsLoaded) {
-        final currentState = state as PlaylistDetailsLoaded;
-        emit(PlaylistDetailsLoaded(
-          currentState.playlist,
-          currentState.currentPlayingIndex,
-          playlist.autoPlayEnabled,
-        ));
-      } else {
-        // Fallback just in case
-        final List<EpisodeEntity?> allPlaylistEpisodes =
-            episodeBox.getMany(playlist.episodeIds);
-        final List<EpisodeEntity> allEpisodeEntities =
-            allPlaylistEpisodes.whereType<EpisodeEntity>().toList();
-        emit(PlaylistDetailsLoaded(List<EpisodeEntity>.from(allEpisodeEntities),
-            playlist.currentPlayingIndex, playlist.autoPlayEnabled));
-      }
+      final List<EpisodeEntity?> allPlaylistEpisodes =
+          episodeBox.getMany(playlist.episodeIds);
+
+      emit(PlaylistDetailsLoaded(
+        playlist: List<EpisodeEntity>.from(allPlaylistEpisodes),
+        autoPlayEnabled: enabled,
+      ));
     } catch (e) {
-      emit(PlaylistDetailsError("Error setting autoplay."));
-    }
-  }
-
-  Future<void> setCurrentPlayingIndex(int? index) async {
-    try {
-      final AppPlaylist? playlist = playlistBox.get(globalPlaylistId);
-
-      if (playlist == null) {
-        return;
-      }
-
-      // Validate index
-      if (index != null && (index < 0 || index >= playlist.episodeIds.length)) {
-        emit(PlaylistDetailsError("Invalid playback index provided."));
-        return;
-      } else {
-        playlist.currentPlayingIndex = index;
-      }
-
-      playlist.currentPlayingIndex = index;
-      playlistBox.put(playlist);
-
-      if (state is PlaylistDetailsLoaded) {
-        final currentState = state as PlaylistDetailsLoaded;
-        emit(PlaylistDetailsLoaded(
-          currentState.playlist,
-          playlist.currentPlayingIndex,
-          currentState.autoPlayEnabled,
-        ));
-      } else {
-        // Fallback just in case
-        final List<EpisodeEntity?> allPlaylistEpisodes =
-            episodeBox.getMany(playlist.episodeIds);
-        final List<EpisodeEntity> allEpisodeEntities =
-            allPlaylistEpisodes.whereType<EpisodeEntity>().toList();
-        emit(PlaylistDetailsLoaded(List<EpisodeEntity>.from(allEpisodeEntities),
-            playlist.currentPlayingIndex, playlist.autoPlayEnabled));
-      }
-    } catch (e) {
-      emit(PlaylistDetailsError("Error setting current playing index."));
+      emit(PlaylistDetailsError("Error updating autoplay setting."));
     }
   }
 }
