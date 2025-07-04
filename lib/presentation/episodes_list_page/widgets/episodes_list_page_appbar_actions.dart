278cf312acf592b488e44fb4dcd869112bf6d722
bruno <1201ba@gmail.com>
2025-07-04 16:42:16 +0200

    Propagate podcast settings changes to playback cubit.
    When the "Autoplay" or "Continuous Playback" setting is changed for a podcast, this change is now also propagated to the `PlaybackCubit` if the current playlist being played is the episode list for that podcast.

--- lib/presentation/episodes_list_page/widgets/episodes_list_page_appbar_actions.dart
+++ lib/presentation/episodes_list_page/widgets/episodes_list_page_appbar_actions.dart
@@ -1,14 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:podcast/application/episodes_bloc/episodes_bloc.dart';
+import 'package:podcast/application/playback_cubit/playback_cubit.dart';
 
 import '../../../application/episode_selection_cubit/episode_selection_cubit.dart';
 import '../../../application/podcast_bloc/podcast_bloc.dart';
 import '../../../application/podcast_settings_cubit/podcast_settings_cubit.dart';
 import 'iconbutton_with_popup_text.dart';
 
-class RowIconButtonsEpisodes extends StatelessWidget {
-  const RowIconButtonsEpisodes({
+class EpisodesListPageAppBarActions extends StatelessWidget {
+  const EpisodesListPageAppBarActions({
     super.key,
   });
 
@@ -119,6 +120,14 @@ class RowIconButtonsEpisodes extends StatelessWidget {
                       context
                           .read<PodcastSettingsCubit>()
                           .updatePersistentSettings(autoplayEnabled: value);
+                      // Update the autoplay setting in the playback cubit if the current playlist is this podcast's episodes list
+                      final String? origin =
+                          context.read<PlaybackCubit>().state.origin;
+                      if (origin == podcastState.currentPodcast.feedId.toString()) {
+                        context
+                            .read<PlaybackCubit>()
+                            .updateAutoPlay(autoplayEnabled: value);
+                      }
                     },
                   ),
                 ],
